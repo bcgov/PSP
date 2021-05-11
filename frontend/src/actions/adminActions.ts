a86db6bd90e952a884c6424ac1f4508693413d40
Devin Smith <devinleighsmith@gmail.com>
2021-05-10 17:51:48 -0700

    Convert agency reducer/actions to slice. Update corresponding tests. merge agencies and agencydetail into one slice of state.

--- frontend/src/actions/adminActions.ts
+++ frontend/src/actions/adminActions.ts
@@ -1,14 +1,5 @@
 import * as ActionTypes from 'constants/actionTypes';
-import {
-  IPagedItems,
-  IUser,
-  IUserDetails,
-  IAgency,
-  IRole,
-  IUsersFilter,
-  IAgencyRecord,
-  IAgencyDetail,
-} from 'interfaces';
+import { IPagedItems, IUser, IUserDetails, IUsersFilter } from 'interfaces';
 import { IUserRecord } from 'features/admin/users/interfaces/IUserRecord';
 import { TableSort } from 'components/Table/TableSort';
 
@@ -93,64 +84,3 @@ export const getUsersSortAction = (sort: TableSort<IUserRecord>): ISortUsersActi
   type: ActionTypes.SORT_USERS,
   sort,
 });
-
-// agency admin
-export interface IStoreAgencyDetail {
-  type: typeof ActionTypes.STORE_AGENCY_DETAILS;
-  agencyDetail: IAgency;
-}
-
-export interface IStoreAgencyAction {
-  type: typeof ActionTypes.STORE_AGENCY_RESULTS;
-  pagedAgencies: IPagedItems<IAgency>;
-}
-
-export interface IAgencyPaginationAction {
-  type: typeof ActionTypes.STORE_AGENCY_RESULTS;
-  pagedAgencies: IPagedItems<IAgency>;
-}
-
-export interface IGetAgencyAction {
-  type: typeof ActionTypes.GET_AGENCY;
-  pagedAgency: IAgency;
-}
-
-export interface IUpdateAgencyAction {
-  type: typeof ActionTypes.UPDATE_AGENCY;
-  agency: IAgency;
-}
-
-export interface ISortAgenciesAction {
-  type: typeof ActionTypes.SORT_AGENCIES;
-  sort: TableSort<IAgencyDetail>;
-}
-
-export const storeAgencyDetails = (agencyDetail: IAgency) => ({
-  type: ActionTypes.STORE_AGENCY_DETAILS,
-  agencyDetail: agencyDetail,
-});
-
-export const storeAgencies = (apiData: IPagedItems<IAgency>) => ({
-  type: ActionTypes.STORE_AGENCY_RESULTS,
-  pagedAgencies: { ...apiData, pageIndex: apiData.page - 1 },
-});
-
-export const updateAgency = (agency: IAgencyDetail): IUpdateAgencyAction => ({
-  type: ActionTypes.UPDATE_AGENCY,
-  agency,
-});
-
-export const setAgenciesPageSize = (size: number) => ({
-  type: ActionTypes.SET_AGENCIES_PAGE_SIZE,
-  size,
-});
-
-export const getAgenciesSortAction = (sort: TableSort<IAgencyRecord>): ISortAgenciesAction => ({
-  type: ActionTypes.SORT_AGENCIES,
-  sort,
-});
-
-export interface IAddNewRoleAndAgency {
-  agency: IAgency;
-  role: IRole;
-}
---
+++ frontend/src/store/slices/agencies/agencyActionCreator.test.tsx
@@ -0,0 +1,140 @@
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import * as genericActions from 'actions/genericActions';
+import * as API from 'constants/API';
+import * as MOCK from 'mocks/dataMocks';
+import { ENVIRONMENT } from 'constants/environment';
+import { Provider } from 'react-redux';
+import { MockStoreEnhanced } from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { renderHook } from '@testing-library/react-hooks';
+import { useAgencies } from './useAgencies';
+import { GET_AGENCIES } from 'constants/actionTypes';
+import { AGENCIES } from 'mocks/filterDataMock';
+
+const dispatch = jest.fn();
+const requestSpy = jest.spyOn(genericActions, 'request');
+const successSpy = jest.spyOn(genericActions, 'success');
+const errorSpy = jest.spyOn(genericActions, 'error');
+const mockAxios = new MockAdapter(axios);
+
+afterEach(() => {
+  mockAxios.reset();
+  dispatch.mockClear();
+  requestSpy.mockClear();
+  successSpy.mockClear();
+  errorSpy.mockClear();
+});
+let currentStore: MockStoreEnhanced<any, {}>;
+const mockStore = configureMockStore([thunk]);
+const getStore = (values?: any) => {
+  currentStore = mockStore(values ?? {});
+  return currentStore;
+};
+const getWrapper = (store: any) => ({ children }: any) => (
+  <Provider store={store}>{children}</Provider>
+);
+
+describe('fetchAgencies action creator', () => {
+  it('Request successful, dispatches success with correct response', () => {
+    const url = ENVIRONMENT.apiUrl + API.POST_AGENCIES();
+    const mockResponse = {
+      data: {
+        items: AGENCIES,
+        page: 1,
+        pageIndex: 0,
+        quantity: 0,
+        total: 0,
+      },
+      pageIndex: null,
+    };
+    mockAxios.onPost(url).reply(200, mockResponse);
+    renderHook(
+      () =>
+        useAgencies()
+          .fetchAgencies({} as any)
+          .then(() => {
+            expect(requestSpy).toHaveBeenCalledTimes(1);
+            expect(successSpy).toHaveBeenCalledTimes(1);
+            expect(currentStore.getActions()).toContainEqual({
+              payload: mockResponse,
+              type: 'agencies/storeAgencies',
+            });
+          }),
+      {
+        wrapper: getWrapper(getStore()),
+      },
+    );
+  });
+
+  it('Request failure, dispatches error with correct response', () => {
+    const url = ENVIRONMENT.apiUrl + API.POST_AGENCIES();
+    const mockResponse = { data: [AGENCIES] };
+    mockAxios.onGet(url).reply(400, MOCK.ERROR);
+    renderHook(
+      () =>
+        useAgencies()
+          .fetchAgencies({} as any)
+          .then(() => {
+            expect(requestSpy).toHaveBeenCalledTimes(1);
+            expect(errorSpy).toHaveBeenCalledTimes(1);
+            expect(currentStore.getActions()).not.toContainEqual({
+              payload: mockResponse,
+              type: GET_AGENCIES,
+            });
+          }),
+      {
+        wrapper: getWrapper(getStore()),
+      },
+    );
+  });
+});
+
+describe('getAgencyDetail action creator', () => {
+  it('Request successful, dispatches success with correct response', () => {
+    const url = ENVIRONMENT.apiUrl + API.AGENCY_DETAIL({ id: AGENCIES[0].id });
+    const mockResponse = {
+      data: [AGENCIES[0]],
+    };
+    mockAxios.onGet(url).reply(200, mockResponse);
+    renderHook(
+      () =>
+        useAgencies()
+          .fetchAgencyDetail({ id: AGENCIES[0].id } as any)
+          .then(() => {
+            expect(requestSpy).toHaveBeenCalledTimes(1);
+            expect(successSpy).toHaveBeenCalledTimes(1);
+            expect(currentStore.getActions()).toContainEqual({
+              payload: mockResponse,
+              type: 'agencies/storeAgencyDetails',
+            });
+          }),
+      {
+        wrapper: getWrapper(getStore()),
+      },
+    );
+  });
+
+  it('Request failure, dispatches error with correct response', () => {
+    const url = ENVIRONMENT.apiUrl + API.AGENCY_DETAIL({ id: AGENCIES[0].id });
+    const mockResponse = { data: [AGENCIES] };
+    mockAxios.onGet(url).reply(400, MOCK.ERROR);
+    renderHook(
+      () =>
+        useAgencies()
+          .fetchAgencyDetail({ id: AGENCIES[0].id } as any)
+          .then(() => {
+            expect(requestSpy).toHaveBeenCalledTimes(1);
+            expect(errorSpy).toHaveBeenCalledTimes(1);
+            expect(currentStore.getActions()).not.toContainEqual({
+              payload: mockResponse,
+              type: 'agencies/storeAgencyDetails',
+            });
+          }),
+      {
+        wrapper: getWrapper(getStore()),
+      },
+    );
+  });
+});
