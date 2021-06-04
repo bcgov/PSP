e59175b2301fc3bb2f203bc1ca2d778288649447
Alejandro Sanchez <emailforasr@gmail.com>
2021-06-04 16:21:22 -0700

    Increase code coverage

--- frontend/src/tenants/FeatureHidden/FeatureHidden.test.tsx
+++ frontend/src/tenants/FeatureHidden/FeatureHidden.test.tsx
@@ -32,4 +32,30 @@ describe('FeatureHidden', () => {
     expect(queryByTestId('feature-toggle-testid')).toBeInTheDocument();
     delete process.env.REACT_APP_TENANT;
   });
+
+  describe('when using render props', () => {
+    const withRenderProps = () =>
+      render(
+        <TenantProvider>
+          {/* Hide the content below if tenant is MOTI */}
+          <FeatureHidden tenant="MOTI">
+            {(hidden: boolean) => (hidden ? null : <div data-testid="feature-toggle-testid"></div>)}
+          </FeatureHidden>
+        </TenantProvider>,
+      );
+
+    it(`should hide content for specified tenant`, () => {
+      process.env.REACT_APP_TENANT = 'MOTI';
+      const { queryByTestId } = withRenderProps();
+      expect(queryByTestId('feature-toggle-testid')).not.toBeInTheDocument();
+      delete process.env.REACT_APP_TENANT;
+    });
+
+    it(`should show the content for other tenants`, () => {
+      process.env.REACT_APP_TENANT = 'CITZ';
+      const { queryByTestId } = withRenderProps();
+      expect(queryByTestId('feature-toggle-testid')).toBeInTheDocument();
+      delete process.env.REACT_APP_TENANT;
+    });
+  });
 });
--- frontend/src/tenants/FeatureVisible/FeatureVisible.test.tsx
+++ frontend/src/tenants/FeatureVisible/FeatureVisible.test.tsx
@@ -7,7 +7,7 @@ import { FeatureVisible } from '.';
 const testRender = () =>
   render(
     <TenantProvider>
-      {/* Hide the content below if tenant is MOTI */}
+      {/* Show the content below only for MOTI */}
       <FeatureVisible tenant="MOTI">
         <div data-testid="feature-toggle-testid"></div>
       </FeatureVisible>
@@ -31,5 +31,31 @@ describe('FeatureVisible', () => {
     const { queryByTestId } = testRender();
     expect(queryByTestId('feature-toggle-testid')).not.toBeInTheDocument();
     delete process.env.REACT_APP_TENANT;
+  });
+
+  describe('when using render props', () => {
+    const withRenderProps = () =>
+      render(
+        <TenantProvider>
+          {/* Show the content below only for MOTI */}
+          <FeatureVisible tenant="MOTI">
+            {(visible: boolean) => visible && <div data-testid="feature-toggle-testid"></div>}
+          </FeatureVisible>
+        </TenantProvider>,
+      );
+
+    it(`should show content for specified tenant`, () => {
+      process.env.REACT_APP_TENANT = 'MOTI';
+      const { queryByTestId } = withRenderProps();
+      expect(queryByTestId('feature-toggle-testid')).toBeInTheDocument();
+      delete process.env.REACT_APP_TENANT;
+    });
+
+    it(`should hide the content for other tenants`, () => {
+      process.env.REACT_APP_TENANT = 'CITZ';
+      const { queryByTestId } = withRenderProps();
+      expect(queryByTestId('feature-toggle-testid')).not.toBeInTheDocument();
+      delete process.env.REACT_APP_TENANT;
+    });
   });
 });
