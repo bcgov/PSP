f251c30673392348298658399e9ac2bd3155b00b
Devin Smith <devinleighsmith@gmail.com>
2021-06-25 11:16:37 -0700

    Updates from UI/UX review from Bobbi PSP-1366

--- frontend/src/features/moti/mapSideBar/components/FormHeader.tsx
+++ frontend/src/features/moti/mapSideBar/components/FormHeader.tsx
@@ -7,4 +7,7 @@ export const FormHeader = styled.h4`
   color: ${props => props.theme.css.textColor};
   background-color: ${props => props.theme.css.filterBackgroundColor};
   text-align: left;
+  position: sticky;
+  top: 0;
+  z-index: 1;
 `;
