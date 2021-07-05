c61ff4359d63b0a9950598a93e83411fff3c51a5
SOOKEKE <SOOKEKE@ND057487>
2021-07-05 14:47:15 -0700

    deployment, build and role-binding pims-logging

--- openshift/4.0/templates/Logging/sidecar-logging.sh
+++ openshift/4.0/templates/Logging/sidecar-logging.sh
@@ -5,54 +5,91 @@ set -euo pipefail
 
 # sensible defaults if not set
 : "${DEDUPE:=true}"
+: "${TERM_EXIT:=false}"
 : "${SLEEP_TIME:=60}"
+: "${EXPORT_TIME:=120}"
 : "${GRACEFUL_EXIT_TIME:=55}"
-: "${HOSTNAME:=unknown}"
-: "${STARTUP_TIME:=30}"
+: "${HOSTNAME:=}"
+: "${STARTUP_TIME:=0}"
+declare -i elapse=0
 
-# get openshift logs for container
-oc_logs='oc logs --since-time $since ${POD_NAME} -c ${CONTAINER_NAME}'
-# send zipped to endpoint
-gzip_curl='gzip -c -f | curl -sS --data-binary @- ${LOG_SERVER_URI} -H "Compression:Gzip"'
+
+DATE_NOW=$(date +"%Y%m%d")
+AZ_VERSION="2018-03-28"
+AZ_BLOB_TARGET="${AZ_BLOB_URL}/${AZ_BLOB_CONTAINER}/"
+
+#login
+oc_login='oc login --token=$OC_TOKEN --server=$OC_SERVER'
+
+  if [ "$(oc whoami 2>/dev/null | wc -l)" == "0" ]; then
+    eval $oc_login; 
+  fi
+#export time must be greater than delayed or sleep time
+if [ $EXPORT_TIME -lt $SLEEP_TIME ]; then
+	echo "EXPORT_TIME must be greater than SLEEP_TIME. pims-logging will gracefully exit after 10s"
+	sleep 10
+	exit 0
+fi
+
+# get app pod and container name
+APP_POD_NAME=$(oc -n $PROJECT_NAMESPACE get pods --selector=name=${FRONTEND_APP_NAME} -o   jsonpath="{.items[*].metadata.name}")
+APP_CONTAINER_NAME=$(oc -n $PROJECT_NAMESPACE get pods --selector=name=${FRONTEND_APP_NAME} -o jsonpath={.items[*].spec.containers[*].name})
+
+# get api pod and container name
+API_POD_NAME=$(oc -n $PROJECT_NAMESPACE get pods --selector=name=${API_NAME} -o   jsonpath="{.items[*].metadata.name}")
+API_CONTAINER_NAME=$(oc -n $PROJECT_NAMESPACE get pods --selector=name=${API_NAME} -o jsonpath={.items[*].spec.containers[*].name})
+
+# set log server URL
+APP_LOG_SERVER_URI="${AZ_BLOB_TARGET}${APP_CONTAINER_NAME}_${DATE_NOW}.gz${AZ_SAS_TOKEN}"
+API_LOG_SERVER_URI="${AZ_BLOB_TARGET}${API_CONTAINER_NAME}_${DATE_NOW}.gz${AZ_SAS_TOKEN}"
+
+
+# get openshift logs for container with timestamp for a set sleep time/period
+app_oc_logs='oc -n $PROJECT_NAMESPACE logs --timestamps --since=$SLEEP_TIME"s" ${APP_POD_NAME} -c ${APP_CONTAINER_NAME}'
+api_oc_logs='oc -n $PROJECT_NAMESPACE logs --timestamps --since=$SLEEP_TIME"s" ${API_POD_NAME} -c ${API_CONTAINER_NAME}'
+
+# send zipped to endpoint 
+app_gzip_curl='zip /logging/${APP_CONTAINER_NAME}_$DATE_NOW.gz /tmp/$APP_POD_NAME*'
+api_gzip_curl='zip /logging/${API_CONTAINER_NAME}_$DATE_NOW.gz /tmp/$API_POD_NAME*'
+app_send_zip='curl -v -X PUT  -H "Content-Type: application/zip" -H "Compression:Gzip" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@/logging/${APP_CONTAINER_NAME}_${DATE_NOW}.gz" ${APP_LOG_SERVER_URI}'
+api_send_zip='curl -v -X PUT  -H "Content-Type: application/zip" -H "Compression:Gzip" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" -H "x-ms-blob-type: BlockBlob" --data-binary "@/logging/${API_CONTAINER_NAME}_${DATE_NOW}.gz" ${API_LOG_SERVER_URI}'
+
+
+
 
 _send() {
-  if [ ! "x${DEDUPE}" = xtrue ]; then
-    # do not handle duplicates
-    eval $oc_logs | eval $gzip_curl;
-    return
-  fi
+
 
   # removes duplicates
   (
     # wait for exclusive lock (fd 200) for 2 seconds
     flock -x 200
 
-    # stores logs temporarily, rotating two files
-    file1=/tmp/001.dat
-    file2=/tmp/002.dat
+    # stores logs temporarily in file
+    app_file=/tmp/$APP_POD_NAME'-'$logdate.LOG ; api_file=/tmp/$API_POD_NAME'-'$logdate.LOG
 
-    # in case there is no match
-    touch $file1 $file2
+    # grep (-v) select non-matching lines, (-x) that match whole lines, (-f) get patterns from files 
+	#if log is not empty output to file
+ 
+	(app_hasLogs=$(eval $app_oc_logs | grep '.') && [ ! -z "$app_hasLogs" ] && echo "$app_hasLogs" > $app_file) & (api_hasLogs=$(eval $api_oc_logs | grep '.') && [ ! -z "$api_hasLogs" ] && echo "$api_hasLogs" > $api_file)
 
-    # grep (-v) select non-matching lines, (-x) that match whole lines, (-f) get patterns from files
-    if [[ ! -f "$file1" && ! -f "$file2" ]]
-    then
-	  eval $oc_logs > $file1
-	  cat $file1 | eval $gzip_curl;
-    elif [[ -f "$file1" && ! -f "$file2" ]]
-    then
-      eval $oc_logs > $file2
-      grep -v -x -f $file1 $file2 | eval $gzip_curl;
-      rm -f $file1
-    elif [[ ! -f "$file1" && -f "$file2" ]]
-    then
-      eval $oc_logs > $file1
-      grep -v -x -f $file2 $file1 | eval $gzip_curl;
-      rm -f $file2
-    else
-      eval $oc_logs | eval $gzip_curl;
-      rm -f $file1 $file2
-    fi
+  #run proess for X# of seconds, check if logs was extracted then upload to server
+	if [[ $elapse -ge $EXPORT_TIME && "$(ls -A /tmp/*$DATE_NOW* 2>/dev/null | wc -l)" != "0" ]]
+	then
+        APP_LOG_SERVER_URI="${AZ_BLOB_TARGET}${APP_CONTAINER_NAME}_${logdate}.gz${AZ_SAS_TOKEN}"
+        API_LOG_SERVER_URI="${AZ_BLOB_TARGET}${API_CONTAINER_NAME}_${logdate}.gz${AZ_SAS_TOKEN}"
+		(eval $app_gzip_curl && eval $app_send_zip && rm -f /tmp/$APP_POD_NAME* && rm -f /logging/$APP_CONTAINER_NAME*) & (eval $api_gzip_curl && eval $api_send_zip && rm -f /tmp/$API_POD_NAME* && rm -f /logging/$API_CONTAINER_NAME*)
+   elif [[ "$(ls -A /tmp/*$DATE_NOW* 2>/dev/null | wc -l)" == "0" ]]
+   then
+      echo "nothing to zip, logs are empty"
+   elif [[ "x${TERM_EXIT}" = xtrue ]] #check for extracted logs and send before pod scaled to zero
+   then
+		APP_LOG_SERVER_URI="${AZ_BLOB_TARGET}${APP_CONTAINER_NAME}_${logdate}.gz${AZ_SAS_TOKEN}"
+        API_LOG_SERVER_URI="${AZ_BLOB_TARGET}${API_CONTAINER_NAME}_${logdate}.gz${AZ_SAS_TOKEN}"
+		(eval $app_gzip_curl && eval $app_send_zip && rm -f /tmp/$APP_POD_NAME* && rm -f /logging/$APP_CONTAINER_NAME*) & (eval $api_gzip_curl && eval $api_send_zip && rm -f /tmp/$API_POD_NAME* && rm -f /logging/$API_CONTAINER_NAME*)
+    else 
+        echo "waiting for export time to elapse"
+	fi
   ) 200>/tmp/.data.lock
 
 };
@@ -65,9 +102,10 @@ sendLogs() {
   start=${SECONDS}
   # get the datetime for log retrieval. take account of previous duration and sleep
   # format since to be parseable by oc logs - use UTC and no additional timezone info (ex 2020-02-24T18:41:41Z)
-  since=$(date -u +"%Y-%m-%dT%H:%M:%SZ" -d@"$(( $(date +%s)-$(($duration + $exitSleep))))" | sed "s/ /T/")
+  logdate=$(date +"%Y%m%dT%H%M%SZ")
+
   # debug
-  echo "${HOSTNAME}: $(date +'%Y-%m-%d %H:%M:%S' | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/') duration: $duration since: $since"
+  echo "${HOSTNAME}: $(date +'%Y-%m-%d %H:%M:%S' | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/') duration: $duration"
   # use openshift client to get logs, gzip them and use curl to send to REST endpoint
   _send
   # this is equivalent to backgrouding our sleep, so we can interrupt when container halted
@@ -75,12 +113,14 @@ sendLogs() {
   wait
   # calculate total processing duration
   duration=$((${SECONDS} - $start));
+  elapse=$elapse+$duration
 };
 
 # sig handler for TERM sent by openshift when pod is stopped or deleted
 # need to adjust for graceful termination of the container we are getting logs for
 _term() {
   echo "Caught SIGTERM signal! Waiting ${GRACEFUL_EXIT_TIME} seconds before sending"
+  TERM_EXIT=true
   sleep ${GRACEFUL_EXIT_TIME}
   sendLogs ${GRACEFUL_EXIT_TIME}
   # exit gracefully
@@ -90,9 +130,10 @@ trap _term SIGTERM
 
 # initial startup delay for container we a getting logs for and ourselves
 duration=${STARTUP_TIME};
+elapse=$elapse+$duration
 
 # set variables to send logs
-while [[ "${POD_NAME}" && "${CONTAINER_NAME}" && "${LOG_SERVER_URI}" ]];
+while [[ "${APP_POD_NAME}" && "${API_POD_NAME}" && "${APP_LOG_SERVER_URI}" ]];
   do sendLogs 0
 done
 # sidecar does nothing if a header is empty
