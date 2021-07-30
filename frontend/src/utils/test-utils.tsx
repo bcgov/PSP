d3491e192b81d2c8485ae848b4f360b5e18b45d2
Alejandro Sanchez <emailforasr@gmail.com>
2021-07-30 14:05:37 -0700

    Create custom render function for unit tests

--- frontend/src/utils/test-utils.tsx
+++ frontend/src/utils/test-utils.tsx
@@ -1,10 +1,22 @@
 import { useKeycloak } from '@react-keycloak/web';
-import { fireEvent } from '@testing-library/react';
+import {
+  fireEvent,
+  render as rtlRender,
+  RenderOptions as RtlRenderOptions,
+  RenderResult,
+} from '@testing-library/react';
 import { createMemoryHistory, MemoryHistory } from 'history';
+import { IRole } from 'interfaces';
+import { IAgency } from 'interfaces/agency';
+import { KeycloakInstance } from 'keycloak-js';
 import noop from 'lodash/noop';
 import React, { ReactNode } from 'react';
 import { MapContainer } from 'react-leaflet';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import TestProviderWrapper from './TestProviderWrapper';
+import TestRouterWrapper from './TestRouterWrapper';
 
 export const mockKeycloak = (claims: string[], agencies: number[]) => {
   (useKeycloak as jest.Mock).mockReturnValue({
@@ -145,3 +157,70 @@ export function createMapContainer(
     );
   };
 }
+
+// re-export everything from RTL
+export * from '@testing-library/react';
+
+// override RenderOptions interface
+export interface RenderOptions extends RtlRenderOptions {
+  store?: any;
+  history?: MemoryHistory;
+  authenticated?: boolean;
+  agencies?: number[];
+  roles?: string[];
+}
+
+function render(
+  ui: React.ReactElement,
+  options: Omit<RenderOptions, 'wrapper'> = {},
+): RenderResult {
+  const { store, history, authenticated = false, agencies, roles, ...renderOptions } = options;
+  let auth: {
+    initialized: boolean;
+    keycloak: Partial<KeycloakInstance>;
+  } = {
+    initialized: true,
+    keycloak: {},
+  };
+
+  // mock authentication state prior to rendering
+  if (!authenticated) {
+    auth.keycloak = { authenticated: false };
+  }
+  if (!!authenticated || !!roles || !!agencies) {
+    auth.keycloak = {
+      userInfo: {
+        agencies: agencies ?? [1],
+        roles: roles ?? [],
+        email: 'test@test.com',
+        name: 'Chester Tester',
+      },
+      subject: 'test',
+      authenticated: true,
+    };
+  }
+  (useKeycloak as jest.Mock).mockReturnValue(auth);
+
+  // new providers will need to be added here
+  function AllTheProviders({ children }: PropsWithChildren) {
+    return (
+      <TestProviderWrapper store={store}>
+        <TestRouterWrapper history={history}>
+          <ToastContainer
+            autoClose={5000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss={false}
+          />
+          {children}
+        </TestRouterWrapper>
+      </TestProviderWrapper>
+    );
+  }
+  return rtlRender(ui, { wrapper: AllTheProviders, ...renderOptions });
+}
+
+// override render method
+export { render };
