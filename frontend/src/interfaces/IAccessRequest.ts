1494f54adb28f685509cde999210be79c8d3eb72
Devin Smith <devinleighsmith@gmail.com>
2021-08-27 16:08:50 -0700

    typescript fix.

--- frontend/src/interfaces/IAccessRequest.ts
+++ frontend/src/interfaces/IAccessRequest.ts
@@ -1,5 +1,8 @@
 import { AccessRequestStatus } from 'constants/index';
 import { IOrganization, IRole, IUser } from 'interfaces';
+import { IOrganization } from './organization';
+import { IRole } from './role';
+
 export interface IAccessRequest {
   id?: number;
   userId: number;
@@ -7,7 +10,9 @@ export interface IAccessRequest {
   note?: string | null;
   status: AccessRequestStatus;
   position?: string;
+  organization?: IOrganization;
   organizationId?: number | '';
+  role?: IRole;
   roleId?: number | '';
   createdOn?: string;
   rowVersion?: number;
