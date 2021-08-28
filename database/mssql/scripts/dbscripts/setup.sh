0ab36239330fa635b157fbfd7dafe0c60bf3f62d
SOOKEKE <SOOKEKE@ND057487>
2021-08-28 00:28:08 -0700

    use quoted identifier for sqlcmd

--- database/mssql/scripts/dbscripts/setup.sh
+++ database/mssql/scripts/dbscripts/setup.sh
@@ -23,17 +23,14 @@ echo "======= MSSQL CONFIG COMPLETE ======="
 #load db schema to local
 #Run every scripts in the build folder
 for i in "${TARGET_SPRINT}"/Build/*.sql; do 
-   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d $TARGET_SPRINT -i $i
+   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d $TARGET_SPRINT -i $i -b -I 
+   count=$?
+   if [ $count -ne 0 ]; 
+   then echo "======= SCRIPT ${i} RETURNS AN ERROR. ========="
+   exit 1;
+   else echo "======= SCRIPT ${i} COMPLETED SUCCESSFULLY. =========" && echo $count > /tmp/log.txt
+   fi
 done
-
-#check if schema was loaded successfully
-response=$(/opt/mssql-tools/bin/sqlcmd  -S localhost -U sa -P $MSSQL_SA_PASSWORD -d $TARGET_SPRINT -q 'SET NOCOUNT ON; SELECT COUNT(DISTINCT TABLE_NAME) as count FROM information_schema.columns WHERE TABLE_CATALOG = "'${TARGET_SPRINT}'";')
-count=$(echo "$response" | grep -o -E '[0-9]+')
-if [ "$count" -eq "0" ]; 
-then echo "======= ERROR LOADING DB SCHEMA ========"
-exit 1;
-else echo $response && echo $count >> /tmp/log.txt;
-fi
 
 echo "=======DB SCHEMA LOADED ========"
 
