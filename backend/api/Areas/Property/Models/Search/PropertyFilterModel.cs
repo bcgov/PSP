8ff9f064db97ae8d53c63659a35f035fa9f5f032
Alejandro Sanchez <emailforasr@gmail.com>
2021-09-08 17:00:34 -0700

    Refactor backend for Property List View

--- backend/api/Areas/Property/Models/Search/PropertyFilterModel.cs
+++ backend/api/Areas/Property/Models/Search/PropertyFilterModel.cs
@@ -12,35 +12,6 @@ namespace Pims.Api.Areas.Property.Models.Search
     public class PropertyFilterModel : PageFilter
     {
         #region Properties
-        /// <summary>
-        /// get/set - North East Latitude.
-        /// </summary>
-        public double? NELatitude { get; set; }
-
-        /// <summary>
-        /// get/set - North East Longitude.
-        /// </summary>
-        public double? NELongitude { get; set; }
-
-        /// <summary>
-        /// get/set - South West Latitude.
-        /// </summary>
-        public double? SWLatitude { get; set; }
-
-        /// <summary>
-        /// get/set - South West Longitude.
-        /// </summary>
-        public double? SWLongitude { get; set; }
-
-        /// <summary>
-        /// get/set - Parcel classification Id.
-        /// </summary>
-        public string ClassificationId { get; set; }
-
-        /// <summary>
-        /// get/set - Property type Id.
-        /// </summary>
-        public string PropertyTypeId { get; set; }
 
         /// <summary>
         /// get/set - The property address.
@@ -49,27 +20,15 @@ namespace Pims.Api.Areas.Property.Models.Search
         public string Address { get; set; }
 
         /// <summary>
-        /// get/set - The property municipality name.
-        /// </summary>
-        /// <value></value>
-        public string Municipality { get; set; }
-
-        /// <summary>
-        /// get/set - Value of the property name to be filtered.
-        /// </summary>
-        /// <value></value>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// get/set - An array of organizations.
-        /// </summary>
-        /// <value></value>
-        public long[] Organizations { get; set; }
-
-        /// <summary>
         /// get/set - The parcel PID.
         /// </summary>
         public string PID { get; set; }
+
+        /// <summary>
+        /// get/set - The unique identifier for untitled properties.
+        /// </summary>
+        public int? PIN { get; set; }
+
         #endregion
 
         #region Constructors
@@ -77,21 +36,6 @@ namespace Pims.Api.Areas.Property.Models.Search
         /// Creates a new instance of a PropertyFilterModel class.
         /// </summary>
         public PropertyFilterModel() { }
-
-        /// <summary>
-        /// Creates a new instance of a PropertyFilterModel class.
-        /// </summary>
-        /// <param name="neLat"></param>
-        /// <param name="neLong"></param>
-        /// <param name="swLat"></param>
-        /// <param name="swLong"></param>
-        public PropertyFilterModel(double neLat, double neLong, double swLat, double swLong)
-        {
-            this.NELatitude = neLat;
-            this.NELongitude = neLong;
-            this.SWLatitude = swLat;
-            this.SWLongitude = swLong;
-        }
 
         /// <summary>
         /// Creates a new instance of a PropertyFilterModel class, initializes with the specified arguments.
@@ -102,19 +46,10 @@ namespace Pims.Api.Areas.Property.Models.Search
             // We want case-insensitive query parameter properties.
             var filter = new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>(query, StringComparer.OrdinalIgnoreCase);
 
-            this.NELatitude = filter.GetDoubleNullValue(nameof(this.NELatitude));
-            this.NELongitude = filter.GetDoubleNullValue(nameof(this.NELongitude));
-            this.SWLatitude = filter.GetDoubleNullValue(nameof(this.SWLatitude));
-            this.SWLongitude = filter.GetDoubleNullValue(nameof(this.SWLongitude));
-
             this.PID = filter.GetStringValue(nameof(this.PID));
-            this.Name = filter.GetStringValue(nameof(this.Name));
+            this.PIN = filter.GetIntNullValue(nameof(this.PIN));
             this.Address = filter.GetStringValue(nameof(this.Address));
-            this.Municipality = filter.GetStringValue(nameof(this.Municipality));
-            this.ClassificationId = filter.GetStringValue(nameof(this.ClassificationId));
-            this.PropertyTypeId = filter.GetStringValue(nameof(this.PropertyTypeId));
 
-            this.Organizations = filter.GetLongArrayValue(nameof(this.Organizations)).Where(a => a != 0).ToArray();
             this.Sort = filter.GetStringArrayValue(nameof(this.Sort));
         }
         #endregion
@@ -131,19 +66,10 @@ namespace Pims.Api.Areas.Property.Models.Search
                 Page = model.Page,
                 Quantity = model.Quantity,
 
-                NELatitude = model.NELatitude,
-                NELongitude = model.NELongitude,
-                SWLatitude = model.SWLatitude,
-                SWLongitude = model.SWLongitude,
-
                 PID = model.PID,
-                Name = model.Name,
-                PropertyTypeId = model.PropertyTypeId,
-                ClassificationId = model.ClassificationId,
+                PIN = model.PIN,
                 Address = model.Address,
-                Municipality = model.Municipality,
 
-                Organizations = model.Organizations,
                 Sort = model.Sort
             };
 
@@ -157,17 +83,9 @@ namespace Pims.Api.Areas.Property.Models.Search
         public override bool IsValid()
         {
             return base.IsValid()
-                || this.NELatitude.HasValue
-                || this.NELongitude.HasValue
-                || this.SWLatitude.HasValue
-                || this.SWLongitude.HasValue
+                || this.PIN.HasValue
                 || !String.IsNullOrWhiteSpace(this.PID)
-                || !String.IsNullOrWhiteSpace(this.Name)
-                || !String.IsNullOrWhiteSpace(this.Address)
-                || !String.IsNullOrWhiteSpace(this.Municipality)
-                || !String.IsNullOrWhiteSpace(this.ClassificationId)
-                || !String.IsNullOrWhiteSpace(this.PropertyTypeId)
-                || this.Organizations?.Any() == true;
+                || !String.IsNullOrWhiteSpace(this.Address);
         }
         #endregion
     }
