d7eb1d340dfb956815915a0f5bd1592b53f6b06b
Devin Smith <devinleighsmith@gmail.com>
2021-09-12 21:59:59 -0700

    eslint cleanup.

--- frontend/src/features/leases/components/LeaseAndLicenseListView.test.tsx
+++ frontend/src/features/leases/components/LeaseAndLicenseListView.test.tsx
@@ -1,4 +1,4 @@
-import { act, prettyDOM, render, screen } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { useApiLeases } from 'hooks/pims-api/useApiLeases';
 import { fillInput } from 'utils/test-utils';
