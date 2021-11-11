dbfabdbeb3bd23c56296b6eeb9dcc6e9fbbd7361
Alejandro Sanchez <emailforasr@gmail.com>
2021-11-10 17:34:51 -0800

    Add generic helpers to render Table cell values

---
+++ frontend/src/components/Table/helpers.ts
@@ -0,0 +1,18 @@
+import { CellProps } from 'react-table';
+import { formatMoney, formatNumber, prettyFormatDate } from 'utils';
+
+/**
+ * These helper methods below provide ways to render common values on a Table cell;
+ *  - dates
+ *  - money
+ *  - percentages
+ */
+
+export const renderDate = <T extends object>({ cell: { value } }: CellProps<T, string>) =>
+  prettyFormatDate(value);
+
+export const renderMoney = <T extends object>({ cell: { value } }: CellProps<T, string>) =>
+  `$${formatMoney(value as any)}`;
+
+export const renderPercentage = <T extends object>({ cell: { value } }: CellProps<T, string>) =>
+  `${formatNumber(value as any, 2)}%`;
