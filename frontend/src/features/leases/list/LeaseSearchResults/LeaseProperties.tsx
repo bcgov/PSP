0ecd744c4353193d3c2397e3ce49970d508e2624
Manuel Rodriguez <marobej@gmail.com>
2021-11-22 16:37:00 -0800

    Lint fixes

--- frontend/src/features/leases/list/LeaseSearchResults/LeaseProperties.tsx
+++ frontend/src/features/leases/list/LeaseSearchResults/LeaseProperties.tsx
@@ -21,7 +21,7 @@ const LeaseProperties: React.FunctionComponent<ILeasePropertiesProps> = props =>
   }
 
   let rowItems = displayProperties.map((property, index) => {
-    let isLastItem = index + 1 === properties.length;
+    //let isLastItem = index + 1 === properties.length;
     return (
       <Row key={index + property.id} className="mx-0 my-2 border border-secondary">
         <Col md="12">
