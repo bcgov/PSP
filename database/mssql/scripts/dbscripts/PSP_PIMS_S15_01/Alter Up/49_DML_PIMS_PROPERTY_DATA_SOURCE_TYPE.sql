9594a4d113db2bd38b17b78286b6baff8c214f96
Smith <Devin.Smith@quartech.com>
2021-12-01 18:04:52 -0800

    psp-2463 fix the 15.1 and 16 alter ups to run when existing data is present.

--- database/mssql/scripts/dbscripts/PSP_PIMS_S15_01/Alter Up/49_DML_PIMS_PROPERTY_DATA_SOURCE_TYPE.sql
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S15_01/Alter Up/49_DML_PIMS_PROPERTY_DATA_SOURCE_TYPE.sql
@@ -15,3 +15,15 @@ VALUES
   (N'LIS', N'Lease Information System (LIS)'),
   (N'PAIMS', N'Property Acquisition and Inventory Management System (PAIMS)'),
   (N'GAZ', N'BC Gazette');
+
+-- Create foreign key constraint PIM_PIDSRT_PIM_PRPRTY_FK
+PRINT N'Create foreign key constraint PIM_PIDSRT_PIM_PRPRTY_FK'
+GO
+ALTER TABLE [dbo].[PIMS_PROPERTY]
+	ADD FOREIGN KEY([PROPERTY_DATA_SOURCE_TYPE_CODE])
+	REFERENCES [dbo].[PIMS_DATA_SOURCE_TYPE]([DATA_SOURCE_TYPE_CODE])
+	ON DELETE NO ACTION 
+	ON UPDATE NO ACTION 
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
