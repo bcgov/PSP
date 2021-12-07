0c5fd930a29ba9e6168e4ce4ae19df6535c07cc9
dfilteau@hilltopz.com <dfilteau>
2021-12-06 22:33:04 -0800

    Update 01_PSP_PIMS_S16_00_Alter_Up.sql
    Further update to 01_PSP_PIMS_S16_00_Alter_Up.sql

--- database/mssql/scripts/dbscripts/PSP_PIMS_S16_01/Alter Up/01_PSP_PIMS_S16_00_Alter_Up.sql
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S16_01/Alter Up/01_PSP_PIMS_S16_00_Alter_Up.sql
@@ -26,48 +26,6 @@ GO
 -- Create procedure dbo.pims_error_handling
 PRINT N'Create procedure dbo.pims_error_handling'
 GO
--- ............................................................................................
-
-
--- --------------------------------------------------------------------------------------------
--- Create procedure pims_error_handling 
--- --------------------------------------------------------------------------------------------
-CREATE PROCEDURE [dbo].[pims_error_handling] AS 
-  begin
-    DECLARE @errmsg   nvarchar(2048),
-      @severity tinyint,
-      @state    tinyint,
-      @errno    int,
-      @proc     sysname,
-      @lineno   int
-           
-    SELECT @errmsg = error_message(), @severity = error_severity(),
-      @state  = error_state(), @errno = error_number(),
-      @proc   = error_procedure(), @lineno = error_line()
-
-    IF @errmsg NOT LIKE '***%'
-      BEGIN
-        SELECT @errmsg = '*** ' + coalesce(quotename(@proc), '<dynamic SQL>') + 
-          ', Line ' + ltrim(str(@lineno)) + '. Errno ' + 
-          ltrim(str(@errno)) + ': ' + @errmsg
-      END
-
-    RAISERROR('%s', @severity, @state, @errmsg)
-  end
-
-GO
-IF @@ERROR <> 0 SET NOEXEC ON
-GO
-
--- Create procedure dbo.pims_error_handling
-PRINT N'Create procedure dbo.pims_error_handling'
-GO
--- ............................................................................................
-
-
--- --------------------------------------------------------------------------------------------
--- Create procedure pims_error_handling 
--- --------------------------------------------------------------------------------------------
 CREATE PROCEDURE [dbo].[pims_error_handling] AS 
   begin
     DECLARE @errmsg   nvarchar(2048),
