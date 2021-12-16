7e55c102cc0873bfe75ca08944d53fc442764130
Rafael Ponce de Leon <36902894+rafaelponcedeleon@users.noreply.github.com>
2021-12-16 14:25:12 -0800

    DB upgrade script

--- database/mssql/scripts/dbscripts/db-upgrade.sh
+++ database/mssql/scripts/dbscripts/db-upgrade.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-eval $(grep -v '^#' ../../.env | xargs)
-
-echo "===== Begin DB Schema Upgrade ====="
-
+# Script to upgrade an existing database to the latest version (default) or TARGET_VERSION (if passed as parameter "TARGET_VERSION=16.01")
 # 1. Find current DB version 
 #    select STATIC_VARIABLE_VALUE from PIMS_STATIC_VARIABLE where STATIC_VARIABLE_NAME = 'dbversion'
 # 2. Search for all SQL scripts under the 'Alter Up' folder and order them by folder name and then file name
 #    find ./PSP*/'Alter Up' -type f -iname "*.sql" | sort -n
-# 3. Execute all SQL scripts that have a higher version than the current DB version
+# 3. Execute all SQL scripts that have a higher version than the current DB version and are less or equal to the target version (if target version passed)
+
+eval $(grep -v '^#' ../../.env | xargs)
+
+echo "===== Begin DB Schema Upgrade ====="
+
  
 # get current db version
 currentdbversion=$(sqlcmd -S $SERVER_NAME -U $DB_USER -P $DB_PASSWORD -d $DB_NAME -h -1 -Q "SET NOCOUNT ON; select STATIC_VARIABLE_VALUE from PIMS_STATIC_VARIABLE where STATIC_VARIABLE_NAME = 'dbversion'");
