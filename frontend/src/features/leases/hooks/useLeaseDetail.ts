56b5c52b3a2f452dca4579b847801bc470c67431
Manuel Rodriguez <marobej@gmail.com>
2021-12-18 01:45:25 -0800

    Fixed react warnings

--- frontend/src/features/leases/hooks/useLeaseDetail.ts
+++ frontend/src/features/leases/hooks/useLeaseDetail.ts
@@ -12,26 +12,26 @@ export function useLeaseDetail(leaseId: number) {
   const { getLease } = useApiLeases();
   const dispatch = useDispatch();
 
-  const getLeaseById = async (id: number) => {
-    try {
-      dispatch(showLoading());
-      const { data } = await getLease(id);
-      setLease(data);
-    } catch (e) {
-      toast.error('Failed to load lease, reload this page to try again.');
-      dispatch(
-        logError({
-          name: 'LeaseLoad',
-          status: e?.response?.status,
-          error: e,
-        }),
-      );
-    } finally {
-      dispatch(hideLoading());
-    }
-  };
-
   useEffect(() => {
+    const getLeaseById = async (id: number) => {
+      try {
+        dispatch(showLoading());
+        const { data } = await getLease(id);
+        setLease(data);
+      } catch (e) {
+        toast.error('Failed to load lease, reload this page to try again.');
+        dispatch(
+          logError({
+            name: 'LeaseLoad',
+            status: e?.response?.status,
+            error: e,
+          }),
+        );
+      } finally {
+        dispatch(hideLoading());
+      }
+    };
+
     if (leaseId) {
       getLeaseById(leaseId);
     } else {
@@ -39,7 +39,7 @@ export function useLeaseDetail(leaseId: number) {
         'No valid lease id provided, go back to the lease and license list and select a valid lease.',
       );
     }
-  }, [getLease, leaseId, dispatch, needsUpdate]);
+  }, [getLease, dispatch, leaseId, needsUpdate]);
 
   const refresh = () => {
     setRefreshIndex(r => r + 1);
