76c3701db787262ccc02983fab8df416d106c189
Sue Tairaku <42981334+stairaku@users.noreply.github.com>
2022-01-06 15:19:34 -0800

    createLeases Automation test

--- testing/katalon/Object Repository/LeaseLicenses/viewLeaseDetails/labelPurpose.rs
+++ testing/katalon/Object Repository/LeaseLicenses/viewLeaseDetails/labelPurpose.rs
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>LeaseLicenseHyperlink</name>
+   <name>labelPurpose</name>
    <tag></tag>
-   <elementGuidId>6d3a0bee-8224-4f13-b9bb-c0e5c56e42b5</elementGuidId>
+   <elementGuidId>63dc4c07-78c6-44fb-90f5-85d49c6d667a</elementGuidId>
    <selectorCollection>
-      <entry>
-         <key>XPATH</key>
-         <value>//a[contains(text(),'Leases &amp; Licenses')]</value>
-      </entry>
       <entry>
          <key>BASIC</key>
          <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='input-purposeType.description']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
