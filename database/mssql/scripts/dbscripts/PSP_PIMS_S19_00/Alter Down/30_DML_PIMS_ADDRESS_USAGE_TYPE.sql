13f6beab36e9430786428562bec038cf91568ad8
dfilteau <dfilteau@hilltopz.com>
2022-01-18 14:00:07 -0800

    IS-19 Updated and Refreshed Database Schema

---
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S19_00/Alter Down/30_DML_PIMS_ADDRESS_USAGE_TYPE.sql
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------------
+Alter the usage type code description for the RESIDNT code value.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2022-Jan-17  Initial version
+----------------------------------------------------------------------------- */
+
+-- Set all current address usage type codes to disabled.
+UPDATE PIMS_ADDRESS_USAGE_TYPE
+SET    DESCRIPTION = N'Residential address'
+     , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+WHERE  ADDRESS_USAGE_TYPE_CODE = 'RESIDNT';
+GO
