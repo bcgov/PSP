69d5b89b3ea0fd5f0b80967e686a17a9222b3b19
Devin Smith <devinleighsmith@gmail.com>
2022-01-24 21:45:22 -0800

    add missing notesmodal

---
+++ frontend/src/components/common/form/NotesModal.tsx
@@ -0,0 +1,62 @@
+import { TextArea } from 'components/common/form';
+import GenericModal from 'components/common/GenericModal';
+import { IconButton } from 'components/common/styles';
+import { getIn, useFormikContext } from 'formik';
+import * as React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { FaRegFileAlt } from 'react-icons/fa';
+import { withNameSpace } from 'utils/formUtils';
+
+export interface INotesModalProps {
+  notesLabel: React.ReactNode;
+  title: string;
+  nameSpace?: string;
+  onSave?: (values: any) => void;
+}
+
+export const NotesModal: React.FunctionComponent<INotesModalProps> = ({
+  nameSpace,
+  onSave,
+  notesLabel,
+  title,
+}) => {
+  const { values, setFieldValue } = useFormikContext();
+  const [showNotes, setShowNotes] = useState(false);
+  const [currentNote, setCurrentNote] = useState();
+  const field = withNameSpace(nameSpace, 'note');
+  const noteValue = getIn(values, field);
+
+  useEffect(() => {
+    if (showNotes === false) {
+      setCurrentNote(noteValue);
+    }
+  }, [showNotes, noteValue]);
+  return (
+    <>
+      <IconButton title="notes" onClick={() => setShowNotes(true)} variant="light">
+        <FaRegFileAlt />
+      </IconButton>
+      <GenericModal
+        display={showNotes}
+        setDisplay={setShowNotes}
+        title={title}
+        message={
+          <>
+            {notesLabel}
+            <TextArea field={field} data-testid="note-field"></TextArea>
+          </>
+        }
+        closeButton
+        okButtonText="Save"
+        cancelButtonText="Cancel"
+        handleOk={() => onSave && onSave(values)}
+        handleCancel={() => {
+          setFieldValue(field, currentNote);
+        }}
+      ></GenericModal>
+    </>
+  );
+};
+
+export default NotesModal;
