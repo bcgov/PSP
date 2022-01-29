bbd8aad2f07342c6e1662c250df75924be5e3dae
Alejandro Sanchez <emailforasr@gmail.com>
2022-01-28 18:38:35 -0800

    Fix backend address mapping

--- backend/api/Areas/Persons/Mapping/Person/AddressMap.cs
+++ backend/api/Areas/Persons/Mapping/Person/AddressMap.cs
@@ -1,7 +1,7 @@
 using Mapster;
 using Pims.Api.Helpers.Extensions;
 using Entity = Pims.Dal.Entities;
-using Model =  Pims.Api.Areas.Persons.Models.Person;
+using Model = Pims.Api.Areas.Persons.Models.Person;
 
 namespace Pims.Api.Areas.Persons.Mapping.Person
 {
@@ -10,10 +10,21 @@ namespace Pims.Api.Areas.Persons.Mapping.Person
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<Entity.PimsPersonAddress, Model.PersonAddressModel>()
+                .Map(dest => dest.Id, src => src.AddressId)
+                .Map(dest => dest.RowVersion, src => src.Address.ConcurrencyControlNumber)
                 .Map(dest => dest.PersonAddressId, src => src.PersonAddressId)
                 .Map(dest => dest.PersonAddressRowVersion, src => src.ConcurrencyControlNumber)
-                .Map(dest => dest.AddressTypeId, src => src.AddressUsageTypeCodeNavigation)
-                .Map(dest => dest, src => src.Address);
+                .Map(dest => dest.StreetAddress1, src => src.Address.StreetAddress1)
+                .Map(dest => dest.StreetAddress2, src => src.Address.StreetAddress2)
+                .Map(dest => dest.StreetAddress3, src => src.Address.StreetAddress3)
+                .Map(dest => dest.Municipality, src => src.Address.MunicipalityName)
+                .Map(dest => dest.RegionId, src => src.Address.RegionCode)
+                .Map(dest => dest.DistrictId, src => src.Address.DistrictCode)
+                .Map(dest => dest.ProvinceId, src => src.Address.ProvinceStateId)
+                .Map(dest => dest.CountryId, src => src.Address.CountryId)
+                .Map(dest => dest.Postal, src => src.Address.PostalCode)
+                .Map(dest => dest.CountryOther, src => src.Address.OtherCountry)
+                .Map(dest => dest.AddressTypeId, src => src.AddressUsageTypeCodeNavigation);
 
             config.NewConfig<Entity.PimsAddress, Model.PersonAddressModel>()
                 .Map(dest => dest.Id, src => src.Id)
