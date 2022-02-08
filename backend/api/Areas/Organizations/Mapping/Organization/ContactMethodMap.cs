c11dacd902df1445d38c7246c61bee6096672fb9
Alejandro Sanchez <emailforasr@gmail.com>
2022-02-07 17:09:12 -0800

    Backend changes

--- backend/api/Areas/Organizations/Mapping/Organization/ContactMethodMap.cs
+++ backend/api/Areas/Organizations/Mapping/Organization/ContactMethodMap.cs
@@ -8,9 +8,17 @@ namespace Pims.Api.Areas.Organizations.Mapping.Organization
     {
         public void Register(TypeAdapterConfig config)
         {
+            config.NewConfig<Entity.PimsContactMethod, Pims.Api.Models.Contact.ContactMethodModel>()
+                .Map(dest => dest.Id, src => src.Id)
+                .Map(dest => dest.RowVersion, src => src.ConcurrencyControlNumber)
+                .Map(dest => dest.OrganizationId, src => src.OrganizationId)
+                .Map(dest => dest.ContactMethodTypeCode, src => src.ContactMethodTypeCodeNavigation)
+                .Map(dest => dest.Value, src => src.ContactMethodValue);
+
             config.NewConfig<Pims.Api.Models.Contact.ContactMethodModel, Entity.PimsContactMethod>()
-                .Map(dest => dest.ContactMethodId, src => src.Id)
+                .Map(dest => dest.Id, src => src.Id)
                 .Map(dest => dest.ConcurrencyControlNumber, src => src.RowVersion)
+                .Map(dest => dest.OrganizationId, src => src.OrganizationId)
                 .Map(dest => dest.ContactMethodTypeCode, src => src.ContactMethodTypeCode.GetTypeId())
                 .Map(dest => dest.ContactMethodValue, src => src.Value)
                 .IgnoreNullValues(true);
