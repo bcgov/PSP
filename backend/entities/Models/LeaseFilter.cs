a5d4e653845e97b86407fe361d3750b5951dc5f0
Manuel Rodriguez <marobej@gmail.com>
2022-02-09 14:14:41 -0800

    Updated lease filter and some underlying components

--- backend/entities/Models/LeaseFilter.cs
+++ backend/entities/Models/LeaseFilter.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Pims.Core.Extensions;
 
 namespace Pims.Dal.Entities.Models
 {
@@ -16,22 +15,46 @@ namespace Pims.Dal.Entities.Models
         public string PinOrPid { get; set; }
 
         /// <summary>
-        /// get/set - The value of the tenant name.
-        /// </summary>
-        /// <value></value>
-        public string TenantName { get; set; }
-
-        /// <summary>
         /// get/set - The LIS L File #.
         /// </summary>
         /// <value></value>
         public string LFileNo { get; set; }
 
         /// <summary>
-        /// get/set - The Program(s) to filter by.
+        /// get/set - The lease status type.
+        /// </summary>
+        public string LeaseStatusType { get; set; }
+
+        /// <summary>
+        /// get/set - The value of the tenant name.
         /// </summary>
         /// <value></value>
+        public string TenantName { get; set; }
+
+        /// <summary>
+        /// get/set - The Program(s) to filter by.
+        /// </summary>
         public IList<string> Programs { get; set; } = new List<string>();
+
+        /// <summary>
+        /// get/set - The expiry filter start date.
+        /// </summary>
+        public DateTime? ExpiryStartDate { get; set; }
+
+        /// <summary>
+        /// get/set - The expiry filter end date.
+        /// </summary>
+        public DateTime? ExpiryEndDate { get; set; }
+
+        /// <summary>
+        /// get/set - The region type.
+        /// </summary>
+        public int? RegionType { get; set; }
+
+        /// <summary>
+        /// get/set - Filter for additional lease details.
+        /// </summary>
+        public string Details { get; set; }
         #endregion
 
         #region Constructors
@@ -39,37 +62,6 @@ namespace Pims.Dal.Entities.Models
         /// Creates a new instance of a LeaseFilter class.
         /// </summary>
         public LeaseFilter() { }
-
-        /// <summary>
-        /// Creates a new instance of a LeaseFilter class, initializes it with the specified arguments.
-        /// </summary>
-        /// <param name="lFileNo"></param>
-        /// <param name="tenantName"></param>
-        /// <param name="pinOrPid"></param>
-        /// <param name="sort"></param>
-        /// <returns></returns>
-        public LeaseFilter(string lFileNo, string tenantName, string pinOrPid, string[] sort)
-        {
-            this.LFileNo = lFileNo;
-            this.TenantName = tenantName;
-            this.PinOrPid = pinOrPid;
-            this.Sort = sort;
-        }
-
-        /// <summary>
-        /// Creates a new instance of a LeaseFilter class, initializes it with the specified arguments.
-        /// Extracts the properties from the query string to generate the filter.
-        /// </summary>
-        /// <param name="query"></param>
-        public LeaseFilter(Dictionary<string, Microsoft.Extensions.Primitives.StringValues> query) : base(query)
-        {
-            // We want case-insensitive query parameter properties.
-            var filter = new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>(query, StringComparer.OrdinalIgnoreCase);
-
-            this.LFileNo = filter.GetStringValue(nameof(this.LFileNo));
-            this.TenantName = filter.GetStringValue(nameof(this.TenantName));
-            this.PinOrPid = filter.GetStringValue(nameof(this.PinOrPid));
-        }
         #endregion
 
         #region Methods
