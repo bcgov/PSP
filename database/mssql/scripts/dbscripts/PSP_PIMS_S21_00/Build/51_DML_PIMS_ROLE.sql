0a7cc928b325d585efd9b469550b43a026625537
dfilteau <dfilteau@hilltopz.com>
2022-02-11 16:56:52 -0800

    PSP Sprint 21
    First push

---
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S21_00/Build/51_DML_PIMS_ROLE.sql
@@ -0,0 +1,18 @@
+/* -----------------------------------------------------------------------------
+Delete all data from the PIMS_ROLE table and repopulate.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2021-Jul-16  Initial version
+----------------------------------------------------------------------------- */
+
+DELETE FROM PIMS_ROLE
+GO
+
+INSERT INTO PIMS_ROLE (ROLE_UID, NAME, DESCRIPTION, IS_PUBLIC, IS_DISABLED, SORT_ORDER, APP_CREATE_TIMESTAMP, APP_CREATE_USERID, APP_CREATE_USER_DIRECTORY, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_USER_DIRECTORY)
+VALUES
+  (NEWID(), N'System Administrator', N'System Administrator of the PIMS solution.', CONVERT([bit],(0)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
+  (NEWID(), N'Organization Administrator', N'Administrator of the user''s organization.', CONVERT([bit],(0)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
+  (NEWID(), N'Undetermined', N'The user has an undetermined role with their organization.', CONVERT([bit],(0)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
+  (NEWID(), N'Real Estate Manager', N'Real Estate Manager can manage properties within their agency.', CONVERT([bit],(1)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
+  (NEWID(), N'Real Estate Analyst', N'Real Estate Analyst can manage properties within their organization.', CONVERT([bit],(1)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data');
