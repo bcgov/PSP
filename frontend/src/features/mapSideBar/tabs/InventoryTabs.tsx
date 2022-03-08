637e73fcaa358ac748c6529a4e2efefaba5a92fe
Manuel Rodriguez <marobej@gmail.com>
2022-03-08 11:57:47 -0800

    Added common styles and a component that reprenesnts label and value pairs

--- frontend/src/features/mapSideBar/tabs/InventoryTabs.tsx
+++ frontend/src/features/mapSideBar/tabs/InventoryTabs.tsx
@@ -1,6 +1,9 @@
 import TabView from 'components/common/TabView';
 import * as React from 'react';
-import { Tab } from 'react-bootstrap';
+import { Col, Row, Tab } from 'react-bootstrap';
+
+import { SectionField } from './SectionField';
+import { StyledFormSection, StyledScrollable, StyledSectionHeader } from './SectionStyles';
 
 interface IInventoryTabsProps {
   PropertyForm: React.ReactNode;
@@ -10,17 +13,55 @@ export enum InventoryTabNames {
   property = 'property',
   title = 'title',
   value = 'value',
+  styles = 'styles',
 }
 /**
  * Tab wrapper, provides styling and nests form components within their corresponding tabs.
  * @param param0 object containing all react components for the corresponding tabs.
  */
 export const InventoryTabs: React.FunctionComponent<IInventoryTabsProps> = ({ PropertyForm }) => {
+  const longText =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor nisl at elit vestibulum vestibulum. Nullam eget consectetur felis, id porta eros. Proin at massa rutrum, molestie lorem a, congue lorem.';
   return (
     <TabView defaultActiveKey={InventoryTabNames.property}>
       <Tab eventKey={InventoryTabNames.title} title="Title"></Tab>
       <Tab eventKey={InventoryTabNames.value} title="Value"></Tab>
       <Tab eventKey={InventoryTabNames.property} title="Property Details"></Tab>
+      <Tab eventKey={InventoryTabNames.styles} title="STYLES">
+        <StyledScrollable>
+          <StyledFormSection>
+            <StyledSectionHeader>Section Header</StyledSectionHeader>
+            <SectionField label="Single line" content={['A test description']} />
+            <SectionField
+              label="Many lines"
+              content={['Up', 'Down', 'Left', 'Right']}
+              isMultiLine
+            />
+            <SectionField label="Multiple inline" content={['Yes', 'No', "I dont't know"]} />
+          </StyledFormSection>
+
+          <StyledFormSection>
+            <StyledSectionHeader>Split section header</StyledSectionHeader>
+            <Row>
+              <Col>
+                <SectionField label="A single label" content={['A test description']} />
+              </Col>
+              <Col>
+                <SectionField
+                  label="Many lines"
+                  content={['Up', 'Down', 'Left', 'Right']}
+                  isMultiLine
+                />
+                <SectionField label="Multiple inline" content={['Yes', 'No', "I dont't know"]} />
+              </Col>
+            </Row>
+          </StyledFormSection>
+          <StyledFormSection>
+            <StyledSectionHeader>Section Header</StyledSectionHeader>
+            {longText}
+          </StyledFormSection>
+        </StyledScrollable>
+      </Tab>
     </TabView>
   );
 };
---
+++ frontend/src/features/mapSideBar/tabs/SectionField.tsx
@@ -0,0 +1,38 @@
+import { Col, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+
+interface ISectionFieldProps {
+  label: string;
+  content: string[];
+  isMultiLine?: boolean;
+}
+
+export const SectionField: React.FunctionComponent<ISectionFieldProps> = props => {
+  console.log(props.content);
+  return (
+    <Row className="pb-2">
+      <Col xs="4">
+        <StyledFieldLabel>{props.label}:</StyledFieldLabel>
+      </Col>
+      <Col>
+        {props.content.map((item, index) =>
+          props.isMultiLine === true ? (
+            <Row key={`label ${props.label} ${index}`}>
+              <Col>
+                <span>{item}</span>
+              </Col>
+            </Row>
+          ) : (
+            <span key={`label ${props.label} ${index}`} className="mr-2">
+              {item}
+            </span>
+          ),
+        )}
+      </Col>
+    </Row>
+  );
+};
+
+const StyledFieldLabel = styled.div`
+  font-weight: bold;
+`;
