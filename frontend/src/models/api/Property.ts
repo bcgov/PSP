43edce971018a685309a2096e602b91469b3f34b
Alejandro Sanchez <emailforasr@gmail.com>
2022-03-17 17:26:24 -0700

    Update frontend models

---
+++ frontend/src/models/api/Property.ts
@@ -0,0 +1,36 @@
+import { Api_Address } from './Address';
+import { Api_ConcurrentVersion } from './ConcurrentVersion';
+import Api_TypeCode from './TypeCode';
+
+export interface Api_Property extends Api_ConcurrentVersion {
+  id: number;
+  pid?: string;
+  pin?: number;
+  zoning: string;
+  notes: string;
+
+  name: string;
+  description: string;
+  isSensitive: boolean;
+  isProvincialPublicHwy?: boolean;
+
+  landArea?: number;
+  areaUnit?: Api_TypeCode<string>;
+
+  isVolumetricParcel?: boolean;
+  volumetricMeasurement?: number;
+  volumetricUnit?: Api_TypeCode<string>;
+  volumetricType?: Api_TypeCode<string>;
+
+  anomalies?: Api_TypeCode<string>;
+  propertyType: Api_TypeCode<string>;
+  tenure: Api_TypeCode<string>;
+  roadType?: Api_TypeCode<string>;
+  adjacentLand?: Api_TypeCode<string>;
+  status?: Api_TypeCode<string>;
+
+  dataSource?: Api_TypeCode<string>;
+  dataSourceEffectiveDate?: string;
+
+  address: Api_Address;
+}
