aa5aac987f828915905260726f446303af53af9f
Alejandro Sanchez <emailforasr@gmail.com>
2022-03-17 17:10:28 -0700

    Add base address model

---
+++ backend/api/Models/Concepts/Address/AddressModel.cs
@@ -0,0 +1,57 @@
+namespace Pims.Api.Models.Concepts
+{
+    /// <summary>
+    /// Provides an address model.
+    /// Doesn't include the concept of address type (e.g. MAILING, BILLING etc).
+    /// For that you should use ContactAddressModel
+    /// </summary>
+    public class AddressModel : BaseAppModel
+    {
+        #region Properties
+        /// <summary>
+        /// get/set - The primary key to identify the address.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// get/set - The street address.
+        /// </summary>
+        public string StreetAddress1 { get; set; }
+
+        /// <summary>
+        /// get/set - The street address.
+        /// </summary>
+        public string StreetAddress2 { get; set; }
+
+        /// <summary>
+        /// get/set - The street address.
+        /// </summary>
+        public string StreetAddress3 { get; set; }
+
+        /// <summary>
+        /// get/set - The name of the municipality name.
+        /// </summary>
+        public string Municipality { get; set; }
+
+        /// <summary>
+        /// get/set - The address's province.
+        /// </summary>
+        public ProvinceStateModel Province { get; set; }
+
+        /// <summary>
+        /// get/set - The address's country.
+        /// </summary>
+        public CountryModel Country { get; set; }
+
+        /// <summary>
+        /// get/set - The free-form value of country when country code is "Other".
+        /// </summary>
+        public string CountryOther { get; set; }
+
+        /// <summary>
+        /// get/set - The postal code.
+        /// </summary>
+        public string Postal { get; set; }
+        #endregion
+    }
+}
