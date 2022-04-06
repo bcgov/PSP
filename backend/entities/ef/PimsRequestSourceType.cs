7d5cf9510a1d5d6940d3b78ceb062e87485735e3
Manuel Rodriguez <marobej@gmail.com>
2022-04-06 15:30:08 -0700

    Scaffold for s25 (#1200)

---
+++ backend/entities/ef/PimsRequestSourceType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Pims.Dal.Entities
+{
+    [Table("PIMS_REQUEST_SOURCE_TYPE")]
+    public partial class PimsRequestSourceType
+    {
+        public PimsRequestSourceType()
+        {
+            PimsResearchFiles = new HashSet<PimsResearchFile>();
+        }
+
+        [Key]
+        [Column("REQUEST_SOURCE_TYPE_CODE")]
+        [StringLength(20)]
+        public string RequestSourceTypeCode { get; set; }
+        [Required]
+        [Column("DESCRIPTION")]
+        [StringLength(200)]
+        public string Description { get; set; }
+        [Column("IS_DISABLED")]
+        public bool? IsDisabled { get; set; }
+        [Column("DISPLAY_ORDER")]
+        public int? DisplayOrder { get; set; }
+        [Column("CONCURRENCY_CONTROL_NUMBER")]
+        public long ConcurrencyControlNumber { get; set; }
+        [Column("DB_CREATE_TIMESTAMP", TypeName = "datetime")]
+        public DateTime DbCreateTimestamp { get; set; }
+        [Required]
+        [Column("DB_CREATE_USERID")]
+        [StringLength(30)]
+        public string DbCreateUserid { get; set; }
+        [Column("DB_LAST_UPDATE_TIMESTAMP", TypeName = "datetime")]
+        public DateTime DbLastUpdateTimestamp { get; set; }
+        [Required]
+        [Column("DB_LAST_UPDATE_USERID")]
+        [StringLength(30)]
+        public string DbLastUpdateUserid { get; set; }
+
+        [InverseProperty(nameof(PimsResearchFile.RequestSourceTypeCodeNavigation))]
+        public virtual ICollection<PimsResearchFile> PimsResearchFiles { get; set; }
+    }
+}
--- backend/entities/ef/PimsResearchFilePurpose.cs
+++ backend/entities/ef/PimsResearchFilePurpose.cs
@@ -8,51 +8,47 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Pims.Dal.Entities
 {
-    [Table("PIMS_PROPERTY_RESEARCH_FILE")]
-    [Index(nameof(PropertyId), Name = "PRSCRC_PROPERTY_ID_IDX")]
-    [Index(nameof(ResearchFileId), Name = "PRSCRC_RESEARCH_FILE_ID_IDX")]
-    public partial class PimsPropertyResearchFile
+    [Table("PIMS_RESEARCH_FILE_PURPOSE")]
+    [Index(nameof(ResearchFileId), Name = "RSFLPR_RESEARCH_FILE_ID_IDX")]
+    [Index(nameof(ResearchPurposeTypeCode), Name = "RSFLPR_RESEARCH_PURPOSE_TYPE_CODE_IDX")]
+    public partial class PimsResearchFilePurpose
     {
         [Key]
-        [Column("PROPERTY_RESEARCH_FILE_ID")]
-        public long PropertyResearchFileId { get; set; }
-        [Column("PROPERTY_ID")]
-        public long PropertyId { get; set; }
+        [Column("RESEARCH_FILE_PURPOSE_ID")]
+        [StringLength(40)]
+        public string ResearchFilePurposeId { get; set; }
         [Column("RESEARCH_FILE_ID")]
         public long ResearchFileId { get; set; }
-        [Column("PROPERTY_NAME")]
-        [StringLength(500)]
-        public string PropertyName { get; set; }
-        [Column("DISPLAY_ORDER")]
-        public int? DisplayOrder { get; set; }
-        [Column("IS_DISABLED")]
-        public bool? IsDisabled { get; set; }
+        [Required]
+        [Column("RESEARCH_PURPOSE_TYPE_CODE")]
+        [StringLength(20)]
+        public string ResearchPurposeTypeCode { get; set; }
+        [Column("CONCURRENCY_CONTROL_NUMBER")]
+        public long ConcurrencyControlNumber { get; set; }
         [Column("APP_CREATE_TIMESTAMP", TypeName = "datetime")]
         public DateTime AppCreateTimestamp { get; set; }
-        [Required]
-        [Column("APP_CREATE_USER_DIRECTORY")]
-        [StringLength(30)]
-        public string AppCreateUserDirectory { get; set; }
-        [Column("APP_CREATE_USER_GUID")]
-        public Guid? AppCreateUserGuid { get; set; }
         [Required]
         [Column("APP_CREATE_USERID")]
         [StringLength(30)]
         public string AppCreateUserid { get; set; }
+        [Column("APP_CREATE_USER_GUID")]
+        public Guid? AppCreateUserGuid { get; set; }
+        [Required]
+        [Column("APP_CREATE_USER_DIRECTORY")]
+        [StringLength(30)]
+        public string AppCreateUserDirectory { get; set; }
         [Column("APP_LAST_UPDATE_TIMESTAMP", TypeName = "datetime")]
         public DateTime AppLastUpdateTimestamp { get; set; }
-        [Required]
-        [Column("APP_LAST_UPDATE_USER_DIRECTORY")]
-        [StringLength(30)]
-        public string AppLastUpdateUserDirectory { get; set; }
-        [Column("APP_LAST_UPDATE_USER_GUID")]
-        public Guid? AppLastUpdateUserGuid { get; set; }
         [Required]
         [Column("APP_LAST_UPDATE_USERID")]
         [StringLength(30)]
         public string AppLastUpdateUserid { get; set; }
-        [Column("CONCURRENCY_CONTROL_NUMBER")]
-        public long ConcurrencyControlNumber { get; set; }
+        [Column("APP_LAST_UPDATE_USER_GUID")]
+        public Guid? AppLastUpdateUserGuid { get; set; }
+        [Required]
+        [Column("APP_LAST_UPDATE_USER_DIRECTORY")]
+        [StringLength(30)]
+        public string AppLastUpdateUserDirectory { get; set; }
         [Column("DB_CREATE_TIMESTAMP", TypeName = "datetime")]
         public DateTime DbCreateTimestamp { get; set; }
         [Required]
@@ -66,11 +62,11 @@ namespace Pims.Dal.Entities
         [StringLength(30)]
         public string DbLastUpdateUserid { get; set; }
 
-        [ForeignKey(nameof(PropertyId))]
-        [InverseProperty(nameof(PimsProperty.PimsPropertyResearchFiles))]
-        public virtual PimsProperty Property { get; set; }
         [ForeignKey(nameof(ResearchFileId))]
-        [InverseProperty(nameof(PimsResearchFile.PimsPropertyResearchFiles))]
+        [InverseProperty(nameof(PimsResearchFile.PimsResearchFilePurposes))]
         public virtual PimsResearchFile ResearchFile { get; set; }
+        [ForeignKey(nameof(ResearchPurposeTypeCode))]
+        [InverseProperty(nameof(PimsResearchPurposeType.PimsResearchFilePurposes))]
+        public virtual PimsResearchPurposeType ResearchPurposeTypeCodeNavigation { get; set; }
     }
 }
