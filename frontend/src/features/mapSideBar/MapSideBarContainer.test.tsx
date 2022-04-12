3d52a1d2fe970038acf36b7752a6ca90ef50eaf7
Alejandro Sanchez <emailforasr@gmail.com>
2022-04-12 16:30:21 -0700

    Unit tests

--- frontend/src/features/mapSideBar/MapSideBarContainer.test.tsx
+++ frontend/src/features/mapSideBar/MapSideBarContainer.test.tsx
@@ -4,20 +4,33 @@ import { createMemoryHistory } from 'history';
 import { cleanup, render, RenderOptions, waitFor } from 'utils/test-utils';
 
 import MapSideBarContainer from './MapSideBarContainer';
+
+// Need to mock this library for unit tests
+jest.mock('react-visibility-sensor', () => {
+  return jest.fn().mockImplementation(({ children }) => {
+    if (children instanceof Function) {
+      return children({ isVisible: true });
+    }
+    return children;
+  });
+});
+
 describe('MapSideBarContainer component', () => {
   const mockAxios = new MockAdapter(axios);
   const history = createMemoryHistory();
-  const setup = (renderOptions: RenderOptions = {}) => {
-    // render component under test
-    const component = render(<MapSideBarContainer />, {
-      ...renderOptions,
-      history,
-    });
 
-    return {
-      component,
-    };
+  // render component under test
+  const setup = (renderOptions: RenderOptions = {}) => {
+    const renderResult = render(<MapSideBarContainer />, { ...renderOptions, history });
+    return { ...renderResult };
   };
+
+  afterEach(() => {
+    mockAxios.reset();
+    jest.restoreAllMocks();
+    cleanup();
+    history.push('');
+  });
 
   it('requests ltsa data by pid', async () => {
     history.push('mapview?pid=9212434&searchBy=pinOrPid&sidebar=true');
@@ -29,10 +42,35 @@ describe('MapSideBarContainer component', () => {
       expect(mockAxios.history.post[0].url).toBe(`/tools/ltsa/all?pid=009-212-434`);
     });
   });
-  afterEach(() => {
-    mockAxios.reset();
-    jest.restoreAllMocks();
-    cleanup();
-    history.push('');
+
+  it('shows the property information tab for inventory properties', async () => {
+    history.push('mapview?pid=9212434&searchBy=pinOrPid&sidebar=true');
+    mockAxios.onPost().reply(200, {});
+    mockAxios.onGet(new RegExp('/properties/*')).reply(200, {});
+    const { getByText } = setup({});
+    await waitFor(() => {
+      expect(mockAxios.history.get.length).toBeGreaterThanOrEqual(1);
+      expect(mockAxios.history.get[0].url).toBe(`/properties/009-212-434`);
+    });
+    expect(getByText('Property attributes')).toBeVisible();
+  });
+
+  it('hides the property information tab for non-inventory properties', async () => {
+    history.push('mapview?pid=9212434&searchBy=pinOrPid&sidebar=true');
+    mockAxios.onPost().reply(200, {});
+    // non-inventory properties return a "not-found" error from API
+    const error = {
+      isAxiosError: true,
+      response: { status: 400 },
+    };
+    mockAxios.onGet(new RegExp('/properties/*')).reply(400, error);
+    const { queryByText } = setup({});
+    await waitFor(() => {
+      expect(mockAxios.history.post).toHaveLength(1);
+      expect(mockAxios.history.post[0].url).toBe(`/tools/ltsa/all?pid=009-212-434`);
+      expect(mockAxios.history.get.length).toBeGreaterThanOrEqual(1);
+      expect(mockAxios.history.get[0].url).toBe(`/properties/009-212-434`);
+    });
+    expect(queryByText('Property attributes')).toBeNull();
   });
 });
