c5c587a2ff8a7937af02d1f2da8c5c41a010d676
Mahesh Babu <kvmb1988@gmail.com>
2022-04-21 19:10:47 -0400

    PSP-3225:Make ltsa charges collapseable

--- frontend/src/features/mapSideBar/tabs/ltsa/LtsaChargeSubForm.tsx
+++ frontend/src/features/mapSideBar/tabs/ltsa/LtsaChargeSubForm.tsx
@@ -2,10 +2,11 @@ import { Input } from 'components/common/form';
 import { FieldArray, getIn, useFormikContext } from 'formik';
 import { ChargeOnTitle, LtsaOrders } from 'interfaces/ltsaModels';
 import * as React from 'react';
+import Collapse from 'react-bootstrap/Collapse';
 import { withNameSpace } from 'utils/formUtils';
 
 import { SectionField } from '../SectionField';
-import { StyledSectionHeader } from '../SectionStyles';
+import { ArrowDropDownIcon, ArrowDropUpIcon, StyledSectionHeader } from '../SectionStyles';
 import LtsaChargeOwnerSubForm from './LtsaChargeOwnerSubForm';
 
 export interface ILtsaChargeSubFormProps {
@@ -17,35 +18,59 @@ export const LtsaChargeSubForm: React.FunctionComponent<ILtsaChargeSubFormProps>
 }) => {
   const { values } = useFormikContext<LtsaOrders>();
   const charges = getIn(values, withNameSpace(nameSpace, 'chargesOnTitle')) ?? [];
+  const [isExpanded, setIsExpanded] = React.useState<boolean>(true);
+
   return (
     <React.Fragment key={`charge-sub-row-${nameSpace}`}>
-      <StyledSectionHeader>Charges, Liens and Interests</StyledSectionHeader>
-      {charges.length === 0 && 'this title has no charges'}
-      <FieldArray
-        name={withNameSpace(nameSpace, 'chargesOnTitle')}
-        render={({ push, remove, name }) => (
-          <React.Fragment key={`charge-sub-row-${nameSpace}`}>
-            {charges.map((charge: ChargeOnTitle, index: number) => {
-              const innerNameSpace = withNameSpace(nameSpace, `chargesOnTitle.${index}.charge`);
-              return (
-                <React.Fragment key={`charge-sub-row-${innerNameSpace}`}>
-                  <SectionField label="Nature">
-                    <Input field={`${withNameSpace(innerNameSpace, 'transactionType')}`} />
-                  </SectionField>
-                  <SectionField label="Registration #">
-                    <Input field={`${withNameSpace(innerNameSpace, 'chargeNumber')}`} />
-                  </SectionField>
-                  <SectionField label="Registered date">
-                    <Input field={`${withNameSpace(innerNameSpace, 'applicationReceivedDate')}`} />
-                  </SectionField>
-                  <LtsaChargeOwnerSubForm nameSpace={innerNameSpace} />
-                  {index < charges.length - 1 && <hr></hr>}
-                </React.Fragment>
-              );
-            })}
-          </React.Fragment>
+      <StyledSectionHeader>
+        Charges, Liens and Interests
+        {!isExpanded && (
+          <ArrowDropDownIcon
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          />
         )}
-      />
+        {isExpanded && (
+          <ArrowDropUpIcon
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          />
+        )}
+      </StyledSectionHeader>
+      <Collapse in={isExpanded}>
+        <div>
+          {charges.length === 0 && 'this title has no charges'}
+          <FieldArray
+            name={withNameSpace(nameSpace, 'chargesOnTitle')}
+            render={({ push, remove, name }) => (
+              <React.Fragment key={`charge-sub-row-${nameSpace}`}>
+                {charges.map((charge: ChargeOnTitle, index: number) => {
+                  const innerNameSpace = withNameSpace(nameSpace, `chargesOnTitle.${index}.charge`);
+                  return (
+                    <React.Fragment key={`charge-sub-row-${innerNameSpace}`}>
+                      <SectionField label="Nature">
+                        <Input field={`${withNameSpace(innerNameSpace, 'transactionType')}`} />
+                      </SectionField>
+                      <SectionField label="Registration #">
+                        <Input field={`${withNameSpace(innerNameSpace, 'chargeNumber')}`} />
+                      </SectionField>
+                      <SectionField label="Registered date">
+                        <Input
+                          field={`${withNameSpace(innerNameSpace, 'applicationReceivedDate')}`}
+                        />
+                      </SectionField>
+                      <LtsaChargeOwnerSubForm nameSpace={innerNameSpace} />
+                      {index < charges.length - 1 && <hr></hr>}
+                    </React.Fragment>
+                  );
+                })}
+              </React.Fragment>
+            )}
+          />
+        </div>
+      </Collapse>
     </React.Fragment>
   );
 };
