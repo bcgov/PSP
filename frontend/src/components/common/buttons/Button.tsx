9b5bb8a0990f8f617248d2c18372a31131c792ea
Alejandro Sanchez <emailforasr@gmail.com>
2022-04-22 21:36:27 -0700

    Move styling from global css to Button component

--- frontend/src/components/common/buttons/Button.tsx
+++ frontend/src/components/common/buttons/Button.tsx
@@ -1,9 +1,8 @@
-import './Button.scss';
-
 import classnames from 'classnames';
 import React, { CSSProperties, MouseEventHandler } from 'react';
-import ButtonBase, { ButtonProps as ButtonPropsBase } from 'react-bootstrap/Button';
+import BootstrapButton, { ButtonProps as ButtonPropsBase } from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
+import styled from 'styled-components';
 
 export type ButtonProps = ButtonPropsBase & {
   /** Adds a custom class to the button element of the <Button> component */
@@ -42,7 +41,7 @@ export const Button: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> =
     [className!]: className,
   });
   return (
-    <ButtonBase className={classes} disabled={disabled} {...rest}>
+    <StyledButton className={classes} disabled={disabled} {...rest}>
       {icon && <div className="Button__icon">{icon}</div>}
       {children && <div className="Button__value">{children}</div>}
       {showSubmitting && isSubmitting && (
@@ -54,6 +53,178 @@ export const Button: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> =
           style={{ marginLeft: '.8rem', padding: '.8rem' }}
         />
       )}
-    </ButtonBase>
+    </StyledButton>
   );
 };
+
+const StyledButton = styled(BootstrapButton)`
+  &.btn {
+    // common styling for all buttons
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.4rem 1.2rem;
+    min-height: 3rem;
+    border: 0.2rem solid transparent;
+    border-radius: 0.4rem;
+    text-align: center;
+    text-decoration: none;
+    font-size: 1.8rem;
+    font-family: 'BCSans', 'Noto Sans', Verdana, Arial, sans-serif;
+    font-weight: 700;
+    letter-spacing: 0.1rem;
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+      opacity: 0.8;
+    }
+
+    &:focus {
+      outline-width: 0.4rem;
+      outline-style: solid;
+      outline-color: ${({ theme }) => theme.css.buttonOutlineColor};
+      outline-offset: 1px;
+      box-shadow: none;
+    }
+
+    // PRIMARY buttons
+    &.btn-primary {
+      color: ${({ theme }) => theme.css.primaryBackgroundColor};
+      background-color: ${({ theme }) => theme.css.primaryColor};
+      border: none;
+    }
+
+    // SECONDARY buttons
+    &.btn-secondary {
+      color: ${({ theme }) => theme.css.primaryColor};
+      background: none;
+      border-color: ${({ theme }) => theme.css.primaryColor};
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.primaryBackgroundColor};
+        background-color: ${({ theme }) => theme.css.primaryColor};
+      }
+    }
+
+    // INFO buttons
+    &.btn-info {
+      color: ${({ theme }) => theme.css.buttonInfoColor};
+      border: none;
+      background: none;
+      padding-left: 0.6rem;
+      padding-right: 0.6rem;
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.sresIconColor};
+        background: none;
+      }
+    }
+
+    // LIGHT buttons
+    &.btn-light {
+      color: ${({ theme }) => theme.css.primaryBackgroundColor};
+      background-color: ${({ theme }) => theme.css.lightVariantColor};
+      border: none;
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.primaryBackgroundColor};
+        background-color: ${({ theme }) => theme.css.lightVariantColor};
+      }
+    }
+
+    // DARK buttons
+    &.btn-dark {
+      color: ${({ theme }) => theme.css.primaryBackgroundColor};
+      background-color: ${({ theme }) => theme.css.darkVariantColor};
+      border: none;
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.primaryBackgroundColor};
+        background-color: ${({ theme }) => theme.css.darkVariantColor};
+      }
+    }
+
+    // DANGER buttons
+    &.btn-danger {
+      color: ${({ theme }) => theme.css.primaryBackgroundColor};
+      background-color: ${({ theme }) => theme.css.dangerColor};
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.primaryBackgroundColor};
+        background-color: ${({ theme }) => theme.css.dangerColor};
+      }
+    }
+
+    // WARNING buttons
+    &.btn-warning {
+      color: ${({ theme }) => theme.css.primaryBackgroundColor};
+      background-color: ${({ theme }) => theme.css.accentColor};
+      border-color: ${({ theme }) => theme.css.accentColor};
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.primaryBackgroundColor};
+        border-color: ${({ theme }) => theme.css.accentColor};
+        background-color: ${({ theme }) => theme.css.accentColor};
+      }
+    }
+
+    // LINK buttons
+    &.btn-link {
+      font-size: 1.6rem;
+      font-weight: 400;
+      color: ${({ theme }) => theme.css.linkColor};
+      background: none;
+      border: none;
+      text-decoration: none;
+      min-height: 2.5rem;
+      line-height: 3rem;
+      justify-content: left;
+      letter-spacing: unset;
+      padding: 0;
+      &:hover,
+      &:active,
+      &:focus {
+        color: ${({ theme }) => theme.css.linkHoverColor};
+        text-decoration: underline;
+        border: none;
+        background: none;
+        box-shadow: none;
+      }
+
+      &:disabled,
+      &.disabled {
+        color: ${({ theme }) => theme.css.disabledColor};
+        background: none;
+        pointer-events: none;
+      }
+    }
+
+    // DISABLED buttons -- applies to all buttons (primary, secondary, etc)
+    &:disabled,
+    &:disabled:hover {
+      box-shadow: none;
+      user-select: none;
+      pointer-events: none;
+      cursor: not-allowed;
+      opacity: 0.65;
+    }
+  }
+
+  &.Button {
+    .Button__icon {
+      margin-right: 1.6rem;
+    }
+    &--icon-only {
+      .Button__icon {
+        margin-right: 0;
+      }
+    }
+  }
+`;
