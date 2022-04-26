cdaf84127cc0b1048c9a831ab26a9524d8b1ada2
Devin Smith <devinleighsmith@gmail.com>
2022-04-26 11:07:12 -0700

    correct issue with pims_property sequence not being set by paims etl.

--- frontend/src/features/properties/map/research/add/AddResearchFileYupSchema.ts
+++ frontend/src/features/properties/map/research/add/AddResearchFileYupSchema.ts
@@ -2,13 +2,5 @@ import * as Yup from 'yup';
 
 export const AddResearchFileYupSchema = Yup.object().shape({
   name: Yup.string().required('Research File name is required'),
-  properties: Yup.array().of(
-    Yup.object().shape({
-      pid: Yup.string().when('pin', {
-        is: (pin: string) => !!pin,
-        then: Yup.string().nullable(),
-        otherwise: Yup.string().required('valid PID or PIN required'),
-      }),
-    }),
-  ),
+  properties: Yup.array(),
 });
