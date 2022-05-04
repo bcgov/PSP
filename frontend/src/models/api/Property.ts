a8d031e037bf78fe1b43889ca0ec578355163349
Alejandro Sanchez <emailforasr@gmail.com>
2022-05-04 16:45:01 -0700

    Update API_Property model

--- frontend/src/models/api/Property.ts
+++ frontend/src/models/api/Property.ts
@@ -14,38 +14,60 @@ export interface Api_Geometry {
 
 export interface Api_Property extends Api_ConcurrentVersion {
   id?: number;
-  pid?: number;
-  pin?: number;
-  status?: string;
-  dataSource?: string;
-  dataSourceEffectiveDate?: string;
-  classification?: string;
-  tenure?: Api_TypeCode<string>;
-  name?: string;
-  description?: string;
-  address?: Api_Address;
+  propertyType?: Api_TypeCode<string>;
+
+  // multi-selects
+  anomalies?: Api_TypeCode<string>[];
+  tenure?: Api_TypeCode<string>[];
+  roadType?: Api_TypeCode<string>[];
+  adjacentLand?: Api_TypeCode<string>[];
+
+  status?: Api_TypeCode<string>;
   region?: Api_TypeCode<number>;
   district?: Api_TypeCode<string>;
-  location?: Api_Geometry;
-  planNumber?: string;
+
+  dataSource?: Api_TypeCode<string>;
+  dataSourceEffectiveDate?: string;
 
   latitude?: number;
   longitude?: number;
+  location?: Api_Geometry;
 
-  landArea?: number;
-  landLegalDescription?: string;
-  encumbranceReason?: string;
+  name?: string;
+  description?: string;
+  pid?: number;
+  pin?: number;
+  planNumber?: string;
+
+  address?: Api_Address;
+
   isSensitive?: boolean;
+  isVisibleToOtherAgencies?: boolean;
   isOwned?: boolean;
   isPropertyOfInterest?: boolean;
-  isVisibleToOtherAgencies?: boolean;
+  isProvincialPublicHwy?: boolean;
+
+  landArea?: number;
+  areaUnit?: Api_TypeCode<string>;
+  landLegalDescription?: string;
+
+  isVolumetricParcel?: boolean;
+  volumetricMeasurement?: number;
+  volumetricUnit?: Api_TypeCode<string>;
+  volumetricType?: Api_TypeCode<string>;
+
+  encumbranceReason?: string;
+
   zoning?: string;
   zoningPotential?: string;
+  municipalZoning?: string;
 
   appCreateTimestamp?: string;
   updatedOn?: string;
   updatedByEmail?: string;
   updatedByName?: string;
+
+  notes?: string;
 }
 
 export interface Api_PropertyAssociations {
