68f343cc501544b8d6e9795fb17f32c208a21b9f
Devin Smith <devinleighsmith@gmail.com>
2022-06-01 23:56:21 -0700

    test corrections.

--- backend/api/Models/Concepts/User/UserRoleMap.cs
+++ backend/api/Models/Concepts/User/UserRoleMap.cs
@@ -8,6 +8,7 @@ namespace Pims.Api.Models.Concepts.AccessRequest
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<Entity.PimsUserRole, UserRoleModel>()
+                .PreserveReference(true)
                 .Map(dest => dest.UserId, src => src.UserId)
                 .Map(dest => dest.RoleId, src => src.RoleId)
                 .Map(dest => dest.Role, src => src.Role)
@@ -15,6 +16,7 @@ namespace Pims.Api.Models.Concepts.AccessRequest
                 .Inherits<Entity.IBaseAppEntity, Api.Models.BaseAppModel>();
 
             config.NewConfig<UserRoleModel, Entity.PimsUserRole>()
+                .PreserveReference(true)
                 .Map(dest => dest.UserId, src => src.UserId)
                 .Map(dest => dest.RoleId, src => src.RoleId)
                 .Map(dest => dest.Role, src => src.Role)
