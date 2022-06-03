7fb0df9b09c5720d07a5babdd11b0e18ac4cf1aa
Devin Smith <devinleighsmith@gmail.com>
2022-06-03 12:56:36 -0700

    system admin role should not be disabled.

--- database/mssql/scripts/dbscripts/PSP_PIMS_S29_00/Build/51_DML_PIMS_ROLE.sql
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S29_00/Build/51_DML_PIMS_ROLE.sql
@@ -3,7 +3,7 @@ GO
 
 INSERT INTO PIMS_ROLE (ROLE_UID, NAME, DESCRIPTION, IS_PUBLIC, IS_DISABLED, SORT_ORDER, APP_CREATE_TIMESTAMP, APP_CREATE_USERID, APP_CREATE_USER_DIRECTORY, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_USER_DIRECTORY)
 VALUES
-  (NEWID(), N'System Administrator', N'System Administrator of the PIMS solution.', CONVERT([bit],(0)), CONVERT([bit],(1)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
+  (NEWID(), N'System Administrator', N'System Administrator of the PIMS solution.', CONVERT([bit],(0)), CONVERT([bit],(0)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
   (NEWID(), N'Organization Administrator', N'Administrator of the user''s organization.', CONVERT([bit],(0)), CONVERT([bit],(1)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
   (NEWID(), N'Undetermined', N'The user has an undetermined role with their organization.', CONVERT([bit],(0)), CONVERT([bit],(1)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
   (NEWID(), N'Real Estate Manager', N'Real Estate Manager can manage properties within their agency.', CONVERT([bit],(1)), CONVERT([bit],(1)), 0, CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data', CURRENT_TIMESTAMP, N'Seed Data', N'Seed Data'),
