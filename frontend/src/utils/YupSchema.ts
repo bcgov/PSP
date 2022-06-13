b3b75941ab89b6a8512791ac797625b5deccd7fe
Mahesh Babu <kvmb1988@gmail.com>
2022-06-13 13:19:50 -0400

    feature(PSP-3663): Enforce character limits on FE - User Management

--- frontend/src/utils/YupSchema.ts
+++ frontend/src/utils/YupSchema.ts
@@ -18,7 +18,7 @@ export const AccessRequestSchema = Yup.object().shape({
   regionCodeId: Yup.number()
     .min(0, 'Invalid Region')
     .required('Required'),
-  note: Yup.string().max(1000, 'Note must be less than 1000 characters'),
+  note: Yup.string().max(4000, 'Note must be less than 4000 characters'),
   user: Yup.object().shape({
     position: Yup.string().max(100, 'Note must be less than 100 characters'),
   }),
@@ -29,8 +29,10 @@ export const UserUpdateSchema = Yup.object().shape({
     .email()
     .max(100, 'Email must be less than 100 characters'),
   firstName: Yup.string().max(100, 'First Name must be less than 100 characters'),
-  middleName: Yup.string().max(100, 'Middle Name must be less than 100 characters'),
+  middleName: Yup.string().max(400, 'Middle Name must be less than 400 characters'),
   surname: Yup.string().max(100, 'Last Name must be less than 100 characters'),
+  note: Yup.string().max(4000, 'Note must be less than 4000 characters'),
+  position: Yup.string().max(100, 'Note must be less than 100 characters'),
 });
 
 export const FilterBarSchema = Yup.object().shape({
