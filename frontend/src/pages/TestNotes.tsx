12dec1b824ebc74dc19bc5d1cbfb2ed6d0ee7ce1
Alejandro Sanchez <emailforasr@gmail.com>
2022-06-27 17:37:00 -0700

    Create test page to open notes modal

---
+++ frontend/src/pages/TestNotes.tsx
@@ -0,0 +1,22 @@
+import { Button } from 'components/common/buttons';
+import { AddNotesContainer } from 'features/notes/add/AddNotesContainer';
+import { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+export const TestNotes: React.FC = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Notes test page</h1>
+        </Col>
+      </Row>
+      <Row className="py-5">
+        <Button onClick={() => setShow(true)}>Add a Note</Button>
+        <AddNotesContainer showNotes={show} setShowNotes={setShow} />
+      </Row>
+    </Container>
+  );
+};
