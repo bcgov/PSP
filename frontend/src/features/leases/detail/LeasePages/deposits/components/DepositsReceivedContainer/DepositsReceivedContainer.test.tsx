e74fa9c80dbdc0b8e7a90e9377ad0526bebe7f21
Devin Smith <devinleighsmith@gmail.com>
2022-06-29 13:16:41 -0700

    lint corrections.

--- frontend/src/features/leases/detail/LeasePages/deposits/components/DepositsReceivedContainer/DepositsReceivedContainer.test.tsx
+++ frontend/src/features/leases/detail/LeasePages/deposits/components/DepositsReceivedContainer/DepositsReceivedContainer.test.tsx
@@ -1,14 +1,7 @@
-import { useKeycloak } from '@react-keycloak/web';
 import Claims from 'constants/claims';
 import { Api_SecurityDeposit } from 'models/api/SecurityDeposit';
 import { formatMoney, prettyFormatDate } from 'utils';
-import {
-  getAllByRole as getAllByRoleBase,
-  getAllByTitle,
-  prettyDOM,
-  render,
-  RenderOptions,
-} from 'utils/test-utils';
+import { getAllByRole as getAllByRoleBase, render, RenderOptions } from 'utils/test-utils';
 
 import DepositsReceivedContainer, {
   IDepositsReceivedContainerProps,
