b4bad290854c64e611ba52986b0ff466de6453fd
Alejandro Sanchez <emailforasr@gmail.com>
2022-06-30 18:07:23 -0700

    Bug fixes:  PSP-3869, PSP-3895

--- frontend/src/features/properties/map/research/add/AddResearchFileYupSchema.ts
+++ frontend/src/features/properties/map/research/add/AddResearchFileYupSchema.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const AddResearchFileYupSchema = Yup.object().shape({
   name: Yup.string()
     .required('Research File name is required')
-    .max(500, 'Research File name must be less than 500 characters'),
+    .max(250, 'Research File name must be less than 250 characters'),
   properties: Yup.array().of(
     Yup.object().shape({
       name: Yup.string().max(250, 'Property name must be less than 250 characters'),
