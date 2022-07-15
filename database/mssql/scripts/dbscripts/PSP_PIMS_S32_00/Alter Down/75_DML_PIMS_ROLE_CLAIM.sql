3895d8b0fbf42bf17230ec8faa27eab6b03e89e9
Alejandro Sanchez <emailforasr@gmail.com>
2022-07-15 15:55:01 -0700

    Add new note claims to DB scripts

---
+++ database/mssql/scripts/dbscripts/PSP_PIMS_S32_00/Alter Down/75_DML_PIMS_ROLE_CLAIM.sql
@@ -0,0 +1,42 @@
+DECLARE @finance BIGINT;
+DECLARE @functional BIGINT;
+DECLARE @restricted BIGINT;
+DECLARE @readOnly BIGINT;
+DECLARE @systemAdministrator BIGINT;
+SELECT @finance = ROLE_ID FROM PIMS_ROLE WHERE NAME = 'Finance';
+SELECT @functional = ROLE_ID FROM PIMS_ROLE WHERE NAME = 'Functional';
+SELECT @restricted = ROLE_ID FROM PIMS_ROLE WHERE NAME = 'Functional (Restricted)';
+SELECT @readOnly = ROLE_ID FROM PIMS_ROLE WHERE NAME = 'Read Only';
+SELECT @systemAdministrator = ROLE_ID FROM PIMS_ROLE WHERE NAME = 'System Administrator';
+
+DECLARE @noteView BIGINT;
+DECLARE @noteAdd BIGINT;
+DECLARE @noteEdit BIGINT;
+DECLARE @noteDelete BIGINT;
+SELECT @noteView = CLAIM_ID FROM PIMS_CLAIM WHERE NAME = 'note-view';
+SELECT @noteAdd = CLAIM_ID FROM PIMS_CLAIM WHERE NAME = 'note-add';
+SELECT @noteEdit = CLAIM_ID FROM PIMS_CLAIM WHERE NAME = 'note-edit';
+SELECT @noteDelete = CLAIM_ID FROM PIMS_CLAIM WHERE NAME = 'note-delete';
+
+/* Functional */
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteView AND ROLE_ID = @functional;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteEdit AND ROLE_ID = @functional;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteAdd AND ROLE_ID = @functional;
+GO
+
+/* Functional Restricted */
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteView AND ROLE_ID = @restricted;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteEdit AND ROLE_ID = @restricted;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteAdd AND ROLE_ID = @restricted;
+GO
+
+/* Read Only */
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteView AND ROLE_ID = @readOnly;
+GO
+
+/* System Administrator */
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteView AND ROLE_ID = @systemAdministrator;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteEdit AND ROLE_ID = @systemAdministrator;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteAdd AND ROLE_ID = @systemAdministrator;
+DELETE FROM [dbo].[PIMS_ROLE_CLAIM] WHERE CLAIM_ID = @noteDelete AND ROLE_ID = @systemAdministrator;
+GO
