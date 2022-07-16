e89f5c720ea27bc65e3d285c8bf850c56aae5dea
Alejandro Sanchez <emailforasr@gmail.com>
2022-07-16 16:35:48 -0700

    Allow hiding footer on modals

--- frontend/src/components/common/GenericModal.tsx
+++ frontend/src/components/common/GenericModal.tsx
@@ -75,6 +75,9 @@ export interface ModalProps {
   /** display this modal as a popup instead of as a modal, allowing the user to click on underlying elements */
   asPopup?: boolean;
   show?: boolean;
+  /** Whether to render the modal footer with Ok, Cancel buttons
+   * Default behaviour is to show the footer */
+  showFooter?: boolean;
 }
 
 /**
@@ -94,6 +97,7 @@ export const GenericModal = (props: BsModalProps & ModalProps) => {
     cancelButtonVariant,
     cancelButtonText,
     closeButton,
+    showFooter = true,
     ...rest
   } = props;
   const [show, setShow] = useState(true);
@@ -133,20 +137,22 @@ export const GenericModal = (props: BsModalProps & ModalProps) => {
 
       <Modal.Body>{message}</Modal.Body>
 
-      <Modal.Footer>
-        {cancelButtonText && (
-          <Button
-            variant={cancelButtonVariant ?? 'secondary'}
-            onClick={close}
-            style={{ width: 'unset' }}
-          >
-            {cancelButtonText}
+      {showFooter && (
+        <Modal.Footer>
+          {cancelButtonText && (
+            <Button
+              variant={cancelButtonVariant ?? 'secondary'}
+              onClick={close}
+              style={{ width: 'unset' }}
+            >
+              {cancelButtonText}
+            </Button>
+          )}
+          <Button variant={okButtonVariant ?? 'primary'} onClick={ok}>
+            {okButtonText ?? 'Ok'}
           </Button>
-        )}
-        <Button variant={okButtonVariant ?? 'primary'} onClick={ok}>
-          {okButtonText ?? 'Ok'}
-        </Button>
-      </Modal.Footer>
+        </Modal.Footer>
+      )}
     </ModalContainer>
   );
 };
