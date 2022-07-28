0e938f8d637019c35878e4d338b8e0bc78a7bf7c
Alejandro Sanchez <emailforasr@gmail.com>
2022-07-27 17:00:47 -0700

    Form hook & validation schema

---
+++ frontend/src/features/properties/map/acquisition/add/AddAcquisitionFileYupSchema.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-template-curly-in-string */
+import * as Yup from 'yup';
+
+export const AddAcquisitionFileYupSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Acquisition file name is required')
+    .max(500, 'Acquisition file name must be at most ${max} characters'),
+  regionId: Yup.string().required('Ministry region is required'),
+});
