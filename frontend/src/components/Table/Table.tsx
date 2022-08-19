3cf269626ce19d999c38d97d216876f70647798b
Devin Smith <devinleighsmith@gmail.com>
2022-08-19 11:32:43 -0700

    setExternalRows duplicate condition in conditional statement.

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -379,7 +379,6 @@ export const Table = <T extends IIdentifiedObject, TFilter extends object = {}>(
                             );
                         } else {
                           setExternalSelectedRows &&
-                            setExternalSelectedRows &&
                             setExternalSelectedRows([...allPreviouslySelected, ...values]);
                         }
                       }}
