ec44b3e9ac01501f5faba04cd6e0a130711e9257
Mahesh Babu <kvmb1988@gmail.com>
2022-09-07 17:30:11 -0400

    PSP-3620: ACQ - Adding Acquisition staff

--- frontend/src/features/properties/map/acquisition/add/AddAcquisitionFileYupSchema.ts
+++ frontend/src/features/properties/map/acquisition/add/AddAcquisitionFileYupSchema.ts
@@ -8,25 +8,14 @@ export const AddAcquisitionFileYupSchema = Yup.object().shape({
   acquisitionType: Yup.string().required('Acquisition type is required'),
   region: Yup.string().required('Ministry region is required'),
   team: Yup.array().of(
-    Yup.object().shape(
-      {
-        contact: Yup.object()
-          .nullable()
-          .when('contactTypeCode', {
-            is: (contactTypeCode: string) => Boolean(contactTypeCode),
-            then: Yup.string().required('Select a team member'),
-          }),
-        contactTypeCode: Yup.string()
-          .trim()
-          .when('contact', {
-            is: (contact: object) => Boolean(contact),
-            then: Yup.string().required('Select a profile'),
-          }),
-      },
-      [
-        ['contact', 'contactTypeCode'],
-        ['contactTypeCode', 'contact'],
-      ],
-    ),
+    Yup.object().shape({
+      contact: Yup.object()
+        .nullable()
+        .required('Select a team member'),
+
+      contactTypeCode: Yup.string()
+        .ensure()
+        .required('Select a profile'),
+    }),
   ),
 });
