b442c933ca186809ed5ce1daca192fa7648239ed
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2022-09-23 10:35:22 -0700

    Psp 4490 (#2136)
    * Correct activity save behaviour such that a status does not need to be selected in order for a save to complete successfully.
    
    * lint correction.
    
    * test corrections.

--- frontend/src/features/properties/map/activity/detail/ActivityForm.tsx
+++ frontend/src/features/properties/map/activity/detail/ActivityForm.tsx
@@ -5,7 +5,6 @@ import { Formik, validateYupSchema, yupToFormErrors } from 'formik';
 import { getCancelModalProps, useModalContext } from 'hooks/useModalContext';
 import { Api_Activity } from 'models/api/Activity';
 import * as React from 'react';
-import { toTypeCode } from 'utils/formUtils';
 
 import { Activity, ActivityFile } from './ActivityContainer';
 import { ActivityView } from './ActivityView';
@@ -81,7 +80,6 @@ export const ActivityForm = ({
         }
       }}
       onSubmit={async (values, formikHelpers) => {
-        values.activityStatusTypeCode = toTypeCode(values.status);
         const updatedActivity = await onSave(values.toApi());
         if (!!updatedActivity) {
           formikHelpers.resetForm({
