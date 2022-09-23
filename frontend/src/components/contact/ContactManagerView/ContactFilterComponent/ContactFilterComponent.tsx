f00a2ea915a07a0f716421a0841c3b3597c6bc71
Mahesh Babu <kvmb1988@gmail.com>
2022-09-22 20:16:14 -0400

    PSP-4489: CR fix

--- frontend/src/components/contact/ContactManagerView/ContactFilterComponent/ContactFilterComponent.tsx
+++ frontend/src/components/contact/ContactManagerView/ContactFilterComponent/ContactFilterComponent.tsx
@@ -41,7 +41,6 @@ export const ContactFilterComponent: React.FunctionComponent<IContactFilterCompo
   return (
     <Formik
       enableReinitialize
-      //initialValues={filter ?? defaultFilter}
       initialValues={
         filter ?? { ...defaultFilter, searchBy: showOnlyIndividuals ? 'persons' : 'all' }
       }
