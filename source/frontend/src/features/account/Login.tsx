8141a891dbbeea435c6d3187f1db3c5ee338b674
devinleighsmith <devinleighsmith@gmail.com>
2022-10-18 23:23:00 -0700

    remove console.log

--- source/frontend/src/features/account/Login.tsx
+++ source/frontend/src/features/account/Login.tsx
@@ -39,7 +39,6 @@ const Login = () => {
   if (isIE) {
     return <Redirect to={{ pathname: '/ienotsupported' }} />;
   }
-  console.log(tenant);
 
   return (
     <LoginStyled className="login" fluid={true}>
