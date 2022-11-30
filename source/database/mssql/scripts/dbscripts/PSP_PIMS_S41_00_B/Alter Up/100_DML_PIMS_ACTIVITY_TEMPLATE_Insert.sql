3731460a2ba47012571606a687ebe482dbc36059
Doug Filteau <dfilteau@hilltopz.com>
2022-11-30 10:01:30 -0800

    IS-41.00_B (Plan B) Database Schema
    PSP_PIMS S41.00_B Sprint 43 2022-Nov-29
    *** ******************************************************************* ***
    *** THIS RELEASE REPLACES THE DEPRECATED S41.00 AND THE S41.01 RELEASES ***
    *** ******************************************************************* ***
    - Altered tables:
      - PIMS_ACTIVITY_INSTANCE
        - Set ACTIVITY_TEMPLATE_ID to NOT NULL
      - PIMS_ACTIVITY_TEMPLATE
        - Set ACTIVITY_TEMPLATE_TYPE_CODE to NOT NULL
      - PIMS_PROPERTY
        - Added index to PID
    - Added tables:
      - PIMS_BUSINESS_FUNCTION_CODE
      - PIMS_BUSINESS_FUNCTION_CODE_HIST
      - PIMS_CHART_OF_ACCOUNTS_CODE
      - PIMS_CHART_OF_ACCOUNTS_CODE_HIST
      - PIMS_COST_TYPE_CODE
      - PIMS_COST_TYPE_CODE_HIST
      - PIMS_FINANCIAL_ACTIVITY_CODE
      - PIMS_FINANCIAL_ACTIVITY_CODE_HIST
      - PIMS_RESPONSIBILITY_CODE
      - PIMS_RESPONSIBILITY_CODE_HIST
      - PIMS_WORK_ACTIVITY_CODE
      - PIMS_WORK_ACTIVITY_CODE_HIST
      - PIMS_YEARLY_FINANCIAL_CODE
      - PIMS_YEARLY_FINANCIAL_CODE_HIST
    - Added seed scripts:
      - 094_DML_PIMS_ACTIVITY_TEMPLATE_TYPE_Insert.sql
      - 094_DML_PIMS_ACTIVITY_TEMPLATE_TYPE_Delete.sql
      - 100_DML_PIMS_ACTIVITY_TEMPLATE_Insert.sql
      - 100_DML_PIMS_ACTIVITY_TEMPLATE_Delete.sql
      - 101_DML_PIMS_COST_TYPE_CODE.sql
      - 102_DML_PIMS_CHART_OF_ACCOUNTS_CODE.sql
      - 103_DML_PIMS_BUSINESS_FUNCTION_CODE.sql
      - 104_DML_PIMS_YEARLY_FINANCIAL_CODE.sql
      - 105_DML_PIMS_RESPONSIBILITY_CODE.sql
      - 106_DML_SET_PIMS_RESPONSIBILITY_CODE_SEQ.sql
      - 107_DML_PIMS_FINANCIAL_ACTIVITY_CODE.sql
      - 108_DML_PIMS_WORK_ACTIVITY_CODE.sql
    - Altered seed script:
      - 094_DML_PIMS_ACTIVITY_TEMPLATE_TYPE.sql
        - Added 'File Document'
      - 100_DML_PIMS_ACTIVITY_TEMPLATE.sql
        - Added 'File Document'
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S41_00_B/Alter Up/100_DML_PIMS_ACTIVITY_TEMPLATE_Insert.sql
@@ -0,0 +1,26 @@
+/* -----------------------------------------------------------------------------
+Insert data into the PIMS_ACTIVITY_TEMPLATE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2022-Sep-30  Initial version
+Doug Filteau  2022-Oct-31  Added 'File Document'
+----------------------------------------------------------------------------- */
+
+BEGIN TRANSACTION;
+
+DECLARE @CurrCd NVARCHAR(20)
+SET @CurrCd = N'FILEDOC'
+
+SELECT ACTIVITY_TEMPLATE_TYPE_CODE
+FROM   PIMS_ACTIVITY_TEMPLATE
+WHERE  ACTIVITY_TEMPLATE_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 0
+  BEGIN
+  INSERT INTO PIMS_ACTIVITY_TEMPLATE (ACTIVITY_TEMPLATE_TYPE_CODE, CONCURRENCY_CONTROL_NUMBER)
+  VALUES 
+    (N'FILEDOC', 1);
+  END
+
+COMMIT TRANSACTION;
