8dab12fa21817bf6a6a05e2caac289c355c2f7e5
Alejandro Sanchez <emailforasr@gmail.com>
2023-01-09 17:05:06 -0800

    Update frontend model

--- source/frontend/src/models/api/FinancialCode.ts
+++ source/frontend/src/models/api/FinancialCode.ts
@@ -1,7 +1,12 @@
 import { Api_AuditFields } from './AuditFields';
 import { Api_ConcurrentVersion } from './ConcurrentVersion';
 
-export interface Api_FinancialCode extends Api_ConcurrentVersion, Api_AuditFields {
+export interface Api_FinancialCode<T> extends Api_ConcurrentVersion, Api_AuditFields {
   id?: number;
   type?: string;
+  code?: T;
+  description?: string;
+  displayOrder?: number;
+  effectiveDate?: string;
+  expiryDate?: string;
 }
