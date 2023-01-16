ab0653c9583d35b9777302b475c9ab45e6f17c98
Manuel Rodriguez <marobej@gmail.com>
2023-01-16 13:29:00 -0800

    Fixed selected feature file property

--- source/frontend/src/components/maps/providers/MapStateContext.tsx
+++ source/frontend/src/components/maps/providers/MapStateContext.tsx
@@ -22,7 +22,6 @@ export enum MapStateActionTypes {
   MAP_STATE = 'MAP_STATE',
   SELECTED_FEATURE = 'SELECTED_FEATURE',
   SELECTED_INVENTORY_PROPERTY = 'SELECTED_INVENTORY_PROPERTY',
-  SELECTED_LEASE_PROPERTY = 'SELECTED_LEASE_PROPERTY',
   SELECTED_FILE_FEATURE = 'SELECTED_FILE_FEATURE',
   DRAFT_PROPERTIES = 'DRAFT_PROPERTIES',
   LOADING = 'LOADING',
@@ -82,10 +81,6 @@ export type MapStateActions =
       selectedInventoryProperty: IProperty | null;
     }
   | {
-      type: MapStateActionTypes.SELECTED_LEASE_PROPERTY;
-      selectedLeaseProperty: IProperty | null;
-    }
-  | {
       type: MapStateActionTypes.SELECTED_FILE_FEATURE;
       selectedFileFeature: Feature<Geometry, GeoJsonProperties> | null;
     }
@@ -122,10 +117,6 @@ export const MapStateContextProvider: React.FC<
         case MapStateActionTypes.SELECTED_INVENTORY_PROPERTY:
           return produce(prevState, draft => {
             draft.selectedInventoryProperty = action.selectedInventoryProperty;
-          });
-        case MapStateActionTypes.SELECTED_LEASE_PROPERTY:
-          return produce(prevState, draft => {
-            draft.selectedLeaseProperty = action.selectedLeaseProperty;
           });
         case MapStateActionTypes.LOADING:
           return produce(prevState, draft => {
