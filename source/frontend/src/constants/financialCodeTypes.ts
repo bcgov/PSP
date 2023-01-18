83d56481cad9aa05fe1cf73c806eb94c40e4712e
Alejandro Sanchez <emailforasr@gmail.com>
2023-01-18 12:33:10 -0800

    Refactor common code

--- source/frontend/src/constants/financialCodeTypes.ts
+++ source/frontend/src/constants/financialCodeTypes.ts
@@ -7,24 +7,3 @@ export enum FinancialCodeTypes {
   Responsibility = 'Responsibility',
   YearlyFinancial = 'YearlyFinancial',
 }
-
-export function formatFinancialCodeType(value: FinancialCodeTypes): string {
-  switch (value) {
-    case FinancialCodeTypes.BusinessFunction:
-      return 'Business function';
-    case FinancialCodeTypes.CostTypes:
-      return 'Cost types';
-    case FinancialCodeTypes.WorkActivity:
-      return 'Work activity';
-    case FinancialCodeTypes.ChartOfAccounts:
-      return 'Chart of accounts';
-    case FinancialCodeTypes.FinancialActivity:
-      return 'Financial activity';
-    case FinancialCodeTypes.Responsibility:
-      return 'Responsibility';
-    case FinancialCodeTypes.YearlyFinancial:
-      return 'Yearly financial';
-    default:
-      return 'Unknown';
-  }
-}
