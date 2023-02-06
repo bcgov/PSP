570a7b54fc91bb1f57092375dd160e334e4dbe89
Doug Filteau <dfilteau@hilltopz.com>
2023-02-06 11:12:21 -0800

    Updated files prior to merge

--- source/database/mssql/scripts/dbscripts/PSP_PIMS_S47_00/Alter Up/115_DML_PIMS_CONSULTATION_TYPE.sql
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S47_00/Alter Up/115_DML_PIMS_CONSULTATION_TYPE.sql
@@ -11,11 +11,11 @@ GO
 
 INSERT INTO PIMS_CONSULTATION_TYPE (CONSULTATION_TYPE_CODE, CONSULTATION_STATUS_TYPE_CODE, DESCRIPTION, OTHER_DESCRIPTION, DISPLAY_ORDER)
 VALUES
-  (N'1STNATION', N'UNKNOWN', N'First nation:',                '',                1),
-  (N'STRATRE',   N'UNKNOWN', N'Strategic Real Estate (SRE):', '',                2),
-  (N'REGPLANG',  N'UNKNOWN', N'Regional planning:',           '',                3),
-  (N'REGPRPSVC', N'UNKNOWN', N'Regional property services:',  '',                4),
-  (N'DISTRICT',  N'UNKNOWN', N'District:',                    '',                5),
-  (N'HQ',        N'UNKNOWN', N'Headquarter (HQ):',            '',                6),
-  (N'OTHER',     N'UNKNOWN', N'Other:',                       'Describe other*', 7);
+  (N'1STNATION', N'UNKNOWN', N'First nation',                '',                1),
+  (N'STRATRE',   N'UNKNOWN', N'Strategic Real Estate (SRE)', '',                2),
+  (N'REGPLANG',  N'UNKNOWN', N'Regional planning',           '',                3),
+  (N'REGPRPSVC', N'UNKNOWN', N'Regional property services',  '',                4),
+  (N'DISTRICT',  N'UNKNOWN', N'District',                    '',                5),
+  (N'HQ',        N'UNKNOWN', N'Headquarter (HQ)',            '',                6),
+  (N'OTHER',     N'UNKNOWN', N'Other',                       'Describe other*', 7);
 GO
