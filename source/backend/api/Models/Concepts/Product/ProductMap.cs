318a6b49100e9744a347b8f0f83b14e97cab74f0
Manuel Rodriguez <marobej@gmail.com>
2023-03-10 14:33:11 -0800

    Fixed products loading recursive references (#2990)

--- source/backend/api/Models/Concepts/Product/ProductMap.cs
+++ source/backend/api/Models/Concepts/Product/ProductMap.cs
@@ -8,6 +8,7 @@ namespace Pims.Api.Models.Concepts
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<Entity.PimsProduct, ProductModel>()
+                .PreserveReference(true)
                 .Map(dest => dest.Id, src => src.Internal_Id)
                 .Map(dest => dest.ParentProject, src => src.ParentProject)
                 .Map(dest => dest.AcquisitionFiles, src => src.PimsAcquisitionFiles)
@@ -21,7 +22,7 @@ namespace Pims.Api.Models.Concepts
                 .Inherits<Entity.IBaseEntity, BaseModel>();
 
             config.NewConfig<ProductModel, Entity.PimsProduct>()
-               .Map(dest => dest.Internal_Id, src => src.Id)
+                .Map(dest => dest.Internal_Id, src => src.Id)
                 .Map(dest => dest.ParentProjectId, src => src.ParentProject.Id)
                 .Map(dest => dest.PimsAcquisitionFiles, src => src.AcquisitionFiles)
                 .Map(dest => dest.Code, src => src.Code)
