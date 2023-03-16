69463f936a73f1310ba0854b8a430f2e33ea8850
Eduardo Herrera <Eduardo.Herrera@quartech.com>
2023-03-16 14:53:44 -0700

    PSP-5591 : Suppress activities and activity documents
     - Remove activity claims from roles.
    - Add Claim validations to activities components

--- source/frontend/src/features/properties/map/acquisition/detail/AcquisitionDocumentsTab.tsx
+++ source/frontend/src/features/properties/map/acquisition/detail/AcquisitionDocumentsTab.tsx
@@ -1,5 +1,7 @@
+import { Claims } from 'constants/claims';
 import { DocumentRelationshipType } from 'constants/documentRelationshipType';
 import DocumentListContainer from 'features/documents/list/DocumentListContainer';
+import useKeycloakWrapper from 'hooks/useKeycloakWrapper';
 import * as React from 'react';
 
 interface IAcquisitionDocumentsTabProps {
@@ -9,6 +11,8 @@ interface IAcquisitionDocumentsTabProps {
 const AcquisitionDocumentsTab: React.FunctionComponent<IAcquisitionDocumentsTabProps> = ({
   acquisitionFileId,
 }) => {
+  const { hasClaim } = useKeycloakWrapper();
+
   return (
     <>
       <DocumentListContainer
@@ -16,12 +20,14 @@ const AcquisitionDocumentsTab: React.FunctionComponent<IAcquisitionDocumentsTabP
         parentId={acquisitionFileId}
         relationshipType={DocumentRelationshipType.ACQUISITION_FILES}
       />
-      <DocumentListContainer
-        title="Activity Documents"
-        parentId={acquisitionFileId}
-        relationshipType={DocumentRelationshipType.ACQUISITION_FILE_ACTIVITIES}
-        disableAdd
-      />
+      {hasClaim(Claims.ACTIVITY_VIEW) && (
+        <DocumentListContainer
+          title="Activity Documents"
+          parentId={acquisitionFileId}
+          relationshipType={DocumentRelationshipType.ACQUISITION_FILE_ACTIVITIES}
+          disableAdd
+        />
+      )}
     </>
   );
 };
