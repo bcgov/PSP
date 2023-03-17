23a409c553f5f9449b05467a0208feb65512922a
Doug Filteau <dfilteau@hilltopz.com>
2023-03-17 12:18:38 -0700

    IS-50.00 Database Schema
    PSP_PIMS S50.00 Sprint 49 2023-Mar-17
    - Added tables:
      - PIMS_PROJECT_PERSON_ROLE_TYPE
      - PIMS_ACQUISITION_FILE_FORM_HIST
      - PIMS_ACQUISITION_FILE_FORM
      - PIMS_FORM_TYPE
      - PIMS_PROJECT_PERSON
      - PIMS_PROJECT_PERSON_HIST
    - Altered tables:
      - PIMS_ACQUISITION_OWNER_HIST
      - PIMS_TAKE_HIST
      - PIMS_PRODUCT
      - PIMS_PRODUCT_HIST
      - PIMS_TAKE
      - PIMS_LEASE_CONSULTATION_HIST
      - PIMS_LEASE_CONSULTATION
      - PIMS_CONSULTATION_TYPE
      - PIMS_ACQUISITION_OWNER
    - Altered views:
      - PIMS_CONTACT_MGR_VW
    - Added seed scripts:
      - 122_DML_PIMS_PROJECT_PERSON_ROLE_TYPE.sql
      - 123_DML_PIMS_FORM_TYPE.sql
    - Altered seed scripts:
      - 115_DML_PIMS_CONSULTATION_TYPE.sql
      - 121_DML_PIMS_ACQ_CHKLST_ITEM_TYPE.sql
    - Altered test scripts:
      - 32_PIMS_ACQUISITION_OWNER.sql
    - Requires additional metadata to meet standards

--- source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Build/115_DML_PIMS_CONSULTATION_TYPE.sql
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Build/115_DML_PIMS_CONSULTATION_TYPE.sql
@@ -9,13 +9,13 @@ Doug Filteau  2023-Jan-30  Initial version
 DELETE FROM PIMS_CONSULTATION_TYPE
 GO
 
-INSERT INTO PIMS_CONSULTATION_TYPE (CONSULTATION_TYPE_CODE, CONSULTATION_STATUS_TYPE_CODE, DESCRIPTION, OTHER_DESCRIPTION, DISPLAY_ORDER)
+INSERT INTO PIMS_CONSULTATION_TYPE (CONSULTATION_TYPE_CODE, DESCRIPTION, OTHER_DESCRIPTION, DISPLAY_ORDER)
 VALUES
-  (N'1STNATION', N'UNKNOWN', N'First nation',                '',                1),
-  (N'STRATRE',   N'UNKNOWN', N'Strategic Real Estate (SRE)', '',                2),
-  (N'REGPLANG',  N'UNKNOWN', N'Regional planning',           '',                3),
-  (N'REGPRPSVC', N'UNKNOWN', N'Regional property services',  '',                4),
-  (N'DISTRICT',  N'UNKNOWN', N'District',                    '',                5),
-  (N'HQ',        N'UNKNOWN', N'Headquarter (HQ)',            '',                6),
-  (N'OTHER',     N'UNKNOWN', N'Other',                       'Describe other*', 7);
+  (N'1STNATION', N'First nation',                '',                1),
+  (N'STRATRE',   N'Strategic Real Estate (SRE)', '',                2),
+  (N'REGPLANG',  N'Regional planning',           '',                3),
+  (N'REGPRPSVC', N'Regional property services',  '',                4),
+  (N'DISTRICT',  N'District',                    '',                5),
+  (N'HQ',        N'Headquarter (HQ)',            '',                6),
+  (N'OTHER',     N'Other',                       'Describe other*', 7);
 GO
