9416cb1d2843562902938f852ea58f03616c617e
Doug Filteau <dfilteau@users.noreply.github.com>
2023-03-20 10:48:08 -0700

    IS-50.00 Database Schema (#3013)
    * IS-50.00 Database Schema
    
    PSP_PIMS S50.00 Sprint 49 2023-Mar-17
    - Added tables:
      - PIMS_PROJECT_PERSON_ROLE_TYPE
      - PIMS_ACQUISITION_FILE_FORM_HIST
      - PIMS_ACQUISITION_FILE_FORM
      - PIMS_FORM_TYPE
      - PIMS_PROJECT_PERSON
      - PIMS_PROJECT_PERSON_HIST
    - Altered tables:
      - PIMS_ACQUISITION_OWNER_HIST
      - PIMS_TAKE_HIST
      - PIMS_PRODUCT
      - PIMS_PRODUCT_HIST
      - PIMS_TAKE
      - PIMS_LEASE_CONSULTATION_HIST
      - PIMS_LEASE_CONSULTATION
      - PIMS_CONSULTATION_TYPE
      - PIMS_ACQUISITION_OWNER
    - Altered views:
      - PIMS_CONTACT_MGR_VW
    - Added seed scripts:
      - 122_DML_PIMS_PROJECT_PERSON_ROLE_TYPE.sql
      - 123_DML_PIMS_FORM_TYPE.sql
    - Altered seed scripts:
      - 115_DML_PIMS_CONSULTATION_TYPE.sql
      - 121_DML_PIMS_ACQ_CHKLST_ITEM_TYPE.sql
    - Altered test scripts:
      - 32_PIMS_ACQUISITION_OWNER.sql
    - Requires additional metadata to meet standards
    
    * Added default value to .CONSULTATION_STATUS_TYPE_CODE
    
    Added default value to PIMS_LEASE_CONSULTATION_HIST.CONSULTATION_STATUS_TYPE_CODE

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S50_00/Test Data/24_PIMS_ACTIVITY_TEMPLATE.sql
@@ -0,0 +1,18 @@
+DELETE FROM PIMS_ACTIVITY_TEMPLATE
+GO
+
+INSERT INTO PIMS_ACTIVITY_TEMPLATE (ACTIVITY_TEMPLATE_TYPE_CODE, IS_DISABLED, CONCURRENCY_CONTROL_NUMBER)
+VALUES
+  (N'GENERAL',   CONVERT([bit],(0)), 1),
+  (N'SURVEY',    CONVERT([bit],(1)), 1),
+  (N'SITEVIS',   CONVERT([bit],(0)), 1),
+  (N'GENLTR',    CONVERT([bit],(0)), 1),
+  (N'FILEDOC',   CONVERT([bit],(1)), 1),
+  (N'NOTENTRY',  CONVERT([bit],(0)), 1),
+  (N'CONDENTRY', CONVERT([bit],(0)), 1),
+  (N'RECNEGOT',  CONVERT([bit],(0)), 1),
+  (N'CONSULT',   CONVERT([bit],(0)), 1),
+  (N'RECTAKES',  CONVERT([bit],(0)), 1),
+  (N'OFFAGREE',  CONVERT([bit],(0)), 1),
+  (N'COMPREQ',   CONVERT([bit],(0)), 1);
+GO
