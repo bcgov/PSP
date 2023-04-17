11e46d75c49fb4fede067258b0952c61979e8c07
Doug Filteau <dfilteau@hilltopz.com>
2023-04-17 13:25:35 -0700

    Added LETTER Form Type
    Added LETTER form type to 123_DML_PIMS_FORM_TYPE.sql in the Build folder.

--- source/database/mssql/scripts/dbscripts/PSP_PIMS_S52_00/Build/123_DML_PIMS_FORM_TYPE.sql
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S52_00/Build/123_DML_PIMS_FORM_TYPE.sql
@@ -11,10 +11,11 @@ GO
 
 INSERT INTO PIMS_FORM_TYPE (FORM_TYPE_CODE, DESCRIPTION)
 VALUES
-  (N'H179P', N'Offer agreement - Partial (H179 P)'),
-  (N'H179T', N'Offer agreement - Total (H179 T)'),
-  (N'H179A', N'Offer agreement - Section 3 (H179 A)'),
-  (N'H120',  N'Payment requisition (H120)'),
-  (N'H0074', N'License of Occupation for Construction Access (H0074)'),
-  (N'H0443', N'Conditions of Entry (H0443)');
+  (N'H179P',  N'Offer agreement - Partial (H179 P)'),
+  (N'H179T',  N'Offer agreement - Total (H179 T)'),
+  (N'H179A',  N'Offer agreement - Section 3 (H179 A)'),
+  (N'H120',   N'Payment requisition (H120)'),
+  (N'H0074',  N'License of Occupation for Construction Access (H0074)'),
+  (N'H0443',  N'Conditions of Entry (H0443)'),
+  (N'LETTER', N'General Letter');
 GO
