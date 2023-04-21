37f65a8eff4ac001852478ce35aa097724de1a88
Doug Filteau <dfilteau@hilltopz.com>
2023-04-21 16:05:11 -0700

    IS-52 Database Schema Updates
    PSP_PIMS S52.00 Sprint 51 2023-Apr-21
    - Added tables:
      - PIMS_ACQ_OWNER_COMP_REQ_CHQ
      - PIMS_ACQ_OWNER_COMP_REQ_CHQ_HIST
      - PIMS_COMP_REQ_H120
      - PIMS_COMP_REQ_H120_HIST
      - PIMS_COMPENSATION_REQUISITION
      - PIMS_COMPENSATION_REQUISITION_HIST
      - PIMS_H120_CATEGORY
      - PIMS_H120_CATEGORY_HIST
      - PIMS_USER_TYPE
    - Altered tables:
      - PIMS_ACCESS_REQUEST
      - PIMS_ACCESS_REQUEST_HIST
      - PIMS_ACQUISITION_FILE
      - PIMS_ACQUISITION_FILE_HIST
      - PIMS_AGREEMENT
      - PIMS_AGREEMENT_HIST
      - PIMS_DOCUMENT_TYP
      - PIMS_DOCUMENT_TYP_HIST
      - PIMS_LEASE_CONSULTATION
      - PIMS_LEASE_CONSULTATION_HIST
      - PIMS_USER
      - PIMS_USER_HIST
    - Added seed scripts:
      - 125_DML_PIMS_USER_TYPE.sql
      - 126_DML_PIMS_H120_CATEGORY.sql
    - Altered seed scripts:
      - 020_DML_PIMS_STATIC_VARIABLE.sql
      - 111_DML_PIMS_BUSINESS_FUNCTION_CODE.sql
      - 112_DML_PIMS_COST_TYPE_CODE.sql
      - 121_DML_PIMS_ACQ_CHKLST_ITEM_TYPE.sql
      - 123_DML_PIMS_FORM_TYPE.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Down/112_DML_PIMS_COST_TYPE_CODE_Alter_Down.sql
@@ -0,0 +1,38 @@
+/* -----------------------------------------------------------------------------
+Alter the data in the PIMS_COST_TYPE_CODE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2023-Mar-22  Initial version
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Delete everything but the WBO code.
+DELETE 
+FROM   PIMS_COST_TYPE_CODE
+WHERE  CODE <> N'WBO';
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
