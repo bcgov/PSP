69ede5b8e78b947e3e0cb4be39f948d6b0b0eb54
Alejandro Sanchez <emailforasr@gmail.com>
2023-05-03 17:08:01 -0700

    Update frontend models

--- source/frontend/src/features/properties/map/project/models.ts
+++ source/frontend/src/features/properties/map/project/models.ts
@@ -1,6 +1,6 @@
 import { Api_Product, Api_Project, defaultProject } from 'models/api/Project';
 import { NumberFieldValue } from 'typings/NumberFieldValue';
-import { stringToNull, toTypeCode } from 'utils/formUtils';
+import { fromTypeCode, stringToNull, toTypeCode } from 'utils/formUtils';
 
 export class ProductForm {
   id: number | null = null;
@@ -55,6 +55,9 @@ export class ProjectForm {
   projectName: string | '' = '';
   projectNumber: string | '' = '';
   projectStatusType: string | '' = '';
+  businessFunctionCode: NumberFieldValue | '' = '';
+  costTypeCode: NumberFieldValue | '' = '';
+  workActivityCode: NumberFieldValue | '' = '';
   region: NumberFieldValue | '' = '';
   summary: string | '' = '';
   rowVersion: number | null = null;
@@ -70,9 +73,13 @@ export class ProjectForm {
       note: this.summary ?? null,
       products: this.products?.map(x => x.toApi(this.id)),
       rowVersion: this.rowVersion ?? null,
-      businessFunctionCode: null,
-      costTypeCode: null,
-      workActivityCode: null,
+      businessFunctionCode: this.businessFunctionCode
+        ? toTypeCode<number>(+this.businessFunctionCode) ?? null
+        : null,
+      costTypeCode: this.costTypeCode ? toTypeCode<number>(+this.costTypeCode) ?? null : null,
+      workActivityCode: this.workActivityCode
+        ? toTypeCode<number>(+this.workActivityCode) ?? null
+        : null,
     };
   }
 
@@ -86,6 +93,15 @@ export class ProjectForm {
     newForm.summary = model.note ?? '';
     newForm.rowVersion = model.rowVersion ?? null;
     newForm.products = model.products?.map(x => ProductForm.fromApi(x)) || [];
+    newForm.businessFunctionCode = model.businessFunctionCode?.id
+      ? fromTypeCode<number>(model.businessFunctionCode) ?? ''
+      : '';
+    newForm.costTypeCode = model.costTypeCode?.id
+      ? fromTypeCode<number>(model.costTypeCode) ?? ''
+      : '';
+    newForm.workActivityCode = model.workActivityCode?.id
+      ? fromTypeCode<number>(model.workActivityCode) ?? ''
+      : '';
 
     return newForm;
   }
