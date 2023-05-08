3b71ce38e89a32cea72b10e0c9ee2b9d1d9235ae
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2023-05-08 10:47:36 -0700

    psp-6136 hotfix - replace map property selector clickaway with manual cancel workflow. (#3138)

--- source/frontend/src/components/common/mapping/SelectProperty.tsx
+++ source/frontend/src/components/common/mapping/SelectProperty.tsx
@@ -1,26 +1,22 @@
 import { ReactComponent as DraftSvg } from 'assets/images/pins/icon-draft.svg';
 import * as React from 'react';
-import ClickAwayListener from 'react-click-away-listener';
 import styled from 'styled-components';
 
 interface ISelectPropertyProps {
   onClick: () => void;
-  onClickAway: () => void;
 }
 
 export const SelectProperty: React.FunctionComponent<
   React.PropsWithChildren<ISelectPropertyProps>
-> = ({ onClick, onClickAway }) => {
+> = ({ onClick }) => {
   return (
     <div className="d-flex flex-column align-items-center">
-      <ClickAwayListener onClickAway={onClickAway}>
-        <StyledDraftSvg
-          width={97}
-          height={97}
-          onClick={onClick}
-          title="Click once to enter property selection mode."
-        />
-      </ClickAwayListener>
+      <StyledDraftSvg
+        width={97}
+        height={97}
+        onClick={onClick}
+        title="Click once to enter property selection mode."
+      />
       <ol>
         <li>Single-click pin above</li>
         <li>Mouse to a parcel on the map</li>
