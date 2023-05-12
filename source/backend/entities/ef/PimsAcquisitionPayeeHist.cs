5414647df67241957372ccf09e2dfd12ef5ad977
Manuel Rodriguez <marobej@gmail.com>
2023-05-11 17:25:32 -0700

    Scaffold for s53 (#3166)

--- source/backend/entities/ef/PimsAcquisitionPayeeHist.cs
+++ source/backend/entities/ef/PimsAcquisitionPayeeHist.cs
@@ -8,40 +8,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Pims.Dal.Entities
 {
-    [Table("PIMS_COMPENSATION_REQUISITION")]
-    [Index(nameof(AcquisitionFileId), Name = "CMPREQ_ACQUISITION_FILE_ID_IDX")]
-    public partial class PimsCompensationRequisition
+    [Table("PIMS_ACQUISITION_PAYEE_HIST")]
+    [Index(nameof(AcquisitionPayeeHistId), nameof(EndDateHist), Name = "PIMS_ACQPAY_H_UK", IsUnique = true)]
+    public partial class PimsAcquisitionPayeeHist
     {
-        public PimsCompensationRequisition()
-        {
-            PimsAcquisitionPayees = new HashSet<PimsAcquisitionPayee>();
-            PimsCompReqH120s = new HashSet<PimsCompReqH120>();
-        }
-
         [Key]
+        [Column("_ACQUISITION_PAYEE_HIST_ID")]
+        public long AcquisitionPayeeHistId { get; set; }
+        [Column("EFFECTIVE_DATE_HIST", TypeName = "datetime")]
+        public DateTime EffectiveDateHist { get; set; }
+        [Column("END_DATE_HIST", TypeName = "datetime")]
+        public DateTime? EndDateHist { get; set; }
+        [Column("ACQUISITION_PAYEE_ID")]
+        public long AcquisitionPayeeId { get; set; }
+        [Column("ACQUISITION_OWNER_ID")]
+        public long AcquisitionOwnerId { get; set; }
         [Column("COMPENSATION_REQUISITION_ID")]
         public long CompensationRequisitionId { get; set; }
-        [Column("ACQUISITION_FILE_ID")]
-        public long AcquisitionFileId { get; set; }
-        [Column("IS_DRAFT")]
-        public bool? IsDraft { get; set; }
-        [Column("FISCAL_YEAR")]
-        [StringLength(9)]
-        public string FiscalYear { get; set; }
-        [Column("AGREEMENT_DT", TypeName = "date")]
-        public DateTime? AgreementDt { get; set; }
-        [Column("EXPROP_NOTICE_SERVED_DT", TypeName = "date")]
-        public DateTime? ExpropNoticeServedDt { get; set; }
-        [Column("EXPROP_VESTING_DT", TypeName = "date")]
-        public DateTime? ExpropVestingDt { get; set; }
-        [Column("GENERATION_DT", TypeName = "date")]
-        public DateTime? GenerationDt { get; set; }
-        [Column("SPECIAL_INSTRUCTION")]
-        [StringLength(2000)]
-        public string SpecialInstruction { get; set; }
-        [Column("DETAILED_REMARKS")]
-        [StringLength(2000)]
-        public string DetailedRemarks { get; set; }
         [Column("IS_DISABLED")]
         public bool? IsDisabled { get; set; }
         [Column("CONCURRENCY_CONTROL_NUMBER")]
@@ -82,13 +65,5 @@ namespace Pims.Dal.Entities
         [Column("DB_LAST_UPDATE_USERID")]
         [StringLength(30)]
         public string DbLastUpdateUserid { get; set; }
-
-        [ForeignKey(nameof(AcquisitionFileId))]
-        [InverseProperty(nameof(PimsAcquisitionFile.PimsCompensationRequisitions))]
-        public virtual PimsAcquisitionFile AcquisitionFile { get; set; }
-        [InverseProperty(nameof(PimsAcquisitionPayee.CompensationRequisition))]
-        public virtual ICollection<PimsAcquisitionPayee> PimsAcquisitionPayees { get; set; }
-        [InverseProperty(nameof(PimsCompReqH120.CompensationRequisition))]
-        public virtual ICollection<PimsCompReqH120> PimsCompReqH120s { get; set; }
     }
 }
