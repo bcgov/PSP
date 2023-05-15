031963c2c8bec22d0520628d939a96b9095bf1e0
Manuel Rodriguez <marobej@gmail.com>
2023-05-15 09:30:40 -0700

    Ran scaffold for s54

--- source/backend/entities/ef/PimsYearlyFinancialCode.cs
+++ source/backend/entities/ef/PimsYearlyFinancialCode.cs
@@ -12,6 +12,11 @@ namespace Pims.Dal.Entities
     [Index(nameof(Code), Name = "YRFINC_CODE_IDX")]
     public partial class PimsYearlyFinancialCode
     {
+        public PimsYearlyFinancialCode()
+        {
+            PimsCompensationRequisitions = new HashSet<PimsCompensationRequisition>();
+        }
+
         [Key]
         [Column("ID")]
         public long Id { get; set; }
@@ -67,5 +72,8 @@ namespace Pims.Dal.Entities
         [Column("DB_LAST_UPDATE_USERID")]
         [StringLength(30)]
         public string DbLastUpdateUserid { get; set; }
+
+        [InverseProperty(nameof(PimsCompensationRequisition.YearlyFinancial))]
+        public virtual ICollection<PimsCompensationRequisition> PimsCompensationRequisitions { get; set; }
     }
 }
