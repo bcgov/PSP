bedbe1725d90320faa94182f3fe94c08e2d12a6a
Manuel Rodriguez <marobej@gmail.com>
2023-06-05 11:25:56 -0700

    Regenerated scaffold (#3222)

--- source/backend/entities/PimsBaseContext.cs
+++ source/backend/entities/PimsBaseContext.cs
@@ -123,7 +123,6 @@ namespace Pims.Dal
         public virtual DbSet<PimsInthldrPropInterestHist> PimsInthldrPropInterestHists { get; set; }
         public virtual DbSet<PimsLandActType> PimsLandActTypes { get; set; }
         public virtual DbSet<PimsLandSurveyorType> PimsLandSurveyorTypes { get; set; }
-
         public virtual DbSet<PimsLease> PimsLeases { get; set; }
         public virtual DbSet<PimsLeaseActivityInstance> PimsLeaseActivityInstances { get; set; }
         public virtual DbSet<PimsLeaseActivityInstanceHist> PimsLeaseActivityInstanceHists { get; set; }
@@ -1316,10 +1315,14 @@ namespace Pims.Dal
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("PIM_ACQNFL_PIM_AQOWSO_FK");
 
+                entity.HasOne(d => d.Organization)
+                    .WithMany(p => p.PimsAcquisitionOwnerSolicitors)
+                    .HasForeignKey(d => d.OrganizationId)
+                    .HasConstraintName("PIM_ORG_PIM_AQOWSO_FK");
+
                 entity.HasOne(d => d.Person)
                     .WithMany(p => p.PimsAcquisitionOwnerSolicitors)
                     .HasForeignKey(d => d.PersonId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("PIM_PERSON_PIM_AQOWSO_FK");
             });
 
