baa8c99c98bcad913b19ed23d0f5886a498a1cc1
Alejandro Sanchez <emailforasr@gmail.com>
2023-06-06 15:09:51 -0700

    Add tooltip for total H120 compensation amount

--- source/frontend/src/features/properties/map/compensation/list/CompensationListView.tsx
+++ source/frontend/src/features/properties/map/compensation/list/CompensationListView.tsx
@@ -50,7 +50,11 @@ export const CompensationListView: React.FunctionComponent<ICompensationListView
           />
         }
       >
-        <SectionField label={'Total compensation for this file'} labelWidth="9">
+        <SectionField
+          label="Total compensation for this file"
+          tooltip={`This is the total of all requisitions in the "Final" status. Draft entries are not included here.`}
+          labelWidth="9"
+        >
           {formatMoney(fileCompensationTotal)}
         </SectionField>
       </Section>
