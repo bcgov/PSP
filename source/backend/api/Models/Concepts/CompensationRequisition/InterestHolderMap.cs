6154927707413929994bbd439b40f64821df97f5
Smith <Devin.Smith@quartech.com>
2023-06-05 18:26:29 -0700

    merge corrections.

--- source/backend/api/Models/Concepts/CompensationRequisition/InterestHolderMap.cs
+++ source/backend/api/Models/Concepts/CompensationRequisition/InterestHolderMap.cs
@@ -8,24 +8,25 @@ namespace Pims.Api.Models.Concepts
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<Entity.PimsInterestHolder, InterestHolderModel>()
-                .PreserveReference(true)
                 .Map(dest => dest.InterestHolderId, src => src.InterestHolderId)
+                .Map(dest => dest.AcquisitionFileId, src => src.AcquisitionFileId)
+                .Map(dest => dest.InterestHolderProperties, src => src.PimsInthldrPropInterests)
+                .Map(dest => dest.Person, src => src.Person)
                 .Map(dest => dest.PersonId, src => src.PersonId)
+                .Map(dest => dest.Organization, src => src.Organization)
                 .Map(dest => dest.OrganizationId, src => src.OrganizationId)
                 .Map(dest => dest.IsDisabled, src => src.IsDisabled)
-                .Map(dest => dest.AcquisitionFileId, src => src.AcquisitionFileId)
-                .Map(dest => dest.AcquisitionFile, src => src.AcquisitionFile)
-                .Map(dest => dest.Organization, src => src.Organization)
-                .Map(dest => dest.Person, src => src.Person)
                 .Inherits<Entity.IBaseAppEntity, BaseAppModel>();
 
             config.NewConfig<InterestHolderModel, Entity.PimsInterestHolder>()
-                .PreserveReference(true)
                 .Map(dest => dest.InterestHolderId, src => src.InterestHolderId)
+                .Map(dest => dest.AcquisitionFileId, src => src.AcquisitionFileId)
+                .Map(dest => dest.PimsInthldrPropInterests, src => src.InterestHolderProperties)
+                .Map(dest => dest.Person, src => src.Person)
                 .Map(dest => dest.PersonId, src => src.PersonId)
+                .Map(dest => dest.Organization, src => src.Organization)
                 .Map(dest => dest.OrganizationId, src => src.OrganizationId)
                 .Map(dest => dest.IsDisabled, src => src.IsDisabled)
-                .Map(dest => dest.AcquisitionFileId, src => src.AcquisitionFileId)
                 .Inherits<BaseAppModel, Entity.IBaseAppEntity>();
         }
     }
