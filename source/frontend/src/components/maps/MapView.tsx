1c842fd1340d709f4effe53c88e87ae16b64cf35
Manuel Rodriguez <marobej@gmail.com>
2023-06-20 09:41:57 -0700

    Fixed lint

--- source/frontend/src/components/maps/MapView.tsx
+++ source/frontend/src/components/maps/MapView.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { LatLngBounds, Map as LeafletMap } from 'leaflet';
+import { Map as LeafletMap } from 'leaflet';
 import isEqual from 'lodash/isEqual';
 import isEqualWith from 'lodash/isEqualWith';
 import React, { useContext, useEffect, useRef, useState } from 'react';
