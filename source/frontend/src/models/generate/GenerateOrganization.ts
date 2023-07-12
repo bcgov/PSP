3e211151f9fe1c5a250146d8d869f09d9fa3d781
Alejandro Sanchez <emailforasr@gmail.com>
2023-07-11 19:40:12 -0700

    Generate form improvements

--- source/frontend/src/models/generate/GenerateOrganization.ts
+++ source/frontend/src/models/generate/GenerateOrganization.ts
@@ -1,3 +1,4 @@
+import { ContactMethodTypes } from '@/constants/contactMethodType';
 import { getApiPersonOrOrgMailingAddress } from '@/features/contacts/contactUtils';
 
 import { Api_Organization } from '../api/Organization';
@@ -6,6 +7,8 @@ export class Api_GenerateOrganization {
   name: string;
   alias: string;
   incorporation_number: string;
+  email: string;
+  phone: string;
   full_name_string: string;
   address: Api_GenerateAddress | null;
 
@@ -13,6 +16,34 @@ export class Api_GenerateOrganization {
     this.name = organization?.name ?? '';
     this.alias = organization?.alias ?? '';
     this.incorporation_number = organization?.incorporationNumber ?? '';
+    const workEmail =
+      organization?.contactMethods?.filter(
+        o => o.contactMethodType?.id === ContactMethodTypes.WorkEmail,
+      ) ?? [];
+    const personalEmail =
+      organization?.contactMethods?.filter(
+        o => o.contactMethodType?.id === ContactMethodTypes.PersonalEmail,
+      ) ?? [];
+    this.email =
+      workEmail?.length > 0
+        ? workEmail.map(p => p.value).join(', ')
+        : personalEmail?.length > 0
+        ? personalEmail.map(p => p.value).join(', ')
+        : '';
+    const workPhone =
+      organization?.contactMethods?.filter(
+        o => o.contactMethodType?.id === ContactMethodTypes.WorkPhone,
+      ) ?? [];
+    const personalPhone =
+      organization?.contactMethods?.filter(
+        o => o.contactMethodType?.id === ContactMethodTypes.PersonalPhone,
+      ) ?? [];
+    this.phone =
+      workPhone?.length > 0
+        ? workPhone.map(p => p.value).join(', ')
+        : personalPhone?.length > 0
+        ? personalPhone.map(p => p.value).join(', ')
+        : '';
     this.address = organization
       ? new Api_GenerateAddress(getApiPersonOrOrgMailingAddress(organization) ?? null)
       : null;
