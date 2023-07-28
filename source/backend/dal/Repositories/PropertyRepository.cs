8ac666af826e7585db8f1137ad3d35fe93ea2b1b
devinleighsmith <devinleighsmith@gmail.com>
2023-07-28 09:23:24 -0700

    remove closedxml

--- source/backend/dal/Repositories/PropertyRepository.cs
+++ source/backend/dal/Repositories/PropertyRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Text.RegularExpressions;
-using ClosedXML.Report.Utils;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NetTopologySuite.Geometries;
