1e526cec8b093ef7f9863913ac9cebfcb4a45bfb
Doug Filteau <dfilteau@users.noreply.github.com>
2023-07-28 15:57:15 -0700

    IS-59.00 Database Schema

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S59_00/Build/123_DML_PIMS_FORM_TYPE.sql
@@ -0,0 +1,25 @@
+/* -----------------------------------------------------------------------------
+Populate the missing code values in the PIMS_FORM_TYPE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2023-Jun-30  Initial version
+----------------------------------------------------------------------------- */
+
+DELETE FROM PIMS_FORM_TYPE
+GO
+
+INSERT INTO PIMS_FORM_TYPE (FORM_TYPE_CODE, DESCRIPTION)
+VALUES
+  (N'H179P',  N'Offer agreement - Partial (H179 P)'),
+  (N'H179T',  N'Offer agreement - Total (H179 T)'),
+  (N'H179A',  N'Offer agreement - Section 3 (H179 A)'),
+  (N'H120',   N'Payment requisition (H120)'),
+  (N'H0074',  N'License of Occupation for Construction Access (H0074)'),
+  (N'H0443',  N'Conditions of Entry (H0443)'),
+  (N'LETTER', N'General Letter'),
+  (N'FORM1',  N'Notice of Expropriation (Form 1)'),
+  (N'FORM5',  N'Certificate of Approval of Expropriation (Form 5)'),
+  (N'FORM8',  N'Notice of Advance Payment (Form 8)'),
+  (N'FORM9',  N'Vesting Notice (Form 9)');
+GO
