c49139ca03826d793208c3a64810ffd03121db2b
Alejandro Sanchez <emailforasr@gmail.com>
2023-08-28 20:56:48 -0700

    Show warning when no records were found to export

--- source/frontend/src/features/projects/reports/ProjectExportContainer.tsx
+++ source/frontend/src/features/projects/reports/ProjectExportContainer.tsx
@@ -1,9 +1,9 @@
 import fileDownload from 'js-file-download';
-import * as React from 'react';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 
 import { useAcquisitionProvider } from '@/hooks/repositories/useAcquisitionProvider';
 import { useProjectProvider } from '@/hooks/repositories/useProjectProvider';
+import { useModalContext } from '@/hooks/useModalContext';
 import { Api_ExportProjectFilter } from '@/models/api/ProjectFilter';
 
 import { ProjectExportTypes } from './models';
@@ -19,6 +19,7 @@ export const SideProjectContainer: React.FunctionComponent<ISideProjectContainer
   const {
     getAllProjects: { response: projects, loading: loadingProjects, execute: loadProjects },
   } = useProjectProvider();
+
   const {
     getAllAcquisitionFileTeamMembers: {
       response: team,
@@ -26,8 +27,14 @@ export const SideProjectContainer: React.FunctionComponent<ISideProjectContainer
       execute: loadAcquisitionTeam,
     },
     getAgreementsReport,
-    getCompensationReport,
+    getCompensationReport: {
+      execute: getCompensationReport,
+      status: statusCompensation,
+      response: dataCompensation,
+    },
   } = useAcquisitionProvider();
+
+  const { setModalContent, setDisplayModal } = useModalContext();
 
   const onExportTypeSelected = useCallback(() => {
     loadProjects();
@@ -42,11 +49,23 @@ export const SideProjectContainer: React.FunctionComponent<ISideProjectContainer
   };
 
   const generateCompensationReport = async (values: Api_ExportProjectFilter) => {
-    var data = await getCompensationReport.execute(values);
-    if (data) {
-      fileDownload(data, `Compensation_Requisition_Export.xlsx`);
-    }
+    await getCompensationReport(values);
   };
+
+  React.useEffect(() => {
+    if (statusCompensation === 204) {
+      setModalContent({
+        title: 'Warning',
+        message: 'There is no data for the input parameters you entered.',
+        okButtonText: 'Close',
+        closeButton: true,
+        handleOk: () => setDisplayModal(false),
+      });
+      setDisplayModal(true);
+    } else if (dataCompensation && statusCompensation === 200) {
+      fileDownload(dataCompensation, `Compensation_Requisition_Export.xlsx`);
+    }
+  }, [dataCompensation, statusCompensation, setDisplayModal, setModalContent]);
 
   return (
     <View
