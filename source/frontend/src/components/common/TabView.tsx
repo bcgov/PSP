dbfa8f8dfe850a604417773831ca12b1b79875f4
Alejandro Sanchez <emailforasr@gmail.com>
2023-10-06 17:07:36 -0700

    Fix double scroll-bars

--- source/frontend/src/components/common/TabView.tsx
+++ source/frontend/src/components/common/TabView.tsx
@@ -20,7 +20,7 @@ const StyledTabWrapper = styled.div`
       position: relative;
     }
     border-radius: 0 0.4rem 0.4rem 0.4rem;
-    height: calc(100% - 2.4rem); // substract nav height
+    height: calc(100% - 2.5rem); // subtract nav height
     overflow-y: auto;
     background-color: ${props => props.theme.css.filterBackgroundColor};
   }
