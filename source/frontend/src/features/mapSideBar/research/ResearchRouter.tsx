0f727a5ea8dc663e9f6ff401e1ecad65958d7f3f
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2023-11-06 16:06:28 -0800

    psp-6988 File tabs fix (#3575)
    * correct file property logic on acquisition/research.
    
    * lint correction.

---
+++ source/frontend/src/features/mapSideBar/research/ResearchRouter.tsx
@@ -0,0 +1,71 @@
+import { FormikProps } from 'formik';
+import React from 'react';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
+
+import { InventoryTabNames } from '@/features/mapSideBar/property/InventoryTabs';
+import { FileTabType } from '@/features/mapSideBar/shared/detail/FileTabs';
+import { Api_ResearchFile } from '@/models/api/ResearchFile';
+import { stripTrailingSlash } from '@/utils';
+
+import UpdateResearchContainer from './tabs/fileDetails/update/UpdateSummaryContainer';
+import ResearchTabsContainer from './tabs/ResearchTabsContainer';
+
+export interface IResearchRouterProps {
+  formikRef: React.Ref<FormikProps<any>>;
+  researchFile?: Api_ResearchFile;
+  isEditing: boolean;
+  setIsEditing: (value: boolean) => void;
+  defaultFileTab: FileTabType;
+  defaultPropertyTab: InventoryTabNames;
+  onSuccess: () => void;
+}
+
+export const ResearchRouter: React.FC<IResearchRouterProps> = props => {
+  const { path, url } = useRouteMatch();
+
+  if (props.researchFile === undefined || props.researchFile === null) {
+    return null;
+  }
+
+  // render edit forms
+  if (props.isEditing) {
+    return (
+      <Switch>
+        <Route exact path={`${stripTrailingSlash(path)}/${FileTabType.FILE_DETAILS}`}>
+          <UpdateResearchContainer
+            researchFile={props.researchFile}
+            ref={props.formikRef}
+            onSuccess={props.onSuccess}
+          />
+        </Route>
+        {/* Ignore property-related routes (which are handled in separate FilePropertyRouter) */}
+        <Route path={`${stripTrailingSlash(path)}/property`}>
+          <></>
+        </Route>
+        <Redirect
+          from={`${path}`}
+          to={`${stripTrailingSlash(url)}/${FileTabType.FILE_DETAILS}?edit=true`}
+        />
+      </Switch>
+    );
+  } else {
+    // render read-only views
+    return (
+      <Switch>
+        {/* Ignore property-related routes (which are handled in separate FilePropertyRouter) */}
+        <Route path={`${stripTrailingSlash(path)}/property`}>
+          <></>
+        </Route>
+        <Route path={`${stripTrailingSlash(path)}/:tab`}>
+          <ResearchTabsContainer
+            researchFile={props.researchFile}
+            setIsEditing={props.setIsEditing}
+          />
+        </Route>
+        <Redirect from={`${path}`} to={`${stripTrailingSlash(url)}/${FileTabType.FILE_DETAILS}`} />
+      </Switch>
+    );
+  }
+};
+
+export default ResearchRouter;
