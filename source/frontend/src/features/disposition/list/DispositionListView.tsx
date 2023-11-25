0557b74799a06a11e42c56f8f6faff3499bac3b3
Alejandro Sanchez <emailforasr@gmail.com>
2023-11-24 22:20:48 -0800

    Wire up disposition results sub-component

--- source/frontend/src/features/disposition/list/DispositionListView.tsx
+++ source/frontend/src/features/disposition/list/DispositionListView.tsx
@@ -1,12 +1,21 @@
+import { AxiosResponse } from 'axios';
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { FaPlus } from 'react-icons/fa';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import { StyledAddButton } from '@/components/common/styles';
 import { Claims } from '@/constants';
+import { IPaginateRequest } from '@/hooks/pims-api/interfaces/IPaginateRequest';
 import { useKeycloakWrapper } from '@/hooks/useKeycloakWrapper';
+import { useSearch } from '@/hooks/useSearch';
+import { IPagedItems } from '@/interfaces';
+import { Api_DispositionFile } from '@/models/api/DispositionFile';
+import { Api_DispositionFilter } from '@/models/api/DispositionFilter';
 
+import { DispositionSearchResults } from './DispositionSearchResults/DispositionSearchResults';
+import { DispositionFilterModel, DispositionSearchResultModel } from './models';
 import * as S from './styles';
 
 /**
@@ -15,6 +24,57 @@ import * as S from './styles';
 export const DispositionListView: React.FC<unknown> = () => {
   const { hasClaim } = useKeycloakWrapper();
   const history = useHistory();
+
+  const searchDispositionFiles = async (
+    params: IPaginateRequest<Api_DispositionFilter>,
+  ): Promise<AxiosResponse<IPagedItems<Api_DispositionFile>, any>> => {
+    const mockResponse: any = {
+      data: {
+        items: [],
+        page: 1,
+        pageIndex: 0,
+        quantity: 0,
+        total: 0,
+      },
+    };
+
+    return mockResponse;
+  };
+
+  const {
+    results,
+    filter,
+    sort,
+    error,
+    totalItems,
+    currentPage,
+    totalPages,
+    pageSize,
+    setFilter,
+    setSort,
+    setCurrentPage,
+    setPageSize,
+    loading,
+  } = useSearch<Api_DispositionFile, Api_DispositionFilter>(
+    new DispositionFilterModel().toApi(),
+    searchDispositionFiles,
+    'No matching results can be found. Try widening your search criteria.',
+  );
+
+  React.useEffect(() => {
+    if (error) {
+      toast.error(error?.message);
+    }
+  }, [error]);
+
+  // update internal state whenever the filter bar changes
+  const changeFilter = React.useCallback(
+    (filter: Api_DispositionFilter) => {
+      setFilter(filter);
+      setCurrentPage(0);
+    },
+    [setFilter, setCurrentPage],
+  );
 
   return (
     <S.ListPage>
@@ -31,7 +91,18 @@ export const DispositionListView: React.FC<unknown> = () => {
             &nbsp;Add a Disposition File
           </StyledAddButton>
         )}
-        {/* TODO: disposition search results go here */}
+        <DispositionSearchResults
+          results={results.map(a => DispositionSearchResultModel.fromApi(a))}
+          totalItems={totalItems}
+          pageIndex={currentPage}
+          pageSize={pageSize}
+          pageCount={totalPages}
+          sort={sort}
+          setSort={setSort}
+          setPageSize={setPageSize}
+          setPageIndex={setCurrentPage}
+          loading={loading}
+        ></DispositionSearchResults>
       </S.Scrollable>
     </S.ListPage>
   );
---
+++ source/frontend/src/features/disposition/list/DispositionSearchResults/columns.tsx
@@ -0,0 +1,178 @@
+import { chain } from 'lodash';
+import { Link } from 'react-router-dom';
+import { CellProps } from 'react-table';
+
+import ExpandableTextList from '@/components/common/ExpandableTextList';
+import ExpandableFileProperties from '@/components/common/List/ExpandableFileProperties';
+import { ColumnWithProps } from '@/components/Table';
+import { Claims } from '@/constants/claims';
+import { useKeycloakWrapper } from '@/hooks/useKeycloakWrapper';
+import { Api_DispositionFileTeam } from '@/models/api/DispositionFile';
+import { Api_Organization } from '@/models/api/Organization';
+import { Api_Person } from '@/models/api/Person';
+import Api_TypeCode from '@/models/api/TypeCode';
+import { stringToFragment } from '@/utils';
+import { formatApiPersonNames } from '@/utils/personUtils';
+
+import { DispositionSearchResultModel } from '../models';
+
+interface MemberRoleGroup {
+  id: string;
+  person: Api_Person | null;
+  organization: Api_Organization | null;
+  roles: string[];
+}
+
+export const columns: ColumnWithProps<DispositionSearchResultModel>[] = [
+  {
+    Header: 'Disposition file #',
+    accessor: 'fileNumber',
+    align: 'right',
+    clickable: true,
+    sortable: true,
+    width: 10,
+    maxWidth: 20,
+    Cell: (props: CellProps<DispositionSearchResultModel>) => {
+      const { hasClaim } = useKeycloakWrapper();
+      if (hasClaim(Claims.DISPOSITION_VIEW)) {
+        return (
+          <Link to={`/mapview/sidebar/disposition/${props.row.original.id}`}>
+            {props.row.original.fileNumber}
+          </Link>
+        );
+      }
+      return stringToFragment(props.row.original.fileNumber);
+    },
+  },
+  {
+    Header: 'Reference #',
+    accessor: 'referenceNumber',
+    align: 'right',
+    clickable: true,
+    sortable: true,
+    width: 10,
+    maxWidth: 20,
+  },
+  {
+    Header: 'Disposition file name',
+    accessor: 'fileName',
+    align: 'left',
+    clickable: true,
+    sortable: true,
+    width: 20,
+    maxWidth: 40,
+  },
+  {
+    Header: 'Disposition type',
+    accessor: 'dispositionType',
+    align: 'left',
+    clickable: true,
+    sortable: true,
+    width: 20,
+    maxWidth: 40,
+  },
+  {
+    Header: 'MOTI Region',
+    accessor: 'region',
+    align: 'left',
+    clickable: true,
+    width: 10,
+    maxWidth: 20,
+  },
+  {
+    Header: 'Team member',
+    accessor: 'dispositionTeam',
+    align: 'left',
+    clickable: true,
+    width: 40,
+    maxWidth: 40,
+    Cell: (props: CellProps<DispositionSearchResultModel>) => {
+      const team = props.row.original.dispositionTeam;
+      const personsInTeam = team?.filter(x => x.personId !== undefined);
+      const organizationsInTeam = team?.filter(x => x.organizationId !== undefined);
+
+      const personsAsString: MemberRoleGroup[] = chain(personsInTeam)
+        .groupBy((groupedTeams: Api_DispositionFileTeam) => groupedTeams.personId)
+        .map<MemberRoleGroup>(x => {
+          return {
+            id: x[0].id?.toString() || '',
+            person: x[0].person || {},
+            organization: null,
+            roles: x
+              .map(t => t.teamProfileType)
+              .filter((z): z is Api_TypeCode<string> => z !== undefined)
+              .flatMap(y => y.description || ''),
+          };
+        })
+        .value();
+
+      const organizationsAsString: MemberRoleGroup[] = chain(organizationsInTeam)
+        .groupBy((groupedTeams: Api_DispositionFileTeam) => groupedTeams.organizationId)
+        .map<MemberRoleGroup>(x => {
+          return {
+            id: x[0].id?.toString() || '',
+            person: null,
+            organization: x[0].organization || {},
+            roles: x
+              .map(t => t.teamProfileType)
+              .filter((z): z is Api_TypeCode<string> => z !== undefined)
+              .flatMap(y => y.description || ''),
+          };
+        })
+        .value();
+
+      const teamAsString = personsAsString.concat(organizationsAsString);
+
+      return (
+        <ExpandableTextList<MemberRoleGroup>
+          items={teamAsString ?? []}
+          keyFunction={(item: MemberRoleGroup, index: number) =>
+            item.person
+              ? `disposition-team-${item.id}-person-${item.person.id ?? index}`
+              : `disposition-team-${item.id}-org-${item.organization?.id ?? index}`
+          }
+          renderFunction={(item: MemberRoleGroup) =>
+            item.person ? (
+              <>{`${formatApiPersonNames(item.person)} (${item.roles.join(', ')})`}</>
+            ) : (
+              <>{`${item.organization?.name} (${item.roles.join(', ')})`}</>
+            )
+          }
+          delimiter={', '}
+          maxCollapsedLength={2}
+        />
+      );
+    },
+  },
+  {
+    Header: 'Civic Address / PID / PIN',
+    accessor: 'fileProperties',
+    align: 'left',
+    Cell: (props: CellProps<DispositionSearchResultModel>) => {
+      return (
+        <ExpandableFileProperties
+          fileProperties={props.row.original.fileProperties}
+          maxDisplayCount={2}
+        ></ExpandableFileProperties>
+      );
+    },
+  },
+  {
+    Header: 'Disposition status',
+    accessor: 'dispositionStatus',
+    align: 'left',
+    clickable: true,
+    sortable: true,
+    width: 10,
+    maxWidth: 20,
+  },
+  {
+    Header: 'Status',
+    accessor: 'physicalFileStatus',
+    align: 'left',
+    clickable: true,
+    sortable: true,
+    width: 10,
+    maxWidth: 20,
+  },
+];
