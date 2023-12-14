0333393f76cb650db40778eaf67eb42d07b707bb
Manuel Rodriguez <marobej@gmail.com>
2023-12-14 14:29:41 -0800

    Added improved error handling of the useParcelMapLayer service

--- source/frontend/src/hooks/repositories/mapLayer/useParcelMapLayer.ts
+++ source/frontend/src/hooks/repositories/mapLayer/useParcelMapLayer.ts
@@ -1,6 +1,7 @@
 import { FeatureCollection, Geometry } from 'geojson';
 import { LatLngLiteral } from 'leaflet';
 import { useCallback } from 'react';
+import { toast } from 'react-toastify';
 
 import { useLayerQuery } from '@/hooks/layer-api/useLayerQuery';
 import { useWfsLayer } from '@/hooks/layer-api/useWfsLayer';
@@ -24,61 +25,85 @@ export const useParcelMapLayer = () => {
 
   const { execute: getAllFeatures, loading: getAllFeaturesLoading } = getAllFeaturesWrapper;
 
+  const handleError = useCallback((error: unknown) => {
+    toast.error('Unable to contact Parcel Map');
+  }, []);
+
   const findByPid = useCallback(
     async (pid: string, forceExactMatch = false) => {
       // Removes dashes to match expectations of the map layer.
       const formattedPid = pid.replace(/[-\s]/g, '');
-      const data = await getAllFeatures(
-        { PID: formattedPid },
-        { forceExactMatch: forceExactMatch, timeout: 30000 },
-      );
-      // TODO: Enhance useLayerQuery to allow generics to match the Property types
-      return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      try {
+        const data = await getAllFeatures(
+          { PID: formattedPid },
+          { forceExactMatch: forceExactMatch, timeout: 30000 },
+        );
+        // TODO: Enhance useLayerQuery to allow generics to match the Property types
+        return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      } catch (e: unknown) {
+        handleError(e);
+        return undefined;
+      }
     },
-    [getAllFeatures],
+    [getAllFeatures, handleError],
   );
 
   const findByPin = useCallback(
     async (pin: string, forceExactMatch = false) => {
-      const data = await getAllFeatures(
-        { PIN: pin },
-        { forceExactMatch: forceExactMatch, timeout: 30000 },
-      );
-      // TODO: Enhance useLayerQuery to allow generics to match the Property types
-      return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      try {
+        const data = await getAllFeatures(
+          { PIN: pin },
+          { forceExactMatch: forceExactMatch, timeout: 30000 },
+        );
+        // TODO: Enhance useLayerQuery to allow generics to match the Property types
+        return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      } catch (e: unknown) {
+        handleError(e);
+        return undefined;
+      }
     },
-    [getAllFeatures],
+    [getAllFeatures, handleError],
   );
 
   const findByPlanNumber = useCallback(
     async (planNumber: string, forceExactMatch = false) => {
-      const data = await getAllFeatures(
-        { PLAN_NUMBER: planNumber },
-        { forceExactMatch: forceExactMatch, timeout: 30000 },
-      );
-      // TODO: Enhance useLayerQuery to allow generics to match the Property types
-      return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      try {
+        const data = await getAllFeatures(
+          { PLAN_NUMBER: planNumber },
+          { forceExactMatch: forceExactMatch, timeout: 30000 },
+        );
+        // TODO: Enhance useLayerQuery to allow generics to match the Property types
+        return data as FeatureCollection<Geometry, PMBC_Feature_Properties> | undefined;
+      } catch (e: unknown) {
+        handleError(e);
+        return undefined;
+      }
     },
-    [getAllFeatures],
+    [getAllFeatures, handleError],
   );
 
   const findOne = useCallback(
     async (latlng: LatLngLiteral, geometryName?: string, spatialReferenceId?: number) => {
-      const featureCollection = await findOneWhereContains(
-        latlng,
-        geometryName,
-        spatialReferenceId,
-      );
+      try {
+        const featureCollection = await findOneWhereContains(
+          latlng,
+          geometryName,
+          spatialReferenceId,
+        );
 
-      // TODO: Enhance useLayerQuery to allow generics to match the Property types
-      const forceCasted = featureCollection as
-        | FeatureCollection<Geometry, PMBC_Feature_Properties>
-        | undefined;
-      return forceCasted !== undefined && forceCasted.features.length > 0
-        ? forceCasted.features[0]
-        : undefined;
+        // TODO: Enhance useLayerQuery to allow generics to match the Property types
+        const forceCasted = featureCollection as
+          | FeatureCollection<Geometry, PMBC_Feature_Properties>
+          | undefined;
+        return forceCasted !== undefined && forceCasted.features.length > 0
+          ? forceCasted.features[0]
+          : undefined;
+      } catch (e: unknown) {
+        handleError(e);
+        return undefined;
+      }
     },
-    [findOneWhereContains],
+    [findOneWhereContains, handleError],
   );
 
   return {
