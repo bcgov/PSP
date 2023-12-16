191a8d6c8a613d2d324edfd74f7cf64b86bfa0c4
devinleighsmith <devinleighsmith@gmail.com>
2023-12-15 17:18:39 -0800

    lint correction.

--- source/frontend/src/components/contact/ContactManagerView/ContactManagerView.tsx
+++ source/frontend/src/components/contact/ContactManagerView/ContactManagerView.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { IoMdPersonAdd } from 'react-icons/io';
 import { useHistory } from 'react-router';
