588dd56566b834ffa61075dc996b4c1c4d679d78
Smith <Devin.Smith@quartech.com>
2023-12-22 16:24:24 -0800

    fix property side display.

--- source/frontend/src/features/mapSideBar/disposition/DispositionView.tsx
+++ source/frontend/src/features/mapSideBar/disposition/DispositionView.tsx
@@ -88,6 +88,7 @@ export const DispositionView: React.FunctionComponent<IDispositionViewProps> = (
     ? getEditTitle(fileMatch, propertySelectorMatch, propertiesMatch)
     : 'Disposition File';
 
+  console.log(dispositionFile?.fileProperties);
   const menuItems = dispositionFile?.fileProperties?.map(x => getFilePropertyName(x).value) || [];
   menuItems.unshift('File Summary');
 
