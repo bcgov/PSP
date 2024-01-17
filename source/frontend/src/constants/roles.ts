91567bd3c15bb2745523b37ad2d5783b352c0b74
Alejandro Sanchez <emailforasr@gmail.com>
2024-01-16 17:03:36 -0800

    Remove obsolete user roles

--- source/frontend/src/constants/roles.ts
+++ source/frontend/src/constants/roles.ts
@@ -3,12 +3,6 @@
  */
 export enum Roles {
   SYSTEM_ADMINISTRATOR = 'System Administrator',
-  ORGANIZATION_ADMINISTRATOR = 'Organization Administrator',
-  REAL_ESTATE_MANAGER = 'Real Estate Manager',
-  FINANCE = 'Finance',
-  FUNCTIONAL = 'Functional',
-  FUNCTIONAL_RESTRICTED = 'Functional (Restricted)',
-  READ_ONLY = 'Read Only',
 }
 
 export default Roles;
