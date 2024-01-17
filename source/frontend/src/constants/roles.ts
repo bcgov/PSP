2d9af5cbdaf065bb2f7e5eb2138768404ed891a4
Alejandro Sanchez <emailforasr@gmail.com>
2024-01-16 17:08:32 -0800

    Update user role list

--- source/frontend/src/constants/roles.ts
+++ source/frontend/src/constants/roles.ts
@@ -3,6 +3,18 @@
  */
 export enum Roles {
   SYSTEM_ADMINISTRATOR = 'System Administrator',
+  ACQUISITION_FUNCTIONAL = 'Acquisition functional',
+  ACQUISITION_READ_ONLY = 'Acquisition read-only',
+  LEASE_FUNCTIONAL = 'Lease/License functional',
+  LEASE_READ_ONLY = 'Lease/License read-only',
+  PROJECT_FUNCTIONAL = 'Project functional',
+  PROJECT_READ_ONLY = 'Project read-only',
+  RESEARCH_FUNCTIONAL = 'Research functional',
+  RESEARCH_READ_ONLY = 'Research read-only',
+  MANAGEMENT_FUNCTIONAL = 'Management functional',
+  MANAGEMENT_READ_ONLY = 'Management read-only',
+  DISPOSITION_FUNCTIONAL = 'Disposition functional',
+  DISPOSITION_READ_ONLY = 'Disposition read-only',
 }
 
 export default Roles;
