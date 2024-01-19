2d593c86ffc38fe3e808ceb4760c9ef63c4abf1e
praveen <praveen.porumalla@quartech.com>
2024-01-19 15:59:48 -0400

    Disposition Files initial Automation

---
+++ testing/PIMS.Tests.Automation/Classes/DispositionFile.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace PIMS.Tests.Automation.Classes
+{
+    public class DispositionFile
+    {
+        public string? DispositionFileStatus { get; set; } = String.Empty;
+
+        public string? AssignedDate { get; set; } = String.Empty;
+
+        public string? DispositionCompletedDate { get; set; } = String.Empty;
+
+        public string? DispositionFileName { get; set; } = String.Empty;
+
+        public string? ReferenceNumber { get; set; } = String.Empty;
+
+        public string? DispositionStatus { get; set; } = null!;
+
+        public string? DispositionType { get; set; } = null!;
+
+        public string? InitiatingDocument { get; set; } = String.Empty;
+
+        public string? OtherInitiatingDocument { get; set; } = String.Empty;
+
+        public string? InitiatingDocumentDate { get; set; } = String.Empty;
+
+        public string? PhysicalFileStatus { get; set; } = String.Empty;
+
+        public string? InitiatingBranch { get; set; } = String.Empty;
+
+        public string DispositionMOTIRegion { get; set; } = null!;
+
+
+    }
+}
---
+++ testing/PIMS.Tests.Automation/Features/DispositionFiles.feature
@@ -0,0 +1,9 @@
+﻿@Disposition-Files
+Feature: DispositionFiles
+
+Test cases for Disposition Files feature
+
+Scenario: 01. Disposition File Details
+	Given I create a new Disposition File from row number 1
+	#When I add additional information to the Disposition File Details
+	#Then A new Disposition file is created successfully
