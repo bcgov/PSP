c2988a7f7bf8778f065f4243650d4c70e90043d7
Smith <Devin.Smith@quartech.com>
2024-02-20 17:55:39 -0800

    hotfix corrections: -force file update when properties updated to display new property information. -ensure that user export always starts at page 1.

--- source/frontend/src/features/mapSideBar/disposition/DispositionContainer.tsx
+++ source/frontend/src/features/mapSideBar/disposition/DispositionContainer.tsx
@@ -240,7 +240,7 @@ export const DispositionContainer: React.FunctionComponent<IDispositionContainer
           )
           .then(response => {
             history.push(`${stripTrailingSlash(match.url)}`);
-            onSuccess(true);
+            onSuccess(true, true);
             return response;
           });
       },
