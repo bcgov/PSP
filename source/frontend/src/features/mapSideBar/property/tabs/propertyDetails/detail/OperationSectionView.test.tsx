552c5138308abcaef40e4a5082de87d556d8d944
devinleighsmith <devinleighsmith@gmail.com>
2024-03-25 14:06:08 -0700

    psp-7962 correct missing no rows message introduced by operation section subtable.

---
+++ source/frontend/src/features/mapSideBar/property/tabs/propertyDetails/detail/OperationSectionView.test.tsx
@@ -0,0 +1,96 @@
+import { lookupCodesSlice } from '@/store/slices/lookupCodes';
+import { createMemoryHistory } from 'history';
+import { mockLookups } from '@/mocks/lookups.mock';
+import { render, RenderOptions } from '@/utils/test-utils';
+import { EpochIsoDateTime } from '@/models/api/UtcIsoDateTime';
+import { getEmptyProperty } from '@/models/defaultInitializers';
+import { ApiGen_Concepts_Property } from '@/models/api/generated/ApiGen_Concepts_Property';
+import { IOperationViewProps, OperationView } from './OperationView';
+import { ApiGen_CodeTypes_PropertyOperationTypes } from '@/models/api/generated/ApiGen_CodeTypes_PropertyOperationTypes';
+import { OperationSectionView } from './propertyOperation/OperationSectionView';
+import { OperationSet } from './propertyOperation/OperationContainer';
+import { getMockApiProperties } from '@/mocks/properties.mock';
+
+const history = createMemoryHistory();
+const store = { [lookupCodesSlice.name]: { lookupCodes: mockLookups } };
+
+describe('Operation section view', () => {
+  const setup = (renderOptions: RenderOptions & { props?: Partial<IOperationViewProps> } = {}) => {
+    const props = renderOptions.props;
+    const component = render(
+      <OperationSectionView
+        subdivisionOperations={props?.subdivisionOperations ?? []}
+        consolidationOperations={props?.consolidationOperations ?? []}
+        loading={props?.loading ?? false}
+      />,
+      {
+        ...renderOptions,
+        useMockAuthentication: true,
+        claims: renderOptions?.claims,
+        history: history,
+        store: store,
+      },
+    );
+
+    return { ...component };
+  };
+
+  it('matches snapshot', () => {
+    const subdivisionOperations: OperationSet = {
+      sourceProperties: [getMockApiProperties()[0]],
+      destinationProperties: [getMockApiProperties()[1]],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.SUBDIVIDE,
+    };
+    const consolidationOperations: OperationSet = {
+      sourceProperties: [getMockApiProperties()[0]],
+      destinationProperties: [getMockApiProperties()[1]],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.CONSOLIDATE,
+    };
+    const { asFragment } = setup({ props: { subdivisionOperations, consolidationOperations } });
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('Displays the empty subdivisions message', async () => {
+    const subdivisionOperations: OperationSet = {
+      sourceProperties: [],
+      destinationProperties: [],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.SUBDIVIDE,
+    };
+    const consolidationOperations: OperationSet = {
+      sourceProperties: [getMockApiProperties()[0]],
+      destinationProperties: [getMockApiProperties()[1]],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.CONSOLIDATE,
+    };
+
+    const { getByText, container } = setup({
+      props: { subdivisionOperations, consolidationOperations },
+    });
+
+    expect(getByText('This property is not part of a subdivision')).toBeVisible();
+  });
+
+  it('Displays the empty consolidations message', async () => {
+    const subdivisionOperations: OperationSet = {
+      sourceProperties: [getMockApiProperties()[0]],
+      destinationProperties: [getMockApiProperties()[1]],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.SUBDIVIDE,
+    };
+    const consolidationOperations: OperationSet = {
+      sourceProperties: [],
+      destinationProperties: [],
+      operationDateTime: '2020-01-01',
+      operationType: ApiGen_CodeTypes_PropertyOperationTypes.CONSOLIDATE,
+    };
+
+    const { getByText, container } = setup({
+      props: { subdivisionOperations, consolidationOperations },
+    });
+
+    expect(getByText('This property is not part of a consolidation')).toBeVisible();
+  });
+});
--- source/frontend/src/features/mapSideBar/property/tabs/propertyDetails/detail/propertyOperation/OperationSectionView.tsx
+++ source/frontend/src/features/mapSideBar/property/tabs/propertyDetails/detail/propertyOperation/OperationSectionView.tsx
@@ -1,5 +1,6 @@
 import LoadingBackdrop from '@/components/common/LoadingBackdrop';
 import { Section } from '@/components/common/Section/Section';
+import { StyledNoData } from '@/features/documents/commonStyles';
 import { ApiGen_CodeTypes_PropertyOperationTypes } from '@/models/api/generated/ApiGen_CodeTypes_PropertyOperationTypes';
 
 import { OperationSet } from './OperationContainer';
@@ -17,36 +18,50 @@ export const OperationSectionView: React.FunctionComponent<IOperationSectionView
   consolidationOperations,
   loading,
 }) => {
+  const hasSubdivisions = subdivisionOperations.length > 0;
+  const hasConsolidations = consolidationOperations.length > 0;
   return (
     <>
       <Section header="Subdivision History" className="position-relative">
-        <LoadingBackdrop show={loading} parentScreen />
-        {subdivisionOperations.map((operationSet, index) => (
+        {hasSubdivisions ? (
           <>
-            <OperationView
-              key={index}
-              operationType={ApiGen_CodeTypes_PropertyOperationTypes.SUBDIVIDE}
-              operationTimeStamp={operationSet.operationDateTime ?? ''}
-              sourceProperties={operationSet.sourceProperties}
-              destinationProperties={operationSet.destinationProperties}
-              ExpandedRowComponent={OperationFileAssociationsContainer}
-            />
-            {index < subdivisionOperations.length - 1 && <br />}
+            <LoadingBackdrop show={loading} parentScreen />
+            {subdivisionOperations.map((operationSet, index) => (
+              <>
+                <OperationView
+                  key={index}
+                  operationType={ApiGen_CodeTypes_PropertyOperationTypes.SUBDIVIDE}
+                  operationTimeStamp={operationSet.operationDateTime ?? ''}
+                  sourceProperties={operationSet.sourceProperties}
+                  destinationProperties={operationSet.destinationProperties}
+                  ExpandedRowComponent={OperationFileAssociationsContainer}
+                />
+                {index < subdivisionOperations.length - 1 && <br />}
+              </>
+            ))}
           </>
-        ))}
+        ) : (
+          <StyledNoData>This property is not part of a subdivision</StyledNoData>
+        )}
       </Section>
       <Section header="Consolidation History" className="position-relative">
-        <LoadingBackdrop show={loading} parentScreen />
-        {consolidationOperations.map((operationSet, index) => (
-          <OperationView
-            key={index}
-            operationType={ApiGen_CodeTypes_PropertyOperationTypes.CONSOLIDATE}
-            operationTimeStamp={operationSet.operationDateTime ?? ''}
-            sourceProperties={operationSet.sourceProperties}
-            destinationProperties={operationSet.destinationProperties}
-            ExpandedRowComponent={OperationFileAssociationsContainer}
-          />
-        ))}
+        {hasConsolidations ? (
+          <>
+            <LoadingBackdrop show={loading} parentScreen />
+            {consolidationOperations.map((operationSet, index) => (
+              <OperationView
+                key={index}
+                operationType={ApiGen_CodeTypes_PropertyOperationTypes.CONSOLIDATE}
+                operationTimeStamp={operationSet.operationDateTime ?? ''}
+                sourceProperties={operationSet.sourceProperties}
+                destinationProperties={operationSet.destinationProperties}
+                ExpandedRowComponent={OperationFileAssociationsContainer}
+              />
+            ))}
+          </>
+        ) : (
+          <StyledNoData>This property is not part of a consolidation</StyledNoData>
+        )}
       </Section>
     </>
   );
