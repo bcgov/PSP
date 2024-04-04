7424e32b8e4c7f7b1964730c6903cd16d0d7ab4d
devinleighsmith <devinleighsmith@gmail.com>
2024-04-04 13:32:39 -0700

    psp-8187, correct enum mapping of pims layer such that area units are processed correctly. Also ensure that pims area units are converted to M2 in consolidation/subdivision.

--- source/frontend/src/utils/utils.ts
+++ source/frontend/src/utils/utils.ts
@@ -29,6 +29,12 @@ export function round(value: number, decimalPlaces = 2): number {
   return Math.round((value + Number.EPSILON) * factorOfTen) / factorOfTen;
 }
 
+export const enumFromValue = <T extends Record<string, string>>(val: string, _enum: T) => {
+  const enumName = (Object.keys(_enum) as Array<keyof T>).find(k => _enum[k] === val);
+  if (!enumName) throw Error(); // here fail fast as an example
+  return _enum[enumName];
+};
+
 /**
  * Determine if the specified 'input' value is a positive number of zero.
  * @param input The value to evaluate.
