affbf034d2cd545bdf4eac2c875853fbee9e9e7b
Eduardo <eddherrera@users.noreply.github.com>
2024-04-04 15:30:40 -0700

    PSP-7886 : Prevent users from adding new takes, and/or deleting completed takes from a non-active (e.g. draft or active) acquisition file (#3923)
    Co-authored-by: Herrera <eduardo.herrera@quartech.com>

--- source/frontend/src/constants/acquisitionFileStatus.ts
+++
@@ -1,9 +0,0 @@
-export enum AcquisitionStatus {
-  Active = 'ACTIVE',
-  Archived = 'ARCHIV',
-  Cancelled = 'CANCEL',
-  Closed = 'CLOSED',
-  Complete = 'COMPLT',
-  Draft = 'DRAFT',
-  Hold = 'HOLD',
-}
