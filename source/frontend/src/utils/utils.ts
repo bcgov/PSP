90dc382209a4493bda76fb18350b0ed7c0797377
devinleighsmith <devinleighsmith@gmail.com>
2024-04-04 13:43:01 -0700

    update handling of undefined enum value.

--- source/frontend/src/utils/utils.ts
+++ source/frontend/src/utils/utils.ts
@@ -31,7 +31,7 @@ export function round(value: number, decimalPlaces = 2): number {
 
 export const enumFromValue = <T extends Record<string, string>>(val: string, _enum: T) => {
   const enumName = (Object.keys(_enum) as Array<keyof T>).find(k => _enum[k] === val);
-  if (!enumName) throw Error(); // here fail fast as an example
+  if (!enumName) return null;
   return _enum[enumName];
 };
 
