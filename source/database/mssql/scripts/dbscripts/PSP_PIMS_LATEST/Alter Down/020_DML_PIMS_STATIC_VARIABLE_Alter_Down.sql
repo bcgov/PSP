0cff2dd088c9590cd41a54e91bc750bf1156ca9d
Doug Filteau <dfilteau@users.noreply.github.com>
2024-04-16 13:53:28 -0700

    IS-78.00 Database Schema Changes
    PSP_PIMS | Development Sprint: S78.00 | Design Sprint: 77 | Date: 2024-Apr-16
    - Altered seed scripts:
      - 020_DML_PIMS_STATIC_VARIABLE.sql
      - 056_DML_PIMS_LEASE_PURPOSE_TYPE.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Down/020_DML_PIMS_STATIC_VARIABLE_Alter_Down.sql
@@ -0,0 +1,70 @@
+/* -----------------------------------------------------------------------------
+Alter the data in the PIMS_STATIC_VARIABLE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2024-Mar-27  Initial version
+Doug Filteau  2024-Apr-16  Updated FY Start and End.
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Update the "FYSTART" variable/
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'FYSTART'
+
+SELECT STATIC_VARIABLE_NAME
+FROM   PIMS_STATIC_VARIABLE
+WHERE  STATIC_VARIABLE_NAME = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  BEGIN
+  UPDATE PIMS_STATIC_VARIABLE
+  SET    STATIC_VARIABLE_VALUE      = N'01/04/2023'
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  STATIC_VARIABLE_NAME = @CurrCd;
+  END
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Update the "FYSTART" variable/
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'FYEND'
+
+SELECT STATIC_VARIABLE_NAME
+FROM   PIMS_STATIC_VARIABLE
+WHERE  STATIC_VARIABLE_NAME = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  BEGIN
+  UPDATE PIMS_STATIC_VARIABLE
+  SET    STATIC_VARIABLE_VALUE      = N'31/03/2024'
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  STATIC_VARIABLE_NAME = @CurrCd;
+  END
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
+
