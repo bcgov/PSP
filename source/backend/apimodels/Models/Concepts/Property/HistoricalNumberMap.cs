17ab7ba64fbf6df34960cd9d684363028fa2902f
Manuel Rodriguez <marobej@gmail.com>
2024-05-13 10:15:04 -0700

    Fixed compilation issues

--- source/backend/apimodels/Models/Concepts/Property/HistoricalNumberMap.cs
+++ source/backend/apimodels/Models/Concepts/Property/HistoricalNumberMap.cs
@@ -9,23 +9,23 @@ namespace Pims.Api.Models.Concepts.Property
     {
         public void Register(TypeAdapterConfig config)
         {
-            config.NewConfig<Entity.PimsFileNumber, HistoricalNumberModel>()
-                .Map(dest => dest.Id, src => src.FileNumberId)
+            config.NewConfig<Entity.PimsHistoricalFileNumber, HistoricalNumberModel>()
+                .Map(dest => dest.Id, src => src.HistoricalFileNumberId)
                 .Map(dest => dest.PropertyId, src => src.PropertyId)
                 .Map(dest => dest.Property, src => src.Property)
-                .Map(dest => dest.HistoricalNumber, src => src.FileNumber)
-                .Map(dest => dest.HistoricalNumberType, src => src.FileNumberTypeCodeNavigation)
-                .Map(dest => dest.OtherHistoricalNumberType, src => src.OtherFileNumberType)
+                .Map(dest => dest.HistoricalNumber, src => src.HistoricalFileNumber)
+                .Map(dest => dest.HistoricalNumberType, src => src.HistoricalFileNumberTypeCodeNavigation)
+                .Map(dest => dest.OtherHistoricalNumberType, src => src.OtherHistFileNumberTypeCode)
                 .Map(dest => dest.IsDisabled, src => src.IsDisabled)
                 .Inherits<Entity.IBaseEntity, BaseConcurrentModel>();
 
-            config.NewConfig<HistoricalNumberModel, Entity.PimsFileNumber>()
-                .Map(dest => dest.FileNumberId, src => src.Id)
+            config.NewConfig<HistoricalNumberModel, Entity.PimsHistoricalFileNumber>()
+                .Map(dest => dest.HistoricalFileNumberId, src => src.Id)
                 .Map(dest => dest.PropertyId, src => src.PropertyId)
                 .Map(dest => dest.Property, src => src.Property)
-                .Map(dest => dest.FileNumber, src => src.HistoricalNumber)
-                .Map(dest => dest.FileNumberTypeCode, src => src.HistoricalNumberType.Id)
-                .Map(dest => dest.OtherFileNumberType, src => src.OtherHistoricalNumberType)
+                .Map(dest => dest.HistoricalFileNumber, src => src.HistoricalNumber)
+                .Map(dest => dest.HistoricalFileNumberTypeCode, src => src.HistoricalNumberType.Id)
+                .Map(dest => dest.OtherHistFileNumberTypeCode, src => src.OtherHistoricalNumberType)
                 .Map(dest => dest.IsDisabled, src => src.IsDisabled)
                 .Inherits<BaseConcurrentModel, Entity.IBaseEntity>();
         }
