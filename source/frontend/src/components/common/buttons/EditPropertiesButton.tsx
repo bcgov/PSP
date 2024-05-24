ca3c9a6ceac18a6d7747e0ed83eb5f64d31d464c
devinleighsmith <devinleighsmith@gmail.com>
2024-05-24 00:14:19 -0700

    psp-8441 correct missing subdivision icon.

---
+++ source/frontend/src/components/common/buttons/EditPropertiesButton.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import EditMapMarkerIcon from '@/assets/images/edit-map-marker.svg?react';
+
+export const EditPropertiesIcon: React.FC = () => {
+  return <EditMapMarkerButton width="2.4rem" height="2.4rem" />;
+};
+
+const EditMapMarkerButton = styled(EditMapMarkerIcon)`
+  fill: ${props => props.theme.css.primaryColor};
+`;
--- source/frontend/src/features/mapSideBar/disposition/common/DispositionMenu.tsx
+++ source/frontend/src/features/mapSideBar/disposition/common/DispositionMenu.tsx
@@ -3,7 +3,7 @@ import { Col, Row } from 'react-bootstrap';
 import { FaCaretRight } from 'react-icons/fa';
 import styled from 'styled-components';
 
-import EditMapMarker from '@/assets/images/edit-map-marker.svg?react';
+import { EditPropertiesIcon } from '@/components/common/buttons/EditPropertiesButton';
 import EditButton from '@/components/common/EditButton';
 import TooltipIcon from '@/components/common/TooltipIcon';
 import { Claims, Roles } from '@/constants/index';
@@ -56,7 +56,7 @@ const DispositionMenu: React.FunctionComponent<
                   {hasClaim(Claims.DISPOSITION_EDIT) && canEditDetails() && (
                     <EditButton
                       title="Change properties"
-                      icon={<EditMapMarker width="2.4rem" height="2.4rem" />}
+                      icon={<EditPropertiesIcon />}
                       onClick={props.onShowPropertySelector}
                     />
                   )}
