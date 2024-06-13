6874a4bb796d6362bb42b81d8ad1032e5ca79d49
Sue Tairaku <sue.tairaku@gmail.com>
2024-06-13 15:31:33 -0700

    Changes on volume measurement

--- source/frontend/src/components/measurements/VolumeView.tsx
+++ source/frontend/src/components/measurements/VolumeView.tsx
@@ -19,7 +19,7 @@ const VolumeView: React.FunctionComponent<IVolumeViewProps> = ({ volume, unitCod
       <Col>
         <StyledGreenBlue>
           <Row>
-            <Col className="text-right">{meters == 0 ? 0 : formatNumber(meters, 4, 4)}</Col>
+            <Col className="text-right">{meters === 0 ? 0 : formatNumber(meters, 4, 4)}</Col>
             <Col>
               <span>
                 metres<sup>3</sup>
