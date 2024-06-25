70818499c5138a74425f3c246a8fdd70312c01fa
Doug Filteau <dfilteau@users.noreply.github.com>
2024-06-25 11:28:49 -0700

    Added Txn Commit
    008_PSP_PIMS_Rename_Build.sql

--- source/database/mssql/scripts/dbscripts/PSP_PIMS_S83_00/Build/008_PSP_PIMS_Rename.sql
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S83_00/Build/008_PSP_PIMS_Rename.sql
@@ -213,3 +213,18 @@ END CATCH;
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
+
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
