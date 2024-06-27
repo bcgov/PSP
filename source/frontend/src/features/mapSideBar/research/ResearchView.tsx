734673315bce0fd6a7e021d77caefc190ef0a44e
devinleighsmith <devinleighsmith@gmail.com>
2024-06-27 09:43:51 -0700

    merge corrections.

--- source/frontend/src/features/mapSideBar/research/ResearchView.tsx
+++ source/frontend/src/features/mapSideBar/research/ResearchView.tsx
@@ -2,7 +2,6 @@ import { FormikProps } from 'formik';
 import { useContext } from 'react';
 import { MdTopic } from 'react-icons/md';
 import { matchPath, Route, useHistory, useRouteMatch } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { FileTypes } from '@/constants';
 import FileLayout from '@/features/mapSideBar/layout/FileLayout';
@@ -142,14 +141,3 @@ const ResearchView: React.FunctionComponent<IResearchViewProps> = props => {
 };
 
 export default ResearchView;
-
-const StyledFormWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  text-align: left;
-  height: 100%;
-  overflow-y: auto;
-  padding-right: 1rem;
-  padding-bottom: 1rem;
-`;
