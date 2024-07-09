a340a1b41a80c5f9b65862b049614ce47f0b8ace
devinleighsmith <devinleighsmith@gmail.com>
2024-07-08 17:38:13 -0700

    code review comments.

--- source/backend/tests/unit/api/Services/PaymentServiceTest.cs
+++ source/backend/tests/unit/api/Services/PaymentServiceTest.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Azure;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Moq;
