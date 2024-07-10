45d04da9ac0eaebf4a9c2de81ccd26bf284e4f2c
Sue Tairaku <sue.tairaku@gmail.com>
2024-07-10 09:23:28 -0700

    #

--- source/frontend/src/features/leases/detail/LeasePages/details/DetailFeeDetermination.tsx
+++ source/frontend/src/features/leases/detail/LeasePages/details/DetailFeeDetermination.tsx
@@ -1,28 +1,28 @@
-import { Section } from "@/components/common/Section/Section";
-import { SectionField } from "@/components/common/Section/SectionField";
-import { ApiGen_Concepts_Lease } from "@/models/api/generated/ApiGen_Concepts_Lease";
-import { useFormikContext } from "formik";
+import { Section } from '@/components/common/Section/Section';
+import { SectionField } from '@/components/common/Section/SectionField';
+import { ApiGen_Concepts_Lease } from '@/models/api/generated/ApiGen_Concepts_Lease';
+import { useFormikContext } from 'formik';
 
 export interface IDetailFeeDeterminationProps {
-    nameSpace?: string;
-    disabled?: boolean;
-  }
+  nameSpace?: string;
+  disabled?: boolean;
+}
 
-  /**
-   * Sub-form containing lease detail administration fields
-   * @param {IDetailFeeDeterminationProps} param0
-   */
-  export const DetailFeeDetermination: React.FunctionComponent<
-    React.PropsWithChildren<IDetailFeeDeterminationProps>
-  > = ({ nameSpace, disabled }) => {
-    const { values } = useFormikContext<ApiGen_Concepts_Lease>();
-    return (
-        <Section initiallyExpanded={true} isCollapsable={true} header="Fee Determination">
-            <SectionField label="Program" labelWidth="3"></SectionField>
+/**
+ * Sub-form containing lease detail administration fields
+ * @param {IDetailFeeDeterminationProps} param0
+ */
+export const DetailFeeDetermination: React.FunctionComponent<
+  React.PropsWithChildren<IDetailFeeDeterminationProps>
+> = ({ nameSpace, disabled }) => {
+  const { values } = useFormikContext<ApiGen_Concepts_Lease>();
+  return (
+    <Section initiallyExpanded={true} isCollapsable={true} header="Fee Determination">
+      <SectionField label="Program" labelWidth="3"></SectionField>
 
-            <SectionField label="Program" labelWidth="3"></SectionField>
+      <SectionField label="Program" labelWidth="3"></SectionField>
 
-            <SectionField label="Program" labelWidth="3"></SectionField>
-        </Section>
-    );
-  };
+      <SectionField label="Program" labelWidth="3"></SectionField>
+    </Section>
+  );
+};
