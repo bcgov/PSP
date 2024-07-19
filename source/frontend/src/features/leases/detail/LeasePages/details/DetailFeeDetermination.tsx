39d161427948d31b558dda4685371ab163048782
devinleighsmith <devinleighsmith@gmail.com>
2024-07-18 17:04:39 -0700

    psp-8912 show suggested text for fee determination.

--- source/frontend/src/features/leases/detail/LeasePages/details/DetailFeeDetermination.tsx
+++ source/frontend/src/features/leases/detail/LeasePages/details/DetailFeeDetermination.tsx
@@ -1,4 +1,5 @@
 import { getIn, useFormikContext } from 'formik';
+import styled from 'styled-components';
 
 import { YesNoSelect } from '@/components/common/form/YesNoSelect';
 import { Section } from '@/components/common/Section/Section';
@@ -6,7 +7,7 @@ import { SectionField } from '@/components/common/Section/SectionField';
 import { ApiGen_Concepts_Lease } from '@/models/api/generated/ApiGen_Concepts_Lease';
 import { withNameSpace } from '@/utils/formUtils';
 
-import { getSuggestedFee } from '../../../leaseUtils';
+import { getSuggestedFee, SuggestedFeeCode } from '../../../leaseUtils';
 
 export interface IDetailFeeDeterminationProps {
   nameSpace?: string;
@@ -27,6 +28,7 @@ export const DetailFeeDetermination: React.FunctionComponent<
     formikProps.values,
     withNameSpace(nameSpace, 'feeDeterminationNote'),
   );
+  const fee = getSuggestedFee(isPublicBenefit, isFinancialGain);
 
   return (
     <Section initiallyExpanded={true} isCollapsable={true} header="Fee Determination">
@@ -43,7 +45,31 @@ export const DetailFeeDetermination: React.FunctionComponent<
         tooltip="Licence Administration Fee (LAF) *: If the financial gain far outweighs the public benefit, Fair Market Value should be considered over Licence Administration Fee"
         labelWidth="3"
       >
-        <span data-testid="suggestedFee">{getSuggestedFee(isPublicBenefit, isFinancialGain)}</span>
+        <span data-testid="suggestedFee">{fee}</span>
+        <StyledHelpText>
+          {(fee === SuggestedFeeCode.NOMINAL || fee === SuggestedFeeCode.ANY) && (
+            <p className="m-0">
+              <b>Nominal:&nbsp;</b>
+              No or nominal fee determinations should include justification in the Comments field.
+            </p>
+          )}
+          {(fee === SuggestedFeeCode.FMV || fee === SuggestedFeeCode.ANY) && (
+            <p className="m-0">
+              <b>FMV:&nbsp;</b>
+              Fair market value fee determination should include the square footage rate in the
+              Comments field. The rate determination summary or appraisal should be uploaded.
+            </p>
+          )}
+          {(fee === SuggestedFeeCode.LAF || fee === SuggestedFeeCode.ANY) && (
+            <p className="m-0">
+              <b>LAF:&nbsp;</b>
+              License administration fees are charged when there is either: a financial gain for the
+              licensee and a benefit to the public, or when there is no financial gain to the
+              licensee and there is no benefit for the public. The LAF can vary based on the impact
+              to the land, the length of the LOO, and if MOTI requires a legal review.
+            </p>
+          )}
+        </StyledHelpText>
       </SectionField>
 
       <SectionField
@@ -56,3 +82,8 @@ export const DetailFeeDetermination: React.FunctionComponent<
     </Section>
   );
 };
+
+const StyledHelpText = styled.div`
+  font-size: 1.4rem;
+  margin-top: 1rem;
+`;
