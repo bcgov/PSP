255cd9c066937d55027c315a63cdc0557abaf26c
Herrera <eduardo.herrera@quartech.com>
2024-07-23 10:33:23 -0700

    PSP-8945 : Lease & Licences - Administration Section: Add an expandable tooltip above MOTI Contact field

--- source/frontend/src/features/leases/add/AdministrationSubForm.tsx
+++ source/frontend/src/features/leases/add/AdministrationSubForm.tsx
@@ -1,8 +1,9 @@
 import { FormikProps } from 'formik/dist/types';
-import { useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 
 import { FastDatePicker, Input, Select } from '@/components/common/form';
+import FormGuideContainer from '@/components/common/form/FormGuide/FormGuideContainer';
 import { InlineInput } from '@/components/common/form/styles';
 import { UserRegionSelectContainer } from '@/components/common/form/UserRegionSelect/UserRegionSelectContainer';
 import { Section } from '@/components/common/Section/Section';
@@ -57,8 +58,64 @@ const AdministrationSubForm: React.FunctionComponent<
     }
   }, [leaseTypeCode, setFieldValue]);
 
+  const guideBodyContent = (): ReactNode => {
+    return (
+      <>
+        <p className="mb-4">
+          Select the appropriate drop-down options based on the intended use of the property.
+          Program is broad level and is searchable. Type is the type of agreement; only certain
+          document generation options will exist depending on what option you select. Purpose is a
+          summary of the intended use.
+        </p>
+        <ul>
+          <li>Program</li>
+          <ul>
+            <li>
+              “Engineering” includes geotechnical, aggregate, environmental, survey and other
+              engineering type uses.
+            </li>
+            <li>
+              “Rail Trail” excludes trails that are not a historical rail corridor being used for
+              multi-use trail purposes. Choose “Other” program and “Trail” purpose for other trail
+              uses.
+            </li>
+          </ul>
+          <li>Type</li>
+          <p>
+            Consult Legal Services if in doubt about the agreement type or agreement conditions.
+          </p>
+          <ul>
+            <li>
+              If Amending Agreement is selected, request legal review if amending conditions other
+              than expiry date and payment amounts.
+            </li>
+            <li>Select “Building Lease (receivable)” if leasing both land and building(s).</li>
+          </ul>
+          <li>Purpose</li>
+          <ul>
+            <li>“Camping” includes tree planter camps and campgrounds.</li>
+            <li>
+              “Crossing” includes oil and gas crossings or crossings other than Railway Crossing.
+            </li>
+            <li>
+              “Emergency Services” includes fire halls, ambulance services, RCMP or police stations,
+              etc.
+            </li>
+            <li>“Historical” is for historical files where the purpose is unknown.</li>
+            <li>“Marine Facility” is for facilities other than BC Ferries i.e. dock.</li>
+            <li>“Staging Area” has the same meaning as laydown area.</li>
+          </ul>
+        </ul>
+      </>
+    );
+  };
+
   return (
     <Section header="Administration">
+      <FormGuideContainer
+        tittle="Help with this section"
+        guideBody={guideBodyContent()}
+      ></FormGuideContainer>
       <SectionField label="MOTI contact" labelWidth="2" contentWidth="8">
         <InlineInput field="motiName" />
       </SectionField>
@@ -121,6 +178,7 @@ const AdministrationSubForm: React.FunctionComponent<
           </Col>
         </Row>
       )}
+
       <Row>
         <Col>
           <SectionField label="Purpose" required>
@@ -140,6 +198,7 @@ const AdministrationSubForm: React.FunctionComponent<
           )}
         </Col>
       </Row>
+
       <SectionField
         label="Initiator"
         tooltip="Where did this lease/licence initiate?"
@@ -148,6 +207,7 @@ const AdministrationSubForm: React.FunctionComponent<
       >
         <Select field="initiatorTypeCode" placeholder="Select initiator" options={initiatorTypes} />
       </SectionField>
+
       <Row>
         <Col>
           <SectionField label="Responsibility" tooltip="Who is currently responsible?">
@@ -165,6 +225,7 @@ const AdministrationSubForm: React.FunctionComponent<
           </SectionField>
         </Col>
       </Row>
+
       <SectionField label="Intended use">
         <Styled.MediumTextArea field="description" />
       </SectionField>
