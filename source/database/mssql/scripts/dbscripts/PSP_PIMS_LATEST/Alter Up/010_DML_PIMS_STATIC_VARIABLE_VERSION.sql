b8ab7f34c49b302d4d54715521eac8f60e7eb443
Doug Filteau <dfilteau@hilltopz.com>
2024-10-09 14:01:35 -0700

    IS-91.00 Database Schema
    PSP_PIMS | Development Sprint: S91.00 | Design Sprint: 90 | Date: 2024-Oct-09
    - Altered tables:
      - PIMS_DOCUMENT_TYP
      - PIMS_ACQUISITION_FILE
      - PIMS_DOCUMENT_TYP_HIST
      - PIMS_PROPERTY_ACTIVITY_HIST
      - PIMS_PROPERTY_ACTIVITY
      - PIMS_ACQUISITION_FILE_HIST
    - Added seed script:
      - 007_PSP_PIMS_PROPERTY_ACTIVITY_HIST_Add.sql
      - 008_PSP_PIMS_PROPERTY_ACTIVITY_Trigger.sql
    - Altered seed script:
      - 054_DML_PIMS_LEASE_LICENSE_TYPE.sql
      - 056_DML_PIMS_LEASE_PURPOSE_TYPE.sql
      - 115_DML_PIMS_CONSULTATION_TYPE.sql
    - Requires additional metadata to meet standards

--- source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Up/010_DML_PIMS_STATIC_VARIABLE_VERSION.sql
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Up/010_DML_PIMS_STATIC_VARIABLE_VERSION.sql
@@ -18,7 +18,7 @@ GO
 
 -- Update the database version number.
 DECLARE @CurrVer NVARCHAR(100)
-SET @CurrVer = N'89.00'
+SET @CurrVer = N'91.00'
 
 UPDATE PIMS_STATIC_VARIABLE
 WITH   (UPDLOCK, SERIALIZABLE) 
