052edc532a50fdcacd339f8ece5d0f76c162d913
Doug Filteau <dfilteau@hilltopz.com>
2024-10-09 14:01:35 -0700

    IS-91.00 Database Schema
    PSP_PIMS | Development Sprint: S91.00 | Design Sprint: 90 | Date: 2024-Oct-09
    - Altered tables:
      - PIMS_DOCUMENT_TYP
      - PIMS_ACQUISITION_FILE
      - PIMS_DOCUMENT_TYP_HIST
      - PIMS_PROPERTY_ACTIVITY_HIST
      - PIMS_PROPERTY_ACTIVITY
      - PIMS_ACQUISITION_FILE_HIST
    - Added seed script:
      - 007_PSP_PIMS_PROPERTY_ACTIVITY_HIST_Add.sql
      - 008_PSP_PIMS_PROPERTY_ACTIVITY_Trigger.sql
    - Altered seed script:
      - 054_DML_PIMS_LEASE_LICENSE_TYPE.sql
      - 056_DML_PIMS_LEASE_PURPOSE_TYPE.sql
      - 115_DML_PIMS_CONSULTATION_TYPE.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Up/056_DML_PIMS_LEASE_PURPOSE_TYPE_Alter_Up.sql
@@ -0,0 +1,49 @@
+/* -----------------------------------------------------------------------------
+Alter the data in the PIMS_LEASE_PURPOSE_TYPE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2024-Oct-07  Initial version.
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Update the "ACCRES" type
+PRINT N'Update the "ACCRES" type'
+GO
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'ACCRES'
+
+SELECT LEASE_PURPOSE_TYPE_CODE
+FROM   PIMS_LEASE_PURPOSE_TYPE
+WHERE  LEASE_PURPOSE_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  UPDATE PIMS_LEASE_PURPOSE_TYPE
+  SET    IS_DISABLED                = 1
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  LEASE_PURPOSE_TYPE_CODE = @CurrCd;
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S91_00/Alter Down/115_DML_PIMS_CONSULTATION_TYPE_Alter_Down.sql
@@ -0,0 +1,82 @@
+/* -----------------------------------------------------------------------------
+Alter the data in the PIMS_CONSULTATION_TYPE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2024-Oct-03  Initial version.
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Update the "REGPLANG" type
+PRINT N'Update the "REGPLANG" type'
+GO
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'REGPLANG'
+
+SELECT CONSULTATION_TYPE_CODE
+FROM   PIMS_CONSULTATION_TYPE
+WHERE  CONSULTATION_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  UPDATE PIMS_CONSULTATION_TYPE
+  SET    DESCRIPTION                = N'Regional planning'
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  CONSULTATION_TYPE_CODE = @CurrCd;
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Update the "REGPRPSVC" type
+PRINT N'Update the "REGPRPSVC" type'
+GO
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'REGPRPSVC'
+
+SELECT CONSULTATION_TYPE_CODE
+FROM   PIMS_CONSULTATION_TYPE
+WHERE  CONSULTATION_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  UPDATE PIMS_CONSULTATION_TYPE
+  SET    DESCRIPTION                = N'Regional property services'
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  CONSULTATION_TYPE_CODE = @CurrCd;
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- --------------------------------------------------------------
+-- Update the display order.
+-- --------------------------------------------------------------
+UPDATE tbl
+SET    tbl.DISPLAY_ORDER              = seq.ROW_NUM
+     , tbl.CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+FROM   PIMS_CONSULTATION_TYPE tbl JOIN
+       (SELECT CONSULTATION_TYPE_CODE
+             , ROW_NUMBER() OVER (ORDER BY DESCRIPTION) AS ROW_NUM
+        FROM   PIMS_CONSULTATION_TYPE) seq  ON seq.CONSULTATION_TYPE_CODE = tbl.CONSULTATION_TYPE_CODE
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
