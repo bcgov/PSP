abb34a68e5588576b12a2cf950ec6db28ba0d0c4
Manuel Rodriguez <marobej@gmail.com>
2024-10-23 16:46:24 -0700

    Fixed tests

--- source/frontend/src/features/contacts/contact/create/ContactRouter.tsx
+++ source/frontend/src/features/contacts/contact/create/ContactRouter.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 
+import { ContactTypes } from '../../interfaces';
 import CreateOrganizationForm from './Organization/CreateOrganizationForm';
 import CreatePersonForm from './Person/CreatePersonForm';
 
 export const ContactRouter: React.FunctionComponent<React.PropsWithChildren<unknown>> = () => {
+  const { path, url } = useRouteMatch();
+  console.log(path);
   return (
     <Switch>
-      <Route exact path={`/contact/new/P`}>
+      <Redirect exact from={path} to={`${url}/${ContactTypes.INDIVIDUAL}`} />
+      <Route exact path={`${path}/${ContactTypes.INDIVIDUAL}`}>
         <CreatePersonForm />
       </Route>
-      <Route exact path={`/contact/new/O`}>
+      <Route exact path={`${path}/${ContactTypes.ORGANIZATION}`}>
         <CreateOrganizationForm />
       </Route>
     </Switch>
