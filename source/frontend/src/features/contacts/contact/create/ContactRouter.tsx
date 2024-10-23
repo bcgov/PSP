7a0f465f795e30057d506092cd8015f9317a879c
Manuel Rodriguez <manuel@Manuels-MacBook-Air.local>
2024-10-23 15:25:43 -0700

    improved logic to contacts navigation checking

--- source/frontend/src/features/contacts/contact/create/ContactRouter.tsx
+++ source/frontend/src/features/contacts/contact/create/ContactRouter.tsx
@@ -1,50 +1,18 @@
 import React from 'react';
-import { Redirect, Switch, useRouteMatch } from 'react-router-dom';
-
-import { ContactTypes } from '@/features/contacts/interfaces';
-import AppRoute from '@/utils/AppRoute';
+import { Route, Switch } from 'react-router-dom';
 
 import CreateOrganizationForm from './Organization/CreateOrganizationForm';
 import CreatePersonForm from './Person/CreatePersonForm';
 
-export interface IContactPage {
-  component: any;
-  title: string;
-}
-
-export const pages = new Map<ContactTypes, IContactPage>([
-  [
-    ContactTypes.INDIVIDUAL,
-    {
-      component: CreatePersonForm,
-      title: 'Create Person',
-    },
-  ],
-  [
-    ContactTypes.ORGANIZATION,
-    {
-      component: CreateOrganizationForm,
-      title: 'Create Organization',
-    },
-  ],
-]);
-
 export const ContactRouter: React.FunctionComponent<React.PropsWithChildren<unknown>> = () => {
-  // The `path` lets us build <Route> paths that are relative to the parent route, while
-  // the `url` lets us build relative links.
-  const { path, url } = useRouteMatch();
   return (
     <Switch>
-      <Redirect exact from={path} to={`${url}/${ContactTypes.INDIVIDUAL}`} />
-      {Array.from(pages.entries()).map(([pageName, page]) => (
-        <AppRoute
-          path={`${path}/${pageName}`}
-          customComponent={page.component ?? React.Fragment}
-          title={`${page.title}`}
-          exact
-          key={pageName}
-        ></AppRoute>
-      ))}
+      <Route exact path={`/contact/new/P`}>
+        <CreatePersonForm />
+      </Route>
+      <Route exact path={`/contact/new/O`}>
+        <CreateOrganizationForm />
+      </Route>
     </Switch>
   );
 };
