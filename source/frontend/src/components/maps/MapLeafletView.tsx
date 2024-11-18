80025010b9a0946837ab1c15520530ef9b8e56dd
devinleighsmith <41091511+devinleighsmith@users.noreply.github.com>
2024-11-18 13:31:10 -0800

    HOTFIX: 9469 Set the map bounds correctly - on startup. (#4484)
    * Set the map bounds correctly - on startup.
    
    * add explicit check for isMapReady.
    
    * snapshot updates.
    
    * update logic to support unit tests.

--- source/frontend/src/components/maps/MapLeafletView.tsx
+++ source/frontend/src/components/maps/MapLeafletView.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { dequal } from 'dequal';
 import { Feature, GeoJsonProperties, Geometry } from 'geojson';
 import {
   geoJSON,
@@ -86,6 +87,14 @@ const MapLeafletView: React.FC<React.PropsWithChildren<MapLeafletViewProps>> = (
   const hasPendingFlyTo = mapMachine.pendingFlyTo;
   const requestedFlyTo = mapMachine.requestedFlyTo;
   const mapMachineProcessFlyTo = mapMachine.processFlyTo;
+
+  // Set the bounds when the map is ready. Not called from existing handleMapCreated as that function is called every time a state change occurs.
+  useEffect(() => {
+    const bounds = mapRef?.current?.getBounds();
+    if (exists(bounds) && isMapReady && !dequal(bounds.getNorthEast(), bounds.getSouthWest())) {
+      setBounds(bounds);
+    }
+  }, [isMapReady, setBounds]);
 
   useEffect(() => {
     if (isMapReady && mapMachinePendingRefresh && mapRef.current !== null) {
