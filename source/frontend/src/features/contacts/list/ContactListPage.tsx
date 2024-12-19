491b29b1e2ed3051721f31859b569855a5c42885
Manuel Rodriguez <marobej@gmail.com>
2024-12-18 17:00:13 -0800

    PSP-8573 | Updated list headers (#4531)
    * Updated list headers
    
    * Added icons
    
    * Fixed Typos
    
    * lint fixes
    
    ---------
    
    Co-authored-by: Alejandro Sanchez <emailforasr@gmail.com>

--- source/frontend/src/features/contacts/list/ContactListPage.tsx
+++ source/frontend/src/features/contacts/list/ContactListPage.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
+import ContactIcon from '@/assets/images/contact-icon.svg?react';
 import { Scrollable as ScrollableBase } from '@/components/common/Scrollable/Scrollable';
+import * as CommonStyled from '@/components/common/styles';
 import ContactManagerView from '@/components/contact/ContactManagerView/ContactManagerView';
 
 /**
@@ -10,7 +12,10 @@ export const ContactListPage = () => {
   return (
     <StyledListPage>
       <StyledScrollable>
-        <StyledPageHeader>Contacts</StyledPageHeader>
+        <CommonStyled.H1>
+          <ContactIcon title="Contact manager icon" fill="currentColor" />
+          <span className="ml-2">Contacts</span>
+        </CommonStyled.H1>
         <ContactManagerView showActiveSelector showAddButton />
       </StyledScrollable>
     </StyledListPage>
@@ -30,8 +35,4 @@ const StyledListPage = styled.div`
 const StyledScrollable = styled(ScrollableBase)`
   padding: 1.6rem 3.2rem;
   width: 100%;
-`;
-
-const StyledPageHeader = styled.h3`
-  text-align: left;
 `;
