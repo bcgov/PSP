41bfa3c2e17de803ce4112758fce2afd199e0992
Sue Tairaku <sue.tairaku@gmail.com>
2025-01-31 13:38:23 -0800

    Update Acquisition Details file

--- testing/PIMS.Tests.Automation/PageObjects/AcquisitionDetails.cs
+++ testing/PIMS.Tests.Automation/PageObjects/AcquisitionDetails.cs
@@ -531,33 +531,6 @@ namespace PIMS.Tests.Automation.PageObjects
             Wait();
             while (webDriver.FindElements(acquisitionFileConfirmationModal).Count() > 0)
             {
-                //if (sharedModals.ModalHeader().Contains("Error"))
-                //{
-                //    Assert.Equal("Different Ministry region", sharedModals.ModalHeader());
-                //    Assert.Contains("The selected Ministry region is different from that associated to one or more selected properties", sharedModals.ModalContent());
-                //    Assert.Contains("Do you want to proceed?", sharedModals.ModalContent());
-                //    sharedModals.ModalClickOKBttn();
-
-                //    Wait();
-                //}
-                //else if (sharedModals.ModalContent().Contains("The selected property already exists in the system's inventory."))
-                //{
-                //    Assert.Equal("User Override Required", sharedModals.ModalHeader());
-                //    Assert.Contains("The selected property already exists in the system's inventory. However, the record is missing spatial details.", sharedModals.ModalContent());
-                //    Assert.Contains("To add the property, the spatial details for this property will need to be updated. The system will attempt to update the property record with spatial information from the current selection.", sharedModals.ModalContent());
-                //    sharedModals.ModalClickOKBttn();
-
-                //    Wait();
-                //}
-                //else if (sharedModals.ModalContent().Contains("This change will be reflected on other related entities - generated documents, sub-files, etc."))
-                //{
-                //    Assert.Equal("Different Project or Product", sharedModals.ModalHeader());
-                //    Assert.Contains("This change will be reflected on other related entities - generated documents, sub-files, etc.", sharedModals.ModalContent());
-                //    Assert.Contains("Do you want to proceed?", sharedModals.ModalContent());
-                //    sharedModals.ModalClickOKBttn();
-
-                //    Wait();
-                //}
                 if (sharedModals.ModalHeader().Contains("Error"))
                 {
                     break;
