0087ed7449c3eb2edc5d1540e2121001f23be474
devinleighsmith <devinleighsmith@gmail.com>
2025-01-31 11:00:48 -0800

    header corrections - standardize usage and make sure font size and spacing correct.

--- source/frontend/src/features/contacts/contact/edit/styles.ts
+++ source/frontend/src/features/contacts/contact/edit/styles.ts
@@ -1,28 +1,10 @@
 import { Form as FormikForm } from 'formik';
 import styled from 'styled-components';
 
-import { Button } from '@/components/common/buttons/Button';
-import { Form as FormBase } from '@/components/common/form';
 import { FlexBox } from '@/components/common/styles';
 
 // common ui styling
 export * from '@/features/contacts/styles';
-
-export const H2 = styled.h2`
-  font-size: 2rem;
-  font-weight: 700;
-  color: ${props => props.theme.css.headerTextColor};
-  text-decoration: none solid rgb(0, 51, 102);
-  line-height: 2.9rem;
-`;
-
-export const H3 = styled.h3`
-  font-size: 1.6rem;
-  font-weight: 700;
-  color: ${props => props.theme.bcTokens.typographyColorPrimary};
-  text-decoration: none solid rgb(33, 37, 41);
-  line-height: 2rem;
-`;
 
 export const Form = styled(FormikForm)`
   .form-group {
@@ -33,14 +15,6 @@ export const Form = styled(FormikForm)`
       }
     }
   }
-`;
-
-export const FormLabel = styled(FormBase.Label)`
-  font-size: 1.6rem;
-  font-weight: 700;
-  color: ${props => props.theme.bcTokens.typographyColorPrimary};
-  text-decoration: none solid rgb(33, 37, 41);
-  line-height: 2rem;
 `;
 
 export const ErrorMessage = styled(FlexBox)`
@@ -56,42 +30,10 @@ export const SectionMessage = styled(FlexBox)<{ appearance: 'information' | 'err
   text-decoration: none;
 `;
 
-export const RemoveButton = styled(Button).attrs({ variant: 'link' })`
-  && {
-    color: #aaaaaa;
-    text-decoration: none;
-    line-height: unset;
-    .text {
-      display: none;
-    }
-    &:hover {
-      color: #d8292f;
-      text-decoration: none;
-      .text {
-        display: inline;
-      }
-    }
-  }
-`;
-
-export const SubtleText = styled.span`
-  font-size: 1.6rem;
-  font-weight: 400;
-  color: ${props => props.theme.bcTokens.iconsColorDisabled};
-  text-decoration: none solid rgb(170, 170, 170);
-  line-height: 2rem;
-`;
-
 export const ButtonGroup = styled(FlexBox)`
   width: 100%;
   padding: 1rem 4rem;
   gap: 2rem;
   justify-content: flex-end;
   align-items: stretch;
-`;
-
-export const TopRightCorner = styled.div`
-  position: absolute;
-  top: 2.5rem;
-  right: 2.5rem;
 `;
