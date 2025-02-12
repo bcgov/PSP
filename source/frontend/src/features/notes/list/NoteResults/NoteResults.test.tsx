abed2b0237ee2ee726a9bfca90a47fe9b68bf43b
Alejandro Sanchez <emailforasr@gmail.com>
2025-02-11 21:48:31 -0800

    Test updates

--- source/frontend/src/features/notes/list/NoteResults/NoteResults.test.tsx
+++ source/frontend/src/features/notes/list/NoteResults/NoteResults.test.tsx
@@ -1,12 +1,12 @@
-import noop from 'lodash/noop';
-
 import { Claims } from '@/constants/claims';
-import { mockNotesResponse } from '@/mocks/noteResponses.mock';
-import { mockKeycloak, render, RenderOptions } from '@/utils/test-utils';
+import { getMockApiNote, mockNotesResponse } from '@/mocks/noteResponses.mock';
+import { act, render, RenderOptions, screen, userEvent } from '@/utils/test-utils';
 
 import { INoteResultProps, NoteResults } from './NoteResults';
 
 const setSort = vi.fn();
+const onShowDetails = vi.fn();
+const onDelete = vi.fn();
 
 // mock auth library
 
@@ -18,11 +18,12 @@ const setup = (renderOptions: RenderOptions & Partial<INoteResultProps> = { resu
       sort={{}}
       results={results ?? []}
       setSort={setSort}
-      onDelete={noop}
-      onShowDetails={noop}
+      onDelete={onDelete}
+      onShowDetails={onShowDetails}
     />,
     {
       ...rest,
+      claims: rest.claims ?? [Claims.NOTE_VIEW],
     },
   );
   const tableRows = utils.container.querySelectorAll('.table .tbody .tr-wrapper');
@@ -39,7 +40,6 @@ const setup = (renderOptions: RenderOptions & Partial<INoteResultProps> = { resu
 
 describe('Note Results Table', () => {
   beforeEach(() => {
-    mockKeycloak({ claims: [Claims.NOTE_DELETE] });
     setSort.mockClear();
   });
 
@@ -54,5 +54,35 @@ describe('Note Results Table', () => {
     expect(tableRows.length).toBe(0);
     const toasts = await findAllByText('No matching Notes found');
     expect(toasts[0]).toBeVisible();
+  });
+
+  it('displays the eye icon-button to open note details popup', async () => {
+    setup({
+      results: [getMockApiNote()],
+    });
+
+    const viewButton = await screen.findByTitle(/View Note/i);
+    expect(viewButton).toBeVisible();
+    await act(async () => userEvent.click(viewButton));
+    expect(onShowDetails).toHaveBeenCalled();
+  });
+
+  it('displays the trashcan button to delete note, when the user has permissions', async () => {
+    setup({
+      results: [getMockApiNote()],
+      claims: [Claims.NOTE_VIEW, Claims.NOTE_DELETE],
+    });
+
+    const deleteButton = await screen.findByTitle(/Delete Note/i);
+    expect(deleteButton).toBeVisible();
+    await act(async () => userEvent.click(deleteButton));
+    expect(onDelete).toHaveBeenCalled();
+  });
+
+  it('displays the correct date when note is created after 5pm PST', async () => {
+    setup({
+      results: [{ ...getMockApiNote(), appCreateTimestamp: '2025-02-12T00:59:37.953' }],
+    });
+    expect(await screen.findByText('Feb 11, 2025')).toBeVisible();
   });
 });
