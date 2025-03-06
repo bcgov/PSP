3e85920d182294cfc2be9540f04d92e053baac3f
Alejandro Sanchez <emailforasr@gmail.com>
2025-03-05 18:04:26 -0800

    Code cleanup

--- source/frontend/src/telemetry/metrics.ts
+++ source/frontend/src/telemetry/metrics.ts
@@ -1,5 +1,12 @@
+import { metrics } from '@opentelemetry/api';
+import {
+  ATTR_HTTP_RESPONSE_STATUS_CODE,
+  METRIC_HTTP_CLIENT_REQUEST_DURATION,
+  METRIC_HTTP_CLIENT_RESPONSE_BODY_SIZE,
+} from '@opentelemetry/semantic-conventions/incubating';
+
 import { MetricsConfig } from './config';
-import { isBlocked, isBrowserEnvironment, MeterKind, registerMeterProvider } from './utils';
+import { isBlocked, isBrowserEnvironment, NETWORK_METER } from './utils';
 
 export const registerNetworkMetrics = (config: MetricsConfig) => {
   if (!isBrowserEnvironment() || !window.performance) {
@@ -10,51 +17,62 @@ export const registerNetworkMetrics = (config: MetricsConfig) => {
     console.info('Registering Network Request metrics');
   }
 
-  const meter = registerMeterProvider(MeterKind.Network, config);
+  const meter = metrics.getMeter(NETWORK_METER);
 
   // measure the time it takes to make an XHR request
-  const timeSpentMetric = meter.createHistogram('network_request_time_spent', {
-    description: 'Time Spent on Request',
-    unit: 'milliseconds',
+  const timeSpentMetric = meter.createHistogram(METRIC_HTTP_CLIENT_REQUEST_DURATION, {
+    description: 'The duration of an outgoing HTTP request.',
+    unit: 's',
+    advice: {
+      explicitBucketBoundaries: [
+        0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10,
+      ],
+    },
   });
 
   // collect metrics regarding the payload size of network requests
-  const sizeMetric = meter.createHistogram('network_request_size', {
-    description: 'Size of Network Request',
-    unit: 'bytes',
+  const sizeMetric = meter.createGauge(METRIC_HTTP_CLIENT_RESPONSE_BODY_SIZE, {
+    description: 'Size of network response',
+    unit: 'byte',
   });
 
   // Use PerformanceObserver Browser API to track these things
   // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver
   const observer = new PerformanceObserver(list => {
-    const entries = list.getEntries();
-    entries.forEach(entry => {
+    const entries = list.getEntriesByType('resource');
+    entries.forEach((entry: PerformanceResourceTiming) => {
       if (
         'initiatorType' in entry &&
         (entry.initiatorType === 'xmlhttprequest' || entry.initiatorType === 'fetch')
       ) {
-        const uri = entry.name;
-        const timeSpent = entry.duration;
-        const size: number =
-          ('transferSize' in entry ? (entry.transferSize as number) : 0) ||
-          ('encodedBodySize' in entry ? (entry.encodedBodySize as number) : 0);
+        const uri = new URL(entry.name);
+        uri.search = '';
+        const sanitizedUri = uri.toString();
 
         // do not collect metrics from blacklisted sites
-        if (!isBlocked(uri, config)) {
-          const attributes = {
-            uri,
-            route: window.location.pathname,
-            environment: config.environment,
-          };
+        if (isBlocked(sanitizedUri, config)) {
+          return;
+        }
 
-          timeSpentMetric.record(timeSpent, attributes);
-          if (size !== 0) {
-            sizeMetric.record(size, attributes);
-          }
+        // convert from ms to seconds
+        const timeSpent = entry.duration / 1000;
+        const size: number = entry.transferSize || entry.encodedBodySize || 0;
+        const statusCode = entry.responseStatus;
+
+        const attributes = {
+          uri: sanitizedUri,
+          route: window.location.pathname,
+          environment: config.environment,
+          [ATTR_HTTP_RESPONSE_STATUS_CODE]: statusCode,
+        };
+
+        timeSpentMetric.record(timeSpent, attributes);
+        if (size !== 0) {
+          sizeMetric.record(size, attributes);
         }
 
         if (config.debug) {
-          console.info({ uri, timeSpent, size });
+          console.info({ uri, timeSpent, size, attributes });
         }
       }
     });
