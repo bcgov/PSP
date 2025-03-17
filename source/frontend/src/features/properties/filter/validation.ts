abd68c30ce4c9187d23eaabed3e627b8783e60a5
Alejandro Sanchez <emailforasr@gmail.com>
2025-03-17 10:16:45 -0700

    Ensure that pid/pin validations display

--- source/frontend/src/features/properties/filter/validation.ts
+++ source/frontend/src/features/properties/filter/validation.ts
@@ -4,8 +4,10 @@ import * as Yup from 'yup';
 import { createNumberSchema } from '@/utils/YupSchema';
 
 export const PropertyFilterValidationSchema = Yup.object().shape({
-  pid: Yup.string().nullable(),
-  pin: Yup.string().nullable(),
+  pid: Yup.string()
+    .nullable()
+    .matches(/^\d{0,3}-\d{3}-\d{3}$|^\d{0,9}$/, 'Invalid PID'),
+  pin: Yup.string().nullable().matches(/^\d+$/, 'Invalid PIN'),
   coordinates: Yup.object().when('searchBy', {
     is: 'coordinates',
     then: Yup.object().shape({
