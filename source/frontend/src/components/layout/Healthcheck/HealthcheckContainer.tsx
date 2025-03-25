cd21afba1a64d03fefdadfe488554ad1d976bc64
Herrera <eduardo.herrera@quartech.com>
2025-03-25 12:21:51 -0700

    - Quality updates

--- source/frontend/src/components/layout/Healthcheck/HealthcheckContainer.tsx
+++ source/frontend/src/components/layout/Healthcheck/HealthcheckContainer.tsx
@@ -29,6 +29,85 @@ export const HealthcheckContainer: React.FunctionComponent<IHealthcheckContainer
   const { getLive, getSystemCheck } = useApiHealth();
   const keycloak = useKeycloakWrapper();
 
+  const handleError = useCallback(
+    async (axiosError: AxiosError<IApiError>): Promise<void> => {
+      const systemIssues: IHealthCheckIssue[] = [];
+
+      // 500 - API NOT Responding
+      if (axiosError?.response?.status === 500) {
+        systemIssues.push({
+          key: HealthcheckMessagesTypesEnum.PIMS_API,
+          msg: pimsHealthcheckMessages[
+            HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.PIMS_API]
+          ],
+        });
+        updateHealthcheckResult(true);
+      }
+
+      // 503 - API responding service not available
+      if (axiosError?.response?.status === 503) {
+        const data = axiosError?.response?.data as unknown as ISystemCheck;
+        if (data.entries?.Geoserver && data.entries.Geoserver?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.GEOSERVER,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.GEOSERVER]
+            ],
+          });
+        }
+
+        if (data.entries?.PmbcExternalApi && data.entries.PmbcExternalApi?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.PMBC,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.PMBC]
+            ],
+          });
+        }
+
+        if (data.entries?.Mayan && data.entries.Mayan?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.MAYAN,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.MAYAN]
+            ],
+          });
+        }
+
+        if (data.entries?.Ltsa && data.entries.Ltsa?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.LTSA,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.LTSA]
+            ],
+          });
+        }
+
+        if (data.entries?.Geocoder && data.entries.Geocoder?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.GEOCODER,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.GEOCODER]
+            ],
+          });
+        }
+
+        if (data.entries?.Cdogs && data.entries.Cdogs?.status !== 'Healthy') {
+          systemIssues.push({
+            key: HealthcheckMessagesTypesEnum.CDOGS,
+            msg: pimsHealthcheckMessages[
+              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.CDOGS]
+            ],
+          });
+        }
+
+        setHealthCheckIssues(systemIssues);
+        updateHealthcheckResult(true);
+      }
+    },
+    [pimsHealthcheckMessages, updateHealthcheckResult],
+  );
+
   const fetchSystemCheckInformation = useCallback(async () => {
     const systemIssues: IHealthCheckIssue[] = [];
     let systemDegraded = false;
@@ -55,82 +134,12 @@ export const HealthcheckContainer: React.FunctionComponent<IHealthcheckContainer
     } catch (e) {
       if (axios.isAxiosError(e)) {
         const axiosError = e as AxiosError<IApiError>;
-        // 500 - API NOT Responding
-        if (axiosError?.response?.status === 500) {
-          systemIssues.push({
-            key: HealthcheckMessagesTypesEnum.PIMS_API,
-            msg: pimsHealthcheckMessages[
-              HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.PIMS_API]
-            ],
-          });
-          updateHealthcheckResult(true);
-        }
-
-        // 503 - API responding service not available
-        if (axiosError?.response?.status === 503) {
-          const data = axiosError?.response?.data as unknown as ISystemCheck;
-          if (data.entries?.Geoserver && data.entries.Geoserver?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.GEOSERVER,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.GEOSERVER]
-              ],
-            });
-          }
-
-          if (data.entries?.PmbcExternalApi && data.entries.PmbcExternalApi?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.PMBC,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.PMBC]
-              ],
-            });
-          }
-
-          if (data.entries?.Mayan && data.entries.Mayan?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.MAYAN,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.MAYAN]
-              ],
-            });
-          }
-
-          if (data.entries?.Ltsa && data.entries.Ltsa?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.LTSA,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.LTSA]
-              ],
-            });
-          }
-
-          if (data.entries?.Geocoder && data.entries.Geocoder?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.GEOCODER,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.GEOCODER]
-              ],
-            });
-          }
-
-          if (data.entries?.Cdogs && data.entries.Cdogs?.status !== 'Healthy') {
-            systemIssues.push({
-              key: HealthcheckMessagesTypesEnum.CDOGS,
-              msg: pimsHealthcheckMessages[
-                HealthcheckMessagesTypesEnum[HealthcheckMessagesTypesEnum.CDOGS]
-              ],
-            });
-          }
-
-          setHealthCheckIssues(systemIssues);
-          updateHealthcheckResult(true);
-        }
+        handleError(axiosError);
       }
     } finally {
       setSystemChecked(true);
     }
-  }, [getLive, getSystemCheck, pimsHealthcheckMessages, updateHealthcheckResult]);
+  }, [getLive, getSystemCheck, handleError, pimsHealthcheckMessages, updateHealthcheckResult]);
 
   useEffect(() => {
     if (systemChecked == null && keycloak.obj.authenticated) {
