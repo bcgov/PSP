246f04119248f3fb2ff6e702a18906413065b6d9
Alejandro Sanchez <emailforasr@gmail.com>
2025-04-05 13:00:03 -0700

    EF partial classes

---
+++ source/backend/entities/Partials/ExpropOwnerHistory.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pims.Dal.Entities
+{
+    /// <summary>
+    /// Entity continaing the details regarding actions involving a property owner associated with an expropriation.
+    /// </summary>
+    public partial class PimsExpropOwnerHistory : StandardIdentityBaseAppEntity<long>, IBaseAppEntity
+    {
+        [NotMapped]
+        public override long Internal_Id { get => this.ExpropOwnerHistoryId; set => this.ExpropOwnerHistoryId = value; }
+    }
+}
