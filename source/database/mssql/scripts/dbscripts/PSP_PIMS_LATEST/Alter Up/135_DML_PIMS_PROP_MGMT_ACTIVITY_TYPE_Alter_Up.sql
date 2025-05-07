419b57aab2eaeb82bf85985d04afc2e2d9636376
dfilteau <dfilteau@hilltopz.com>
2025-05-07 13:07:42 -0700

    IS-104.00 Database Schema
    PSP_PIMS | Development Sprint: S104.00 | Design Sprint: 103 | Date: 2025-May-07
    - Added tables:
      - PIMS_MANAGEMENT_FILE_PURPOSE_TYPE
      - PIMS_PROPERTY_DOCUMENT
      - PIMS_PROPERTY_DOCUMENT_HIST
      - PIMS_PROPERTY_NOTE
      - PIMS_PROPERTY_NOTE_HIST
      - PIMS_TAX_RESPONSIBILITY_TYPE
      - PIMS_UTILITY_RESPONSIBILITY_TYPE
    - Altered tables:
      - PIMS_MANAGEMENT_FILE
      - PIMS_MANAGEMENT_FILE_HIST
      - PIMS_MANAGEMENT_FILE_DOCUMENT
      - PIMS_MANAGEMENT_FILE_DOCUMENT_HIST
      - PIMS_MANAGEMENT_FILE_NOTE
      - PIMS_MANAGEMENT_FILE_NOTE_HIST
      - PIMS_MANAGEMENT_FILE_PROPERTY
      - PIMS_MANAGEMENT_FILE_PROPERTY_HIST
      - PIMS_PROPERTY
      - PIMS_PROPERTY_HIST
    - Added views:
      - PIMS_PROPERTY_BOUNDARY_ACQUISITION_VW
      - PIMS_PROPERTY_BOUNDARY_DISPOSITION_VW
      - PIMS_PROPERTY_BOUNDARY_INTEREST_VW
      - PIMS_PROPERTY_BOUNDARY_LEASE_VW
      - PIMS_PROPERTY_BOUNDARY_MANAGEMENT_VW
      - PIMS_PROPERTY_BOUNDARY_RESEARCH_VW
      - PIMS_PROPERTY_BOUNDARY_RETIRED_VW
    - Added seed scripts:
      - 168_DML_PIMS_MANAGEMENT_FILE_PURPOSE_TYPE.sql
      - 169_DML_PIMS_UTILITY_RESPONSIBILITY_TYPE.sql
      - 170_DML_PIMS_TAX_RESPONSIBILITY_TYPE.sql
    - Altered seed scripts:
      - 090_DML_PIMS_ACQUISITION_TYPE.sql
      - 135_DML_PIMS_PROP_MGMT_ACTIVITY_TYPE.sql
      - 136_DML_PIMS_PROP_MGMT_ACTIVITY_SUBTYPE.sql
    - Dropped seed scripts:
      - 168_DML_PIMS_MANAGEMENT_FILE_PROGRAM_TYPE.sql
    - Altered test scripts:
      - 119_PIMS_MANAGEMENT_FILE.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Up/135_DML_PIMS_PROP_MGMT_ACTIVITY_TYPE_Alter_Up.sql
@@ -0,0 +1,74 @@
+/* -----------------------------------------------------------------------------
+Alter the PIMS_PROP_MGMT_ACTIVITY_TYPE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2025-May-07  Initial version.
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Add/Enable the "CONSULTATION" type
+PRINT N'Add/Enable the "CONSULTATION" type'
+GO
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'CONSULTATION'
+
+SELECT PROP_MGMT_ACTIVITY_TYPE_CODE
+FROM   PIMS_PROP_MGMT_ACTIVITY_TYPE
+WHERE  PROP_MGMT_ACTIVITY_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  UPDATE PIMS_PROP_MGMT_ACTIVITY_TYPE
+  SET    IS_DISABLED                = 0
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  PROP_MGMT_ACTIVITY_TYPE_CODE = @CurrCd;
+ELSE
+  INSERT INTO PIMS_PROP_MGMT_ACTIVITY_TYPE (PROP_MGMT_ACTIVITY_TYPE_CODE, DESCRIPTION)
+  VALUES (N'CONSULTATION', N'Consultation (other)');  
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Add/Enable the "TRAILMTC" type
+PRINT N'Add/Enable the "TRAILMTC" type'
+GO
+DECLARE @CurrCd NVARCHAR(20)
+SET     @CurrCd = N'TRAILMTC'
+
+SELECT PROP_MGMT_ACTIVITY_TYPE_CODE
+FROM   PIMS_PROP_MGMT_ACTIVITY_TYPE
+WHERE  PROP_MGMT_ACTIVITY_TYPE_CODE = @CurrCd;
+
+IF @@ROWCOUNT = 1
+  UPDATE PIMS_PROP_MGMT_ACTIVITY_TYPE
+  SET    IS_DISABLED                = 0
+       , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+  WHERE  PROP_MGMT_ACTIVITY_TYPE_CODE = @CurrCd;
+ELSE
+  INSERT INTO PIMS_PROP_MGMT_ACTIVITY_TYPE (PROP_MGMT_ACTIVITY_TYPE_CODE, DESCRIPTION)
+  VALUES (N'TRAILMTC', N'Trail Maintenance');
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
