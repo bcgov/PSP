8afd19aa283d69ee707dc0601f8b63b90f965ccb
dfilteau <dfilteau@hilltopz.com>
2025-07-09 13:40:34 -0700

    IS107.00 Database Schema
    PSP_PIMS | Development Sprint: S107.00 | Design Sprint: 106 | Date: 2025-Jul-09
    - Added tables:
      - PIMS_MANAGEMENT_ACTIVITY
      - PIMS_MANAGEMENT_ACTIVITY_HIST
      - PIMS_MANAGEMENT_ACTIVITY_PROPERTY
      - PIMS_MANAGEMENT_ACTIVITY_PROPERTY_HIST
      - PIMS_MANAGEMENT_FILE_CONTACT
      - PIMS_MANAGEMENT_FILE_CONTACT_HIST
    - Dropped tables:
      - PIMS_PROP_PROP_ACTIVITY
      - PIMS_PROP_PROP_ACTIVITY_HIST
      - PIMS_PROPERTY_ACTIVITY
      - PIMS_PROPERTY_ACTIVITY_HIST
    - Altered tables:
      - PIMS_ACQUISITION_FILE
      - PIMS_PROP_ACT_MIN_CONTACT
      - PIMS_PROP_ACT_MIN_CONTACT_HIST
      - PIMS_PROP_ACT_INVOLVED_PARTY
      - PIMS_PROP_ACT_INVOLVED_PARTY_HIST
      - PIMS_PROP_ACTIVITY_MGMT_ACTIVITY
      - PIMS_PROP_ACTIVITY_MGMT_ACTIVITY_HIST
      - PIMS_PROPERTY
      - PIMS_PROPERTY_HIST
      - PIMS_PROPERTY_ACTIVITY_INVOICE_HIST
      - PIMS_PROPERTY_CONTACT
      - PIMS_PROPERTY_ACTIVITY_DOCUMENT
      - PIMS_PROPERTY_ACTIVITY_DOCUMENT_HIST
      - PIMS_PROPERTY_ACTIVITY_INVOICE
    - Added test scripts:
      - 125_PIMS_NOTE.sql
      - 126_PIMS_PROPERTY_NOTE.sql
      - 127_PIMS_PROP_TENURE_CLEANUP.sql
      - 128_PIMS_MANAGEMENT_FILE_CONTACT.sql
      - 129_PIMS_EXPROP_OWNER_HISTORY.sql
    - Altered test scripts:
      - 005_DML_PIMS_PROPERTY.sql
      - 051_PIMS_MANAGEMENT_ACTIVITY.sql
      - 052_PIMS_PROPERTY_ACTIVITY_INVOICE.sql
      - 053_PIMS_PROP_ACT_MIN_CONTACT.sql
      - 054_PIMS_PROP_ACT_INVOLVED_PARTY.sql
      - 055_PIMS_PROP_PROP_ACTIVITY.sql
      - 056_PIMS_PROP_ACT_MIN_CONTACT.sql
      - 107_PIMS_PROPERTY_ACTIVITY_DOCUMENT.sql
      - 124_PIMS_PROP_ACTIVITY_MGMT_ACTIVITY.sql
    - Altered seed scripts:
      - 087_DML_PIMS_PPH_STATUS_TYPE.sql
      - 007_PSP_PIMS_PROPERTY_ACTIVITY_HIST_Add.sql
      - 008_PSP_PIMS_PROPERTY_ACTIVITY_Trigger.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Alter Down/000_Load_TMP_PROPERTY_ACTIVITY_Alter Down.sql
@@ -0,0 +1,105 @@
+/* -----------------------------------------------------------------------------
+Create and populate the temporary TMP_PROPERTY_ACTIVITY table.  This data will
+be migrated to the PIMS_MANAGEMENT_ACTIVITY table in IS-107.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2025-Jul-07  Initial version
+----------------------------------------------------------------------------- */
+
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Drop the temporary table
+PRINT N'Drop the temporary table'
+GO
+DROP TABLE IF EXISTS dbo.TMP_PROPERTY_ACTIVITY
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Create the temporary table
+PRINT N'Create the temporary table'
+GO
+CREATE TABLE dbo.TMP_PROPERTY_ACTIVITY (
+	PIMS_PROPERTY_ACTIVITY_ID          	bigint,
+	PROP_MGMT_ACTIVITY_STATUS_TYPE_CODE	nvarchar(20),
+	SERVICE_PROVIDER_PERSON_ID         	bigint,
+	SERVICE_PROVIDER_ORG_ID            	bigint,
+	MANAGEMENT_FILE_ID                 	bigint,
+	PROP_MGMT_ACTIVITY_TYPE_CODE       	nvarchar(20),
+	REQUEST_ADDED_DT                   	date,
+	COMPLETION_DT                      	date,
+	DESCRIPTION                        	nvarchar(max),
+	REQUEST_SOURCE                     	nvarchar(2000),
+	IS_DISABLED                        	bit,
+	CONCURRENCY_CONTROL_NUMBER         	bigint,
+	APP_CREATE_TIMESTAMP               	datetime,
+	APP_CREATE_USERID                  	nvarchar(30),
+	APP_CREATE_USER_GUID               	uniqueidentifier,
+	APP_CREATE_USER_DIRECTORY          	nvarchar(30),
+	APP_LAST_UPDATE_TIMESTAMP          	datetime,
+	APP_LAST_UPDATE_USERID             	nvarchar(30),
+	APP_LAST_UPDATE_USER_GUID          	uniqueidentifier,
+	APP_LAST_UPDATE_USER_DIRECTORY     	nvarchar(30),
+	DB_CREATE_TIMESTAMP                	datetime,
+	DB_CREATE_USERID                   	nvarchar(30),
+	DB_LAST_UPDATE_TIMESTAMP           	datetime,
+	DB_LAST_UPDATE_USERID              	nvarchar(30)
+  )
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+-- Migrate the PIMS_MANAGEMENT_ACTIVITY data to the TMP_PROPERTY_ACTIVITY table
+PRINT N'Migrate the PIMS_MANAGEMENT_ACTIVITY data to the TMP_PROPERTY_ACTIVITY table'
+GO
+INSERT INTO dbo.TMP_PROPERTY_ACTIVITY (PIMS_PROPERTY_ACTIVITY_ID, PROP_MGMT_ACTIVITY_STATUS_TYPE_CODE, SERVICE_PROVIDER_PERSON_ID, SERVICE_PROVIDER_ORG_ID, MANAGEMENT_FILE_ID, PROP_MGMT_ACTIVITY_TYPE_CODE, REQUEST_ADDED_DT, COMPLETION_DT, DESCRIPTION, REQUEST_SOURCE, IS_DISABLED, CONCURRENCY_CONTROL_NUMBER, APP_CREATE_TIMESTAMP, APP_CREATE_USERID, APP_CREATE_USER_GUID, APP_CREATE_USER_DIRECTORY, APP_LAST_UPDATE_TIMESTAMP, APP_LAST_UPDATE_USERID, APP_LAST_UPDATE_USER_GUID, APP_LAST_UPDATE_USER_DIRECTORY, DB_CREATE_TIMESTAMP, DB_CREATE_USERID, DB_LAST_UPDATE_TIMESTAMP, DB_LAST_UPDATE_USERID)
+  SELECT PIMS_MANAGEMENT_ACTIVITY_ID
+       , PROP_MGMT_ACTIVITY_STATUS_TYPE_CODE
+       , SERVICE_PROVIDER_PERSON_ID
+       , SERVICE_PROVIDER_ORG_ID
+       , MANAGEMENT_FILE_ID
+       , PROP_MGMT_ACTIVITY_TYPE_CODE
+       , REQUEST_ADDED_DT
+       , COMPLETION_DT
+       , DESCRIPTION
+       , REQUEST_SOURCE
+       , IS_DISABLED
+       , CONCURRENCY_CONTROL_NUMBER
+       , APP_CREATE_TIMESTAMP
+       , APP_CREATE_USERID
+       , APP_CREATE_USER_GUID
+       , APP_CREATE_USER_DIRECTORY
+       , APP_LAST_UPDATE_TIMESTAMP
+       , APP_LAST_UPDATE_USERID
+       , APP_LAST_UPDATE_USER_GUID
+       , APP_LAST_UPDATE_USER_DIRECTORY
+       , DB_CREATE_TIMESTAMP
+       , DB_CREATE_USERID
+       , DB_LAST_UPDATE_TIMESTAMP
+       , DB_LAST_UPDATE_USERID
+  FROM   dbo.PIMS_MANAGEMENT_ACTIVITY
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+
+COMMIT TRANSACTION
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
+DECLARE @Success AS BIT
+SET @Success = 1
+SET NOEXEC OFF
+IF (@Success = 1) PRINT 'The database update succeeded'
+ELSE BEGIN
+   IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION
+   PRINT 'The database update failed'
+END
+GO
