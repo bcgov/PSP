060ffd7f807a6f559848d86d42d7980b18d2312f
dfilteau <dfilteau@hilltopz.com>
2025-07-29 09:16:35 -0700

    IS-108.00 Database Schema
    PSP_PIMS | Development Sprint: S108.00 | Design Sprint: 107 | Date: 2025-Jul-28
    - Added tables:
      - PIMS_MANAGEMENT_ACTIVITY_INVOICE
      - PIMS_MANAGEMENT_ACTIVITY_INVOICE_HIST
      - PIMS_MGMT_ACT_INVOLVED_PARTY
      - PIMS_MGMT_ACT_INVOLVED_PARTY_HIST
      - PIMS_MGMT_ACT_MIN_CONTACT
      - PIMS_MGMT_ACT_MIN_CONTACT_HIST
      - PIMS_MGMT_ACTIVITY_ACTIVITY_SUBTYP
      - PIMS_MGMT_ACTIVITY_ACTIVITY_SUBTYP_HIST
      - PIMS_MGMT_ACTIVITY_DOCUMENT
      - PIMS_MGMT_ACTIVITY_DOCUMENT_HIST
      - PIMS_MGMT_ACTIVITY_STATUS_TYPE
      - PIMS_MGMT_ACTIVITY_SUBTYPE
      - PIMS_MGMT_ACTIVITY_TYPE
    - Dropped tables:
      - PIMS_PROP_ACT_INVOLVED_PARTY
      - PIMS_PROP_ACT_INVOLVED_PARTY_HIST
      - PIMS_PROP_ACT_MIN_CONTACT
      - PIMS_PROP_ACT_MIN_CONTACT_HIST
      - PIMS_PROP_ACTIVITY_MGMT_ACTIVITY
      - PIMS_PROP_ACTIVITY_MGMT_ACTIVITY_HIST
      - PIMS_PROP_MGMT_ACTIVITY_STATUS_TYPE
      - PIMS_PROP_MGMT_ACTIVITY_SUBTYPE
      - PIMS_PROP_MGMT_ACTIVITY_TYPE
      - PIMS_PROPERTY_ACTIVITY_DOCUMENT
      - PIMS_PROPERTY_ACTIVITY_DOCUMENT_HIST
      - PIMS_PROPERTY_ACTIVITY_INVOICE
      - PIMS_PROPERTY_ACTIVITY_INVOICE_HIST
    - Altered tables:
      - PIMS_ACQUISITION_FILE
      - PIMS_MANAGEMENT_ACTIVITY
      - PIMS_MANAGEMENT_ACTIVITY_HIST
      - PIMS_MANAGEMENT_ACTIVITY_PROPERTY
      - PIMS_MANAGEMENT_ACTIVITY_PROPERTY_HIST
      - PIMS_PROPERTY
      - PIMS_PROPERTY_HIST
    - Altered views:
      - PIMS_PROPERTY_VW
    - Altered test scripts:
      - 051_PIMS_MANAGEMENT_ACTIVITY.sql
      - 052_PIMS_MANAGEMENT_ACTIVITY_INVOICE.sql
      - 053_PIMS_MGMT_ACT_MIN_CONTACT.sql
      - 054_PIMS_MGMT_ACT_INVOLVED_PARTY.sql
      - 055_PIMS_MANAGEMENT_ACTIVITY_PROPERTY.sql
      - 056_PIMS_MGMT_ACT_MIN_CONTACT.sql
      - 107_PIMS_MGMT_ACTIVITY_DOCUMENT.sql
      - 124_PIMS_MGMT_ACTIVITY_ACTIVITY_SUBTYP.sql
    - Altered seed scripts:
      - 007_PSP_PIMS_MANAGEMENT_ACTIVITY_HIST_Add.sql
      - 134_DML_PIMS_MGMT_ACTIVITY_STATUS_TYPE.sql
      - 136_DML_PIMS_MGMT_ACTIVITY_SUBTYPE.sql
      - 135_DML_PIMS_MGMT_ACTIVITY_TYPE.sql
    - Requires additional metadata to meet standards

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_LATEST/Build/136_DML_PIMS_MGMT_ACTIVITY_SUBTYPE.sql
@@ -0,0 +1,121 @@
+/* -----------------------------------------------------------------------------
+Populate the PIMS_PROP_MGMT_ACTIVITY_SUBTYPE table.
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+Author        Date         Comment
+------------  -----------  -----------------------------------------------------
+Doug Filteau  2023-Sep-11  Initial version.
+Doug Filteau  2024-Feb-26  Added UTILITYBILL and TAXESLEVIES.
+Doug Filteau  2024-Jul-03  Add/enable PROPADMIN and remove the leading space
+                           from WATERANDSEWER.
+Doug Filteau  2024-Jul-11  Added BYLAWINFRAC.
+Doug Filteau  2025-May-07  Added CONSULTATION and TRAILMTC.
+Doug Filteau  2025-Jun-27  Added UNKNOWN activity subtype.
+Doug Filteau  2025-Jul-17  PIMS_PROP_MGMT_ACTIVITY_SUBTYPE table renamed to 
+                           PIMS_MGMT_ACTIVITY_SUBTYPE.
+----------------------------------------------------------------------------- */
+
+DELETE FROM PIMS_MGMT_ACTIVITY_SUBTYPE
+GO
+
+INSERT INTO PIMS_MGMT_ACTIVITY_SUBTYPE (MGMT_ACTIVITY_TYPE_CODE, MGMT_ACTIVITY_SUBTYPE_CODE, DESCRIPTION)
+VALUES
+  (N'PROPERTYMGMT',  N'PROPADMIN',        N'Administration'),
+
+  (N'PROPERTYMTC',   N'ARBORIST',         N'Danger tree removal/arborist report'),
+  (N'PROPERTYMTC',   N'DEMOLITION',       N'Demolition'),
+  (N'PROPERTYMTC',   N'SITEINSPECT',      N'Site Inspection'),
+  (N'PROPERTYMTC',   N'LANDSCAPING',      N'Landscaping'),
+  (N'PROPERTYMTC',   N'JANITORIAL',       N'Janitorial'),
+  (N'PROPERTYMTC',   N'WINDOWCLEAN',      N'Window Cleaning'),
+  (N'PROPERTYMTC',   N'SNOWREMOVAL',      N'Snow Removal'),
+  (N'PROPERTYMTC',   N'HVAC',             N'HVAC'),
+  (N'PROPERTYMTC',   N'ELEVATOR',         N'Elevator'),
+  (N'PROPERTYMTC',   N'LIGHTING',         N'Lighting'),
+  (N'PROPERTYMTC',   N'GARBAGERECYCLING', N'Garbage/Recycling'),
+  (N'PROPERTYMTC',   N'WATERANDSEWER',    N'Water and Sewer'),
+  (N'PROPERTYMTC',   N'HEATINGFUEL',      N'Heating Fuel'),
+  (N'PROPERTYMTC',   N'ELECTRICAL',       N'Electrical'),
+  (N'PROPERTYMTC',   N'SECURITY',         N'Security'),
+  (N'PROPERTYMTC',   N'FIRESAFETY',       N'Fire Safety'),
+  (N'PROPERTYMTC',   N'TELEPHONE',        N'Telephone'),
+  (N'PROPERTYMTC',   N'INTERNET',         N'Internet'),
+  (N'PROPERTYMTC',   N'PARKINGLOT',       N'Parking Lot'),      
+
+  (N'LANDLORDIMPRV', N'LL_PREMISES',      N'Premises'),
+  (N'LANDLORDIMPRV', N'LL_BUILDGLAND',    N'Building/Land'),
+  
+  (N'TENANTIMPROV',  N'TN_PREMISES',      N'Premises'),   
+  (N'TENANTIMPROV',  N'TN_BUILDGLAND',    N'Building/Land'),
+  
+  (N'INCDNTISSUE',   N'TRESPASS',         N'Trespass'),
+  (N'INCDNTISSUE',   N'ENCAMPMENT',       N'Encampment'),
+  (N'INCDNTISSUE',   N'VANDALISM',        N'Vandalism'),
+  (N'INCDNTISSUE',   N'GARBAGEDUMP',      N'Garbage/Dumping'),
+  (N'INCDNTISSUE',   N'BYLAWINFRAC',      N'By-Law Infraction'),
+  
+  (N'INQUIRY',       N'GENINQUIRY',       N'General Inquiry'),
+  (N'INQUIRY',       N'EXPRINTEREST',     N'Expression of Interest'),
+  
+  (N'INVESTRPT',     N'APPRAISAL',        N'Appraisal'),
+  (N'INVESTRPT',     N'ENGINEERiNG',      N'Engineering'),
+  (N'INVESTRPT',     N'ENVIRONMENT',      N'Environmental'),
+  (N'INVESTRPT',     N'GEOTECH',          N'Geotechnical'),
+  (N'INVESTRPT',     N'LEGALSURVEY',      N'Legal Survey'),
+  (N'INVESTRPT',     N'LANDUSEPLAN',      N'Land Use Planning'),
+
+  (N'1STNTNCONSULT', N'STRENOFCLAIM',     N'Strength of Claim Assessment'),
+  (N'1STNTNCONSULT', N'CONSULTREC',       N'Consultation Record'),
+  (N'1STNTNCONSULT', N'ACCOMMAGREE',      N'Accommodation Agreement'),
+  
+  (N'APPLICPERMIT',  N'ZONING',           N'Zoning'),
+  (N'APPLICPERMIT',  N'ACCESS',           N'Access'),
+  (N'APPLICPERMIT',  N'AGRILANDRES',      N'Agricultural Land Reserves (ALR)'),
+  (N'APPLICPERMIT',  N'DEVELOPMENT',      N'Development'),
+  (N'APPLICPERMIT',  N'SUBDIVISION',      N'Sub-Division'),
+  (N'APPLICPERMIT',  N'PERMITS',          N'Permits'),
+  
+  (N'UTILITYBILL',   N'ELECTRICITYBILL',  N'Electricity'),
+  (N'UTILITYBILL',   N'GASBILL',          N'Gas'),
+  (N'UTILITYBILL',   N'INTERNETBILL',     N'Internet'),
+  (N'UTILITYBILL',   N'SEWERWATERBILL',   N'Sewer and Water'),
+  (N'UTILITYBILL',   N'TELEPHONEBILL',    N'Telephone'),
+  
+  (N'TAXESLEVIES',   N'MUNIPROPTAX',      N'Municipal property taxes'),
+  (N'TAXESLEVIES',   N'WATERTAX',         N'Water'),
+  (N'TAXESLEVIES',   N'OTHERTAX',         N'Other'),
+  
+  (N'CONSULTATION',  N'PUBLIC',           N'Public'),
+  (N'CONSULTATION',  N'INTERNAL',         N'Internal'),
+  (N'CONSULTATION',  N'CONTRACTOR',       N'MoTT contractor/consultant'),
+  (N'CONSULTATION',  N'OTHERCONS',        N'Other'),
+
+  (N'TRAILMTC',      N'RESURFACING',      N'Resurfacing'),
+  (N'TRAILMTC',      N'COMPLAINT',        N'Issue/complaint'),
+  (N'TRAILMTC',      N'VEGETATION',       N'Vegetation brushing'),
+  (N'TRAILMTC',      N'TRESTLEMTC',       N'Trestle maintenance/rehabilitation'),
+  (N'TRAILMTC',      N'RIPARIAN',         N'Riparian restoration'),
+  (N'TRAILMTC',      N'SIGNAGE',          N'Signage'),
+  (N'TRAILMTC',      N'ENGINEERRVW',      N'Engineering review'),
+  
+  (N'CORRESPOND',    N'CORRESPOND',       N'Correspondence');
+GO
+  
+-- --------------------------------------------------------------
+-- Insert the disabled 'UNKNOWN' activity subtype.
+-- --------------------------------------------------------------
+INSERT INTO PIMS_MGMT_ACTIVITY_SUBTYPE (MGMT_ACTIVITY_TYPE_CODE, MGMT_ACTIVITY_SUBTYPE_CODE, DESCRIPTION, IS_DISABLED)
+VALUES
+  (N'UNKNOWN', N'UNKNOWN', N'Unknown', 1);
+GO
+
+-- --------------------------------------------------------------
+-- Update the display order.
+-- --------------------------------------------------------------
+UPDATE pmas
+SET    pmas.DISPLAY_ORDER              = seq.ROW_NUM
+     , pmas.CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+FROM   PIMS_MGMT_ACTIVITY_SUBTYPE pmas JOIN
+       (SELECT MGMT_ACTIVITY_SUBTYPE_CODE
+             , ROW_NUMBER() OVER (ORDER BY DESCRIPTION) AS ROW_NUM
+        FROM   PIMS_MGMT_ACTIVITY_SUBTYPE) seq  ON seq.MGMT_ACTIVITY_SUBTYPE_CODE = pmas.MGMT_ACTIVITY_SUBTYPE_CODE
+GO
