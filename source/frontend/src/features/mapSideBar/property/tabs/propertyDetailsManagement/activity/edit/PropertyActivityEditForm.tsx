8add3dd420b6d3ab2a05a3d2db0829b41c9d2a81
Herrera <eduardo.herrera@quartech.com>
2025-08-07 15:55:30 -0700

    PSP-10678 :  Rename Existing Property Management Tables and Columns

--- source/frontend/src/features/mapSideBar/property/tabs/propertyDetailsManagement/activity/edit/PropertyActivityEditForm.tsx
+++ source/frontend/src/features/mapSideBar/property/tabs/propertyDetailsManagement/activity/edit/PropertyActivityEditForm.tsx
@@ -17,9 +17,9 @@ import * as Styled from '@/components/common/styles';
 import { TrayHeaderContent } from '@/components/common/styles';
 import { RestrictContactType } from '@/components/contact/ContactManagerView/ContactFilterComponent/ContactFilterComponent';
 import {
-  PROP_MGMT_ACTIVITY_STATUS_TYPES,
-  PROP_MGMT_ACTIVITY_SUBTYPES_TYPES,
-  PROP_MGMT_ACTIVITY_TYPES,
+  MGMT_ACTIVITY_STATUS_TYPES,
+  MGMT_ACTIVITY_SUBTYPES_TYPES,
+  MGMT_ACTIVITY_TYPES,
 } from '@/constants/API';
 import SaveCancelButtons from '@/features/leases/SaveCancelButtons';
 import { StyledFormWrapper } from '@/features/mapSideBar/shared/styles';
@@ -59,14 +59,10 @@ export const PropertyActivityEditForm: React.FunctionComponent<
     useState<ManagementActivitySubTypeModel[]>(null);
 
   const lookupCodes = useLookupCodeHelpers();
-  const activityTypeOptions = lookupCodes
-    .getByType(PROP_MGMT_ACTIVITY_TYPES)
-    .map(c => mapLookupCode(c));
-  const activitySubTypeCodes: ILookupCode[] = lookupCodes.getByType(
-    PROP_MGMT_ACTIVITY_SUBTYPES_TYPES,
-  );
+  const activityTypeOptions = lookupCodes.getByType(MGMT_ACTIVITY_TYPES).map(c => mapLookupCode(c));
+  const activitySubTypeCodes: ILookupCode[] = lookupCodes.getByType(MGMT_ACTIVITY_SUBTYPES_TYPES);
   const activityStatusOptions = lookupCodes
-    .getByType(PROP_MGMT_ACTIVITY_STATUS_TYPES)
+    .getByType(MGMT_ACTIVITY_STATUS_TYPES)
     .map(c => mapLookupCode(c));
 
   const onActivityTypeChange = async (changeEvent: ChangeEvent<HTMLInputElement>) => {
