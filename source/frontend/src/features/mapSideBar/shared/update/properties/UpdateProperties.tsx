316268d2ba9b0cd58be0c06c187339e91a42e914
devinleighsmith <devinleighsmith@gmail.com>
2025-09-22 14:05:28 -0700

    psp-10817 re-add property relocation logic.

--- source/frontend/src/features/mapSideBar/shared/update/properties/UpdateProperties.tsx
+++ source/frontend/src/features/mapSideBar/shared/update/properties/UpdateProperties.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { FieldArray, Formik, FormikProps } from 'formik';
-import { geoJSON } from 'leaflet';
+import { geoJSON, LatLngLiteral } from 'leaflet';
 import noop from 'lodash/noop';
 import { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
@@ -16,6 +16,7 @@ import { useMapStateMachine } from '@/components/common/mapFSM/MapStateMachineCo
 import { SelectedFeatureDataset } from '@/components/common/mapFSM/useLocationFeatureLoader';
 import { Section } from '@/components/common/Section/Section';
 import TooltipWrapper from '@/components/common/TooltipWrapper';
+import MapClickMonitor from '@/components/propertySelector/MapClickMonitor';
 import SelectedPropertyHeaderRow from '@/components/propertySelector/selectedPropertyList/SelectedPropertyHeaderRow';
 import SelectedPropertyRow from '@/components/propertySelector/selectedPropertyList/SelectedPropertyRow';
 import { SideBarContext } from '@/features/mapSideBar/context/sidebarContext';
@@ -27,6 +28,14 @@ import { ApiGen_Concepts_File } from '@/models/api/generated/ApiGen_Concepts_Fil
 import { UserOverrideCode } from '@/models/api/UserOverrideCode';
 import { exists, firstOrNull, isValidId, latLngLiteralToGeometry } from '@/utils';
 import { addPropertiesToCurrentFile } from '@/utils/propertyUtils';
+import {
+  arePropertyFormsEqual,
+  exists,
+  isLatLngInFeatureSetBoundary,
+  isNumber,
+  isValidId,
+  latLngLiteralToGeometry,
+} from '@/utils';
 
 import { FileForm, PropertyForm } from '../../models';
 import SidebarFooter from '../../SidebarFooter';
@@ -238,7 +247,7 @@ export const UpdateProperties: React.FunctionComponent<IUpdatePropertiesProps> =
           >
             {formikProps => (
               <FieldArray name="properties">
-                {({ remove }) => (
+              {({ remove, replace }) => (
                   <Section
                     header={
                       <Row>
@@ -256,6 +265,40 @@ export const UpdateProperties: React.FunctionComponent<IUpdatePropertiesProps> =
                       </Row>
                     }
                   >
+                  <Row className="py-3 no-gutters">
+                    <Col>
+                      <MapClickMonitor
+                        selectedComponentId={null}
+                        addProperty={noop}
+                        repositionProperty={(
+                          featureset: SelectedFeatureDataset,
+                          latLng: LatLngLiteral,
+                          index: number | null,
+                        ) => {
+                          // As long as the marker is repositioned within the boundary of the originally selected property simply reposition the marker without further notification.
+                          if (
+                            isNumber(index) &&
+                            index >= 0 &&
+                            isLatLngInFeatureSetBoundary(latLng, featureset)
+                          ) {
+                            const formProperty = formikProps.values.properties[index];
+                            const updatedFormProperty = new PropertyForm(formProperty);
+                            updatedFormProperty.fileLocation = latLng;
+
+                            // Find property within formik values and reposition it based on incoming file marker position
+                            replace(index, updatedFormProperty);
+                          } else if (!isLatLngInFeatureSetBoundary(latLng, featureset)) {
+                            toast.warn(
+                              'Please choose a location that is within the (highlighted) boundary of this property.',
+                            );
+                          }
+                        }}
+                        modifiedProperties={formikProps.values.properties.map(p =>
+                          p.toFeatureDataset(),
+                        )}
+                      />
+                    </Col>
+                  </Row>
                     <SelectedPropertyHeaderRow />
                     {formikProps.values.properties.map((property, index) => (
                       <SelectedPropertyRow
