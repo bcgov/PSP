38bfff0a6961919fb984a27bd390c0eaa802e93c
Alejandro Sanchez <emailforasr@gmail.com>
2025-10-03 15:31:32 -0700

    Update frontend validation of L-File number

--- source/frontend/src/features/leases/list/LeaseFilter/LeaseFilterYupSchema.ts
+++ source/frontend/src/features/leases/list/LeaseFilter/LeaseFilterYupSchema.ts
@@ -1,6 +1,9 @@
 import * as Yup from 'yup';
 
 export const LeaseFilterSchema = Yup.object().shape({
+  lFileNo: Yup.string()
+    .nullable()
+    .matches(/^\s*L?\s*-?\s*[0-9\s-]+\s*$/, 'Invalid L-File number'),
   expiryStartDate: Yup.date(),
   expiryEndDate: Yup.date().when(
     'expiryStartDate',
