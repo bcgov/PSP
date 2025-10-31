de440b16dc6b449598869b06f0dc5c126122db02
Herrera <eduardo.herrera@quartech.com>
2025-10-31 10:11:29 -0700

    PSP-10675 : Expand PIMS Text Fields Character Limitation and Enable Text Block Formatting (line breaks)

--- source/frontend/src/features/mapSideBar/research/tabs/fileDetails/details/ResearchSummaryView.test.tsx
+++ source/frontend/src/features/mapSideBar/research/tabs/fileDetails/details/ResearchSummaryView.test.tsx
@@ -1,20 +1,23 @@
 import Claims from '@/constants/claims';
 import { getMockResearchFile } from '@/mocks/researchFile.mock';
-import { act, render, RenderOptions, userEvent } from '@/utils/test-utils';
+import { act, render, RenderOptions, userEvent, waitFor, waitForEffects } from '@/utils/test-utils';
 
 import ResearchSummaryView, { IResearchSummaryViewProps } from './ResearchSummaryView';
 import Roles from '@/constants/roles';
 
 const setEditMode = vi.fn();
+const mockResearchFile = getMockResearchFile();
 
 describe('ResearchSummaryView component', () => {
-  const setup = (renderOptions: RenderOptions & IResearchSummaryViewProps) => {
+  const setup = async (
+    renderOptions: RenderOptions & { props?: Partial<IResearchSummaryViewProps> },
+  ) => {
     // render component under test
     const utils = render(
       <ResearchSummaryView
-        researchFile={renderOptions.researchFile}
-        setEditMode={renderOptions.setEditMode}
-        isFileFinalStatus={renderOptions.isFileFinalStatus}
+        researchFile={renderOptions.props?.researchFile ?? mockResearchFile}
+        setEditMode={renderOptions.props?.setEditMode ?? setEditMode}
+        isFileFinalStatus={renderOptions.props?.isFileFinalStatus ?? false}
       />,
       {
         ...renderOptions,
@@ -22,50 +25,59 @@ describe('ResearchSummaryView component', () => {
       },
     );
 
-    return { ...utils };
+    return { ...utils, useMockAuthentication: true };
   };
 
-  afterEach(() => {
+  beforeEach(() => {
     vi.resetAllMocks();
   });
 
-  it('renders as expected when research file is provided', () => {
-    const { asFragment } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
-      isFileFinalStatus: false,
-    });
-    expect(asFragment()).toMatchSnapshot();
+  afterEach(() => {
+    vi.clearAllMocks();
   });
 
-  it('does not render the edit button if the user does not have research edit permissions', () => {
-    const { queryByTitle } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
-      claims: [],
-      isFileFinalStatus: false,
-    });
+  it('renders as expected', () => {
+    setup({});
+    expect(document.body).toMatchSnapshot();
+  });
+
+  it('does not render the edit button if the user does not have research edit permissions', async () => {
+    const { queryByTitle } = await setup(
+      {
+        props: {
+        researchFile: getMockResearchFile(),
+        setEditMode,
+        isFileFinalStatus: false,
+        },
+        claims: [],
+      },
+    );
+    await waitForEffects();
     const editResearchFile = queryByTitle('Edit research file');
     expect(editResearchFile).toBeNull();
   });
 
-  it('renders the edit button if the user has research edit permissions', () => {
-    const { getByTitle } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
+  it('renders the edit button if the user has research edit permissions', async () => {
+    const { getByTitle } = await setup({
+      props: {
+        researchFile: getMockResearchFile(),
+        setEditMode,
+        isFileFinalStatus: false,
+      },
       claims: [Claims.RESEARCH_EDIT],
-      isFileFinalStatus: false,
     });
     const editResearchFile = getByTitle('Edit research file');
     expect(editResearchFile).toBeVisible();
   });
 
   it('switches to Edit mode when edit button is clicked', async () => {
-    const { getByTitle } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
+    const { getByTitle } = await setup({
+      props: {
+        researchFile: getMockResearchFile(),
+        setEditMode,
+        isFileFinalStatus: false,
+      },
       claims: [Claims.RESEARCH_EDIT],
-      isFileFinalStatus: false,
     });
     const editResearchFile = getByTitle('Edit research file');
     await act(async () => userEvent.click(editResearchFile));
@@ -73,23 +85,27 @@ describe('ResearchSummaryView component', () => {
   });
 
   it('displays a warning tooltip when the user cannot edit', async () => {
-    const { getByTestId } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
+    const { getByTestId } = await setup({
+      props: {
+        researchFile: getMockResearchFile(),
+        setEditMode,
+        isFileFinalStatus: true,
+      },
       claims: [Claims.RESEARCH_EDIT],
-      isFileFinalStatus: true,
     });
     const editResearchFile = getByTestId('tooltip-icon-research-cannot-edit-tooltip');
     expect(editResearchFile).toBeVisible();
   });
 
   it('ignores final file status and is editable when user is an admin', async () => {
-    const { queryByTestId } = setup({
-      researchFile: getMockResearchFile(),
-      setEditMode,
+    const { queryByTestId } = await setup({
+      props: {
+        researchFile: getMockResearchFile(),
+        setEditMode,
+        isFileFinalStatus: true,
+      },
       claims: [Claims.RESEARCH_EDIT],
       roles: [Roles.SYSTEM_ADMINISTRATOR],
-      isFileFinalStatus: true,
     });
     const editResearchFile = queryByTestId('tooltip-icon-research-cannot-edit-tooltip');
     expect(editResearchFile).toBeNull();
