2450a4df5bef8a8f6e629f4432e8aa254c05a0e2
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2025-12-30 16:05:18 -0700

    Added Search by Legal Description in Search

--- source/frontend/src/hooks/repositories/mapLayer/useFullyAttributedParcelMapLayer.ts
+++ source/frontend/src/hooks/repositories/mapLayer/useFullyAttributedParcelMapLayer.ts
@@ -36,14 +36,22 @@ export const useFullyAttributedParcelMapLayer = () => {
 
   const findByLegalDescription = useCallback(
     async (legalDesc: string) => {
-      const data = await getAllFeatures({ LEGAL_DESCRIPTION: legalDesc }, { timeout: 40000 });
+      try {
+        const pmbc_response = await getAllFeatures(
+          { LEGAL_DESCRIPTION: legalDesc },
+          { timeout: 40000 },
+        );
 
-      // TODO: Enhance useLayerQuery to allow generics to match the Property types
-      return data as
-        | FeatureCollection<Geometry, PMBC_FullyAttributed_Feature_Properties>
-        | undefined;
+        // Return results from PMBC
+        return pmbc_response as
+          | FeatureCollection<Geometry, PMBC_FullyAttributed_Feature_Properties>
+          | undefined;
+      } catch (e: unknown) {
+        handleError();
+        return undefined;
+      }
     },
-    [getAllFeatures],
+    [getAllFeatures, handleError],
   );
 
   const findByPid = useCallback(
