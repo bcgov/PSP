3ce03b721a6eb7c8fe74b981cdc9b4a5886b72b2
devinleighsmith <devinleighsmith@gmail.com>
2026-01-13 21:27:13 -0800

    psp-11083 correct wording of generate* property selection modals.

--- source/frontend/src/features/mapSideBar/research/ResearchGenerateContainer.tsx
+++ source/frontend/src/features/mapSideBar/research/ResearchGenerateContainer.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
-import { useForm12Generation } from '@/features/mapSideBar/shared/generateForm/useForm12Generation';
+import {
+  IGeneratingModal,
+  useForm12Generation,
+} from '@/features/mapSideBar/shared/generateForm/useForm12Generation';
 import { ApiGen_CodeTypes_FormTypes } from '@/models/api/generated/ApiGen_CodeTypes_FormTypes';
 import { ApiGen_Concepts_ResearchFile } from '@/models/api/generated/ApiGen_Concepts_ResearchFile';
 
@@ -19,7 +22,10 @@ export interface IResearchGenerateContainerProps {
 const ResearchGenerateContainer: React.FunctionComponent<
   React.PropsWithChildren<IResearchGenerateContainerProps>
 > = ({ researchFile, View }) => {
-  const [isGenerating, setIsGenerating] = useState(false);
+  const [isGenerating, setIsGenerating] = useState<IGeneratingModal>({
+    display: false,
+    title: null,
+  });
   const generateForm12 = useGenerateForm12();
   const {
     isPropertyModalOpen,
@@ -29,11 +35,11 @@ const ResearchGenerateContainer: React.FunctionComponent<
   } = useForm12Generation(generateForm12, setIsGenerating);
   const generateNotice = useGenerateResearchNotice();
   const handleSelectedNoticeEntries = () => {
-    generateNotice(researchFile?.id).finally(() => setIsGenerating(false));
+    generateNotice(researchFile?.id).finally(() => setIsGenerating({ display: false }));
   };
 
   return (
-    <View isLoading={isGenerating}>
+    <View isLoading={isGenerating.display}>
       <GenerateItemView
         label="Generate Form 12"
         formType={ApiGen_CodeTypes_FormTypes.FORM12}
@@ -41,7 +47,7 @@ const ResearchGenerateContainer: React.FunctionComponent<
       />
       <PropertySelectorModal
         isOpened={isPropertyModalOpen}
-        availiableProperties={researchFile?.fileProperties ?? []}
+        availableProperties={researchFile?.fileProperties ?? []}
         onSelectOk={handleSelectedProperties}
         onCancelClick={closePropertyModal}
       />
