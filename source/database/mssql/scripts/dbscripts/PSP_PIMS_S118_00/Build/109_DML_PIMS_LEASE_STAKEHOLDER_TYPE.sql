7a9f5fa45c08c73d391ccae7e86ee3d2f494d368
dfilteau <dfilteau@hilltopz.com>
2026-02-25 11:54:19 -0800

    IS-118.00 Database Schema
    PSP_PIMS | Development Sprint: S118.00 | Design Sprint: 117 | Date: 2026-Feb-25
    - Dropped sequences:
      - PIMS_ACQUISITION_ACTIVITY_ID_SEQ
      - PIMS_ACQUISITION_ACTIVITY_INSTANCE_ID_SEQ
      - PIMS_ACQUISITION_FILE_PERSON_ID_SEQ
      - PIMS_ACQUISITION_PAYEE_ID_SEQ
      - PIMS_ACT_INST_PROP_ACQ_FILE_ID_SEQ
      - PIMS_ACT_INST_PROP_RSRCH_FILE_ID_SEQ
      - PIMS_ACTIVITY_INSTANCE_DOCUMENT_ID_SEQ
      - PIMS_ACTIVITY_INSTANCE_ID_SEQ
      - PIMS_ACTIVITY_INSTANCE_NOTE_ID_SEQ
      - PIMS_ACTIVITY_MODEL_ID_SEQ
      - PIMS_ACTIVITY_MODEL_TASK_ID_SEQ
      - PIMS_ACTIVITY_SERVICE_FILE_ID_SEQ
      - PIMS_ACTIVITY_TASK_ID_SEQ
      - PIMS_ACTIVITY_TEMPLATE_DOCUMENT_ID_SEQ
      - PIMS_ACTIVITY_TEMPLATE_ID_SEQ
      - PIMS_ASSET_EVALUATION_ID_SEQs
      - PIMS_BUILDING_CONSTRUCTION_TYPE_ID_SEQ
      - PIMS_BUILDING_EVALUATION_ID_SEQ
      - PIMS_BUILDING_FISCAL_ID_SEQ
      - PIMS_BUILDING_ID_SEQ
      - PIMS_BUILDING_OCCUPANT_TYPE_ID_SEQ
      - PIMS_BUILDING_PREDOMINATE_USE_ID_SEQ
      - PIMS_FILE_ENTITY_ID_SEQ
      - PIMS_FILE_ENTITY_PERMISSIONS_ID_SEQ
      - PIMS_FORM_ID_SEQ
      - PIMS_GL_ACCOUNT_ID_SEQ
      - PIMS_INTEREST_HOLDER_PROPERTY_ID_SEQ
      - PIMS_LEASE_ACTIVITY_ID_SEQ
      - PIMS_LEASE_ACTIVITY_INSTANCE_ID_SEQ
      - PIMS_LEASE_ACTIVITY_PERIOD_ID_SEQ
      - PIMS_LEASE_PAYMENT_FORECAST_ID_SEQ
      - PIMS_LEASE_PAYMENT_PERIOD_ID_SEQ
      - PIMS_LEASE_STAKEHOLDER_COMP_REQ_ID_SEQ
      - PIMS_LEASE_TERM_ID_SEQ
      - PIMS_MANAGEMENT_FILE_ACTIVITY_ID_SEQ
      - PIMS_OWNER_REPRESENTATIVE_ID_SEQ
      - PIMS_OWNER_SOLICITOR_ID_SEQ
      - PIMS_PRODUCT_BUSINESS_FUNCTION_ID_SEQ
      - PIMS_PRODUCT_COST_ID_SEQ
      - PIMS_PRODUCT_WORK_ACTIVITY_ID_SEQ
      - PIMS_PROJECT_NUMBER_SEQ
      - PIMS_PROJECT_ORGANIZATION_ID_SEQ
      - PIMS_PROJECT_PROPERTY_ID_SEQ
      - PIMS_PROJECT_TEAM_ID_SEQ
      - PIMS_PROJECT_WORKFLOW_MODEL_ID_SEQ
      - PIMS_PROP_PROP_ADJACENT_LAND_TYPE_ID_SEQ
      - PIMS_PROPERTY_EVALUATION_ID_SEQ
      - PIMS_PROPERTY_PROPERTY_SERVICE_FILE_ID_SEQ
      - PIMS_PROPERTY_SERVICE_FILE_ID_SEQ
      - PIMS_PROPERTY_STRUCTURE_ID_SEQ
      - PIMS_PROPERTY_TAX_ID_SEQ
      - PIMS_RESEARCH_ACTIVITY_INSTANCE_ID_SEQ
      - PIMS_RESPONSIBILITY_CENTRE_ID_SEQ
      - PIMS_STRUCTURE_ID_SEQ
      - PIMS_TASK_ID_SEQ
      - PIMS_TASK_TEMPLATE_ACTIVITY_MODEL_ID_SEQ
      - PIMS_TASK_TEMPLATE_ID_SEQ
      - PIMS_USER_TASK_ID_SEQ
      - PIMS_WORKFLOW_MODEL_ID_SEQ

---
+++ source/database/mssql/scripts/dbscripts/PSP_PIMS_S118_00/Build/109_DML_PIMS_LEASE_STAKEHOLDER_TYPE.sql
@@ -0,0 +1,47 @@
+-- -------------------------------------------------------------------------------------------
+-- Delete all data from the PIMS_LEASE_STAKEHOLDER_TYPE table and repopulate.
+-- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+-- Author        Date         Ticket     Comment
+-- ------------  -----------  ---------  -----------------------------------------------------
+-- Doug Filteau  2021-Aug-24  N/A        Initial version.
+-- Doug Filteau  2025-Oct-15  N/A        Added OWNSOL.
+-- Doug Filteau  2026-Jan-08  PSP-11108  Added OTHRCV and OTHPAY.
+-- -------------------------------------------------------------------------------------------
+
+DELETE FROM PIMS_LEASE_STAKEHOLDER_TYPE
+GO
+
+INSERT INTO PIMS_LEASE_STAKEHOLDER_TYPE (LEASE_STAKEHOLDER_TYPE_CODE, DESCRIPTION, IS_PAYABLE_RELATED)
+VALUES
+  (N'TEN',    N'Tenant',               0),
+  (N'ASGN',   N'Assignee',             0),
+  (N'REP',    N'Representative',       0),
+  (N'PMGR',   N'Property manager',     0),
+  (N'UNK',    N'Other',                0),
+  (N'OTHPAY', N'Other',                1),
+  (N'OWNER',  N'Owner',                1),
+  (N'OWNREP', N'Owner Representative', 1),
+  (N'OWNSOL', N'Owner Solicitor',      1);
+GO
+
+-- -------------------------------------------------------------------------------------------
+-- Update the display order for the non-Other codes by DESCRIPTION
+-- -------------------------------------------------------------------------------------------
+UPDATE biz
+SET    biz.DISPLAY_ORDER              = seq.ROW_NUM
+     , biz.CONCURRENCY_CONTROL_NUMBER = biz.CONCURRENCY_CONTROL_NUMBER + 1
+FROM   PIMS_LEASE_STAKEHOLDER_TYPE biz JOIN
+       (SELECT LEASE_STAKEHOLDER_TYPE_CODE
+             , ROW_NUMBER() OVER (ORDER BY DESCRIPTION) AS ROW_NUM
+        FROM   PIMS_LEASE_STAKEHOLDER_TYPE
+        WHERE  DESCRIPTION <> N'Other') seq ON seq.LEASE_STAKEHOLDER_TYPE_CODE = biz.LEASE_STAKEHOLDER_TYPE_CODE
+GO
+
+-- -------------------------------------------------------------------------------------------
+-- Update the display order for the Other codes
+-- -------------------------------------------------------------------------------------------
+UPDATE PIMS_LEASE_STAKEHOLDER_TYPE
+SET    DISPLAY_ORDER              = 99
+     , CONCURRENCY_CONTROL_NUMBER = CONCURRENCY_CONTROL_NUMBER + 1
+WHERE  DESCRIPTION = N'Other'
+GO
