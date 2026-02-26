3f846dba2d6290c5a751b237abe71aaff45207a0
Alejandro Sanchez <emailforasr@gmail.com>
2026-02-25 21:08:05 -0800

    Refactor configuration handling to use runtime variables instead of config.js

--- source/frontend/src/utils/config.ts
+++ source/frontend/src/utils/config.ts
@@ -5,7 +5,7 @@ import { stringToNull, stringToNullableBoolean } from './formUtils';
 
 declare global {
   interface Window {
-    config: {
+    runtime: {
       VITE_DISABLE_TELEMERY: string;
       VITE_TELEMERY_DEBUG: string;
       VITE_TELEMERY_ENVIRONMENT: string;
@@ -18,36 +18,36 @@ declare global {
   }
 }
 
-// Global toggle to disable telemetry, can be set via environment variable VITE_DISABLE_TELEMERY or window.config.VITE_DISABLE_TELEMERY in production.
+// Global toggle to disable telemetry, can be set via environment variable VITE_DISABLE_TELEMERY or window.runtime.VITE_DISABLE_TELEMERY in production.
 const DISABLE_TELEMETRY: string =
-  window.config?.VITE_DISABLE_TELEMERY || import.meta.env.VITE_DISABLE_TELEMERY || '';
+  window.runtime?.VITE_DISABLE_TELEMERY || import.meta.env.VITE_DISABLE_TELEMERY || '';
 
 const APP_TELEMETRY_DEBUG: string =
-  window.config?.VITE_TELEMERY_DEBUG || import.meta.env.VITE_TELEMERY_DEBUG || '';
+  window.runtime?.VITE_TELEMERY_DEBUG || import.meta.env.VITE_TELEMERY_DEBUG || '';
 
 const APP_TELEMETRY_ENVIRONMENT: string =
-  window.config?.VITE_TELEMERY_ENVIRONMENT || import.meta.env.VITE_TELEMERY_ENVIRONMENT || '';
+  window.runtime?.VITE_TELEMERY_ENVIRONMENT || import.meta.env.VITE_TELEMERY_ENVIRONMENT || '';
 
 const APP_NAME: string =
-  window.config?.VITE_TELEMERY_SERVICE_NAME || import.meta.env.VITE_TELEMERY_SERVICE_NAME || '';
+  window.runtime?.VITE_TELEMERY_SERVICE_NAME || import.meta.env.VITE_TELEMERY_SERVICE_NAME || '';
 
 const APP_VERSION: string = import.meta.env.VITE_PACKAGE_VERSION || '';
 
 const APP_TELEMETRY_URL: string =
-  window.config?.VITE_TELEMERY_URL || import.meta.env.VITE_TELEMERY_URL || '';
+  window.runtime?.VITE_TELEMERY_URL || import.meta.env.VITE_TELEMERY_URL || '';
 
 const APP_TELEMETRY_METRIC_EXPORT_INTERVAL: string =
-  window.config?.VITE_TELEMERY_METRIC_EXPORT_INTERVAL ||
+  window.runtime?.VITE_TELEMERY_METRIC_EXPORT_INTERVAL ||
   import.meta.env.VITE_TELEMERY_METRIC_EXPORT_INTERVAL ||
   '';
 
 const APP_TELEMETRY_TRACE_EXPORT_INTERVAL: string =
-  window.config?.VITE_TELEMERY_TRACE_EXPORT_INTERVAL ||
+  window.runtime?.VITE_TELEMERY_TRACE_EXPORT_INTERVAL ||
   import.meta.env.VITE_TELEMERY_TRACE_EXPORT_INTERVAL ||
   '';
 
 const APP_TELEMETRY_HISTOGRAM_BUCKETS: string =
-  window.config?.VITE_TELEMERY_HISTOGRAM_BUCKETS ||
+  window.runtime?.VITE_TELEMERY_HISTOGRAM_BUCKETS ||
   import.meta.env.VITE_TELEMERY_HISTOGRAM_BUCKETS ||
   '';
 
