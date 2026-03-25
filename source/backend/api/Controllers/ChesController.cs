8159565b6baa8a0c144f02f1b9b2ea07d505f187
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2026-03-25 10:15:04 -0600

    Fix SonarCloud Code Analysis

--- source/backend/api/Controllers/ChesController.cs
+++ source/backend/api/Controllers/ChesController.cs
@@ -1,23 +1,12 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Pims.Api.Models.Ches;
-using Pims.Api.Models.CodeTypes;
-using Pims.Api.Services;
 
 namespace Pims.Api.Controllers
 {
     [ApiController]
     [ApiVersion("1.0")]
-    [Route("v{version:apiVersion}/ches/")]
-    [Route("/ches/")]
+    [Route("v{version:apiVersion}/ches")]
+    [Route("/ches")]
     public class ChesController : ControllerBase
     {
-        private readonly IEmailService _emailService;
-
-        public ChesController(IEmailService emailService)
-        {
-            _emailService = emailService;
-        }
-
     }
 }
