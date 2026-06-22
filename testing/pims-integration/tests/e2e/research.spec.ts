dfb7a74fb265ee2470812fd125734a667c06be42
Sue Tairaku <sue.tairaku@gmail.com>
2026-06-22 11:42:26 -0700

    linting

--- testing/pims-integration/tests/e2e/research.spec.ts
+++ testing/pims-integration/tests/e2e/research.spec.ts
@@ -1,12 +1,11 @@
 import test, { BrowserContext, expect, Page } from '@playwright/test';
 import { ResearchCreatePage } from '../../pages/research/research-create.page';
-import { DocumentListPage } from '../../pages/document/document-list.page'
+import { DocumentListPage } from '../../pages/document/document-list.page';
 
 let context: BrowserContext;
 let page: Page;
 let researchCreatePage: ResearchCreatePage;
 let documentListPage: DocumentListPage;
-
 
 test.describe('Research Files feature', () => {
   test.beforeAll(async ({ browser }) => {
@@ -20,9 +19,7 @@ test.describe('Research Files feature', () => {
     await context.close();
   });
 
-
   test('verify documents invalid types', async () => {
-
     //Navigate to new research file and create a minimum viable research
     await researchCreatePage.goto();
     await researchCreatePage.fillMinimumResearchForm();
@@ -32,7 +29,5 @@ test.describe('Research Files feature', () => {
     //Navigate to the Document tab and create a new document
     await documentListPage.navigateDocumentsTab();
     await documentListPage.addNewDocument();
-    
-
   });
 });
