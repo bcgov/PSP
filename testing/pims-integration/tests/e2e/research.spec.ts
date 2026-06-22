3d250dbed2d0a609d9cf3470ddf7daf667889865
Sue Tairaku <sue.tairaku@gmail.com>
2026-06-22 11:41:20 -0700

    research e2e automation

---
+++ testing/pims-integration/tests/e2e/research.spec.ts
@@ -0,0 +1,38 @@
+import test, { BrowserContext, expect, Page } from '@playwright/test';
+import { ResearchCreatePage } from '../../pages/research/research-create.page';
+import { DocumentListPage } from '../../pages/document/document-list.page'
+
+let context: BrowserContext;
+let page: Page;
+let researchCreatePage: ResearchCreatePage;
+let documentListPage: DocumentListPage;
+
+
+test.describe('Research Files feature', () => {
+  test.beforeAll(async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
+    researchCreatePage = new ResearchCreatePage(page);
+    documentListPage = new DocumentListPage(page);
+  });
+
+  test.afterAll(async () => {
+    await context.close();
+  });
+
+
+  test('verify documents invalid types', async () => {
+
+    //Navigate to new research file and create a minimum viable research
+    await researchCreatePage.goto();
+    await researchCreatePage.fillMinimumResearchForm();
+    await researchCreatePage.confirmButtonClick();
+    await expect(documentListPage.documentTabLink).toBeVisible();
+
+    //Navigate to the Document tab and create a new document
+    await documentListPage.navigateDocumentsTab();
+    await documentListPage.addNewDocument();
+    
+
+  });
+});
