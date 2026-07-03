d15ab80e76f099d9eb1dea935c20e7f954c98810
Sue Tairaku <sue.tairaku@gmail.com>
2026-07-03 16:31:44 -0700

    linting

--- testing/pims-integration/utils/utils.ts
+++ testing/pims-integration/utils/utils.ts
@@ -1,18 +1,16 @@
 export const generateFileName = (fileType: string): string => {
   const now = new Date();
 
-    const timestamp =
-      now.getFullYear().toString() +
-      String(now.getMonth() + 1).padStart(2, '0') +
-      String(now.getDate()).padStart(2, '0') +
-      String(now.getHours()).padStart(2, '0') +
-      String(now.getMinutes()).padStart(2, '0') +
-      String(now.getSeconds()).padStart(2, '0');
+  const timestamp =
+    now.getFullYear().toString() +
+    String(now.getMonth() + 1).padStart(2, '0') +
+    String(now.getDate()).padStart(2, '0') +
+    String(now.getHours()).padStart(2, '0') +
+    String(now.getMinutes()).padStart(2, '0') +
+    String(now.getSeconds()).padStart(2, '0');
 
-    return `${fileType}-${timestamp}`;
+  return `${fileType}-${timestamp}`;
 };
-
-
 export const normalize = (value: string | null): string => {
   return value == null ? '' : String(value).trim();
 };
