c37c3f25ec25b9244ee92e5b17e7dbe8b943394b
Herrera <eduardo.herrera@quartech.com>
2026-07-23 13:06:49 -0700

    PSP-11432 : e2e patch

--- testing/playwright-testing/playwright.config.js
+++ testing/playwright-testing/playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
-
+    headless: process.env.CI ? true : false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     timeout: 50000,
