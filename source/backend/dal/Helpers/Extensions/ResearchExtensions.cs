a85cb3a8a00bc57e94aba53a4ab9bf6514b1a2d3
Sue Tairaku <sue.tairaku@gmail.com>
2026-08-14 14:06:49 -0700

    Changes according to PR feedback

--- source/backend/dal/Helpers/Extensions/ResearchExtensions.cs
+++ source/backend/dal/Helpers/Extensions/ResearchExtensions.cs
@@ -36,14 +36,14 @@ namespace Pims.Dal.Helpers.Extensions
                 predicate = predicate.And(acq => acq.PimsPropertyResearchFiles.Any(pr => pr != null && EF.Functions.Like(pr.Property.Pin.ToString(), $"%{pinValue}%")));
             }
 
-            bool allRegionsSelected = filter.RegionCodes.Count == 4;
+            int allRegions = context.PimsRegions.Where(r => !r.IsDisabled).Count();
+            bool allRegionsSelected = filter.RegionCodes.Count == allRegions;
             if (filter.RegionCodes.Any() && !allRegionsSelected)
             {
                 predicate.And(r => r.PimsPropertyResearchFiles.Any(pr =>
                     pr.Property != null &&
                     filter.RegionCodes.Contains(pr.Property.RegionCode)));
             }
-
 
             if (!string.IsNullOrWhiteSpace(filter.ResearchFileStatusTypeCode))
             {
